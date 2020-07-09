87e00c8c1a400ca1a0e1566bf4dfafdd198e7065
Jhonny Gonzalez <55560129+jhonnyoddball@users.noreply.github.com>
2020-07-09 16:04:44 -0400

    Rename Modal folder (#320)
    * Rename packages/formation-react/src/components/modal/Modal.jsx to packages/formation-react/src/components/Modal/Modal.jsx
    
    * fixed Modal path
    
    * Rename packages/formation-react/src/components/modal/Modal.mdx to packages/formation-react/src/components/Modal/Modal.mdx
    
    * Rename packages/formation-react/src/components/modal/Modal.unit.spec.jsx to packages/formation-react/src/components/Modal/Modal.unit.spec.jsx

--- src/components/OMBInfo/OMBInfo.jsx
+++ src/components/OMBInfo/OMBInfo.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Modal from '../modal/Modal';
+import Modal from '../Modal/Modal';
 
 const modalContents = minutes => (
   <div>
