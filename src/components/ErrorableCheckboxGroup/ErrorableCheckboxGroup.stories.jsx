3c0ec4c29a1599d6e7a59d0ea509717342e02ed7
Craig Wheeler <craig@craigwheeler.net>
2020-10-30 08:57:59 -0400

    15409: Storybook Component - ErrorableCheckboxGroup (#487)

---
+++ src/components/ErrorableCheckboxGroup/ErrorableCheckboxGroup.stories.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ErrorableCheckboxGroup from './ErrorableCheckboxGroup';
+
+export default {
+  title: 'Library/ErrorableCheckboxGroup',
+  component: ErrorableCheckboxGroup,
+  argTypes: {
+    options: {
+      control: 'object',
+    },
+  },
+};
+
+const Template = (args) => <ErrorableCheckboxGroup {...args} />;
+
+const defaultArgs = {
+  label: 'Errorable Checkbox Group',
+  errorMessage: 'This is an error message',
+  required: true,
+  options: [
+    {
+      label: 'Checkbox label',
+      value: 'value',
+      content: 'a string',
+      additional: 'another thing',
+    },
+    {
+      label: 'Checkbox label',
+      value: 'value',
+      content: 'some more content',
+      additional: 'and another thing',
+    },
+  ],
+  values: { key: 'value' },
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  ...defaultArgs,
+};
