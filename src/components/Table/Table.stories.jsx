26adf992acbe8219e92b75e38bd7acf516ae7364
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2020-11-04 14:04:04 -0800

    Change table css on small screens (#496)
    * Change table css on small screens
    
    * Add another row to table data in stories
    
    * Allow long content to wrap
    
    * Improve the stories with better text
    
    * Add story showing that data can have a component
    
    * v6.10.2

--- src/components/Table/Table.stories.jsx
+++ src/components/Table/Table.stories.jsx
@@ -16,22 +16,42 @@ const defaultArgs = {
   },
   data: [
     {
-      column1: 'Testing',
-      column2: 'Moooore testing',
+      title: 'Declaration of Independence',
+      description:
+        'Statement adopted by the Continental Congress declaring independence from the British Empire',
+      year: '1776',
     },
     {
-      column1: 'Second row',
-      column2: 'Some things',
+      title: 'Bill of Rights',
+      description:
+        'The first ten ammendements of the U.S. Constitution guaranteeing rights and freedoms',
+      year: '1791',
+    },
+    {
+      title: 'Declaration of Sentiments',
+      description:
+        'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
+      year: '1848',
+    },
+    {
+      title: 'Emancipation Proclamation',
+      description:
+        'An executive order granting freedom to slaves in designated southern states.',
+      year: '1863',
     },
   ],
   fields: [
     {
-      label: 'Column 1',
-      value: 'column1',
+      label: 'Document title',
+      value: 'title',
     },
     {
-      label: 'Column 2',
-      value: 'column2',
+      label: 'Description',
+      value: 'description',
+    },
+    {
+      label: 'Year',
+      value: 'year',
     },
   ],
 };
@@ -49,11 +69,12 @@ Unsortable.args = {
   ...defaultArgs,
   fields: [
     {
-      label: 'Column 1',
-      value: 'column1',
+      label: 'Document title',
+      value: 'title',
       nonSortable: true,
     },
     defaultArgs.fields[1],
+    defaultArgs.fields[2],
   ],
 };
 
@@ -65,32 +86,54 @@ MissingData.args = {
   ...defaultArgs,
   data: [
     {
-      column1: null,
-      column2: 'Things',
+      title: 'A document',
+      description: null,
+      year: null,
     },
     defaultArgs.data[1],
   ],
 };
 
+// This story might not be useful
 export const AlignLeft = Template.bind({});
 AlignLeft.args = {
   ...defaultArgs,
   fields: [
+    defaultArgs.fields[0],
+    defaultArgs.fields[1],
     {
-      label: 'Cost ($)',
-      value: 'cost',
+      label: 'Year',
+      value: 'year',
       alignLeft: true,
     },
-    defaultArgs.fields[1],
   ],
+};
+
+export const CustomComponents = Template.bind({});
+CustomComponents.args = {
+  ...defaultArgs,
   data: [
+    ...defaultArgs.data,
     {
-      cost: 50.5,
-      column2: 'Food',
-    },
-    {
-      cost: 70.0,
-      column2: 'Utilities',
+      title: 'Social Security Act',
+      description: (
+        <>
+          <div>
+            An act to provide for the general welfare by establishing a system
+            of Federal old-age benefits. Enables provisions for:
+          </div>
+
+          <ul>
+            <li>aged persons</li>
+            <li>blind persons</li>
+            <li>dependent and crippled children</li>
+            <li>maternal and child welfare</li>
+            <li>public health</li>
+            <li>unemployment compensation</li>
+          </ul>
+        </>
+      ),
+      year: '1935',
     },
   ],
 };
