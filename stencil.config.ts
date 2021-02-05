6d2a5a8043281337b861d24757814079daa75a96
Brooks Johnson <brooks.johnson@pm.me>
2021-02-05 09:00:00 -0800

    Setup postcss to inline assets

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,14 @@
 import { Config } from '@stencil/core';
+import { postcss } from '@stencil/postcss';
+import url from 'postcss-url';
 
 export const config: Config = {
   namespace: 'component-library',
+  plugins: [
+    postcss({
+      plugins: [url({ url: 'inline' })],
+    }),
+  ],
   outputTargets: [
     {
       type: 'dist',
