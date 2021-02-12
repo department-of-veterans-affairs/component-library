9ec2d2ff1b29b89aa18724550608f9346392d698
Brian Martin <brian.martin@empiricalpath.com>
2021-02-11 20:36:11 -0500

    Remove old comments

--- src/helpers/analytics.js
+++ src/helpers/analytics.js
@@ -1,10 +1,3 @@
-/**
- * Helper function for reporting events to Google Analytics. An alias for window.dataLayer.push.
- * @module platform/monitoring/record-event
- * @see https://developers.google.com/tag-manager/devguide
- * @param {object} data - The event data that will be sent to GA.
- */
-
 export default function dispatchAnalayticsEvent({type, action, ...data}) {
   const event = new CustomEvent("component.library.analytics", {
     detail: {
