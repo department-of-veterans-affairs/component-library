12811568b42c22c9abbba06aa565471043156088
Brooks Johnson <brooks.johnson@pm.me>
2021-02-16 10:07:59 -0800

    Add e2e test to accordion item for aria attribute

--- src/components/va-accordion/va-accordion-item.e2e.ts
+++ src/components/va-accordion/va-accordion-item.e2e.ts
@@ -8,4 +8,12 @@ describe('va-accordion-item', () => {
     const element = await page.find('va-accordion-item');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('sets aria-expanded to true based on prop', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-accordion-item open header="The header">Content inside</va-accordion-item>');
+    const button = await page.find('va-accordion-item >>> button');
+
+    expect(button.getAttribute('aria-expanded')).toEqual('true');
+  });
 });
