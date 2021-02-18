e28f29e6fad6654bd7e6ba73190a52af24f4bc77
Brooks Johnson <brooks.johnson@pm.me>
2021-02-18 11:58:26 -0800

    Include component docs in build

--- src/components/va-accordion/va-accordion.tsx
+++ src/components/va-accordion/va-accordion.tsx
@@ -1,5 +1,22 @@
 import { Component, Element, Host, Listen, Prop, h } from '@stencil/core';
 
+/**
+ * Accordions are a list of headers that can be clicked to hide or reveal additional content.
+ *
+ * ## Usability
+ * ### When to use
+ * - Users only need a few specific pieces of content within a page.
+ * - Information needs to be displayed in a small space.
+ *
+ * ### When to consider something else
+ * - If visitors need to see most or all of the information on a page. Use well-formatted text instead.
+ * - If there is not enough content to warrant condensing. Accordions increase cognitive load and interaction cost, as users have to make decisions about what headers to click on.
+ *
+ * ### Guidance
+ *
+ * - Allow users to click anywhere in the header area to expand or collapse the content; a larger target is easier to manipulate.
+ * - Make sure interactive elements within the collapsible region are far enough from the headers that users don’t accidentally trigger a collapse. (The exact distance depends on the device.
+ */
 @Component({
   tag: 'va-accordion',
   styleUrl: 'va-accordion.css',
