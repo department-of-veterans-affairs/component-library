2479cabc27e6f813dbed4466c16172e3b72288a4
Brooks Johnson <brooks.johnson@pm.me>
2021-02-18 20:44:36 -0800

    Disable rules rather than filter them out

--- src/testing/test-helpers.ts
+++ src/testing/test-helpers.ts
@@ -4,10 +4,9 @@ const ignoredDefaults = ['document-title', 'html-has-lang'];
 
 export async function axeCheck(page: any, additionalIgnored: string[] = []) {
   const ignoredRules = [...ignoredDefaults, ...additionalIgnored];
-  const results = await new AxePuppeteer(page).analyze();
-  const validViolations = results.violations.filter(violation => !ignoredRules.includes(violation.id));
+  const results = await new AxePuppeteer(page).disableRules(ignoredRules).analyze();
 
-  const appropriateSeverity = validViolations.filter(violation => ['serious', 'critical'].includes(violation.impact));
+  const appropriateSeverity = results.violations.filter(violation => ['serious', 'critical'].includes(violation.impact));
 
   expect(appropriateSeverity).toEqual([]);
 }
