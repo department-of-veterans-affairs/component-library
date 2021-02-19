56bf4e6033359853004890ef5b7353b19c93a927
Brooks Johnson <brooks.johnson@pm.me>
2021-02-18 22:04:34 -0800

    Add better output formatting

--- src/testing/test-helpers.ts
+++ src/testing/test-helpers.ts
@@ -2,11 +2,22 @@ import { AxePuppeteer } from '@axe-core/puppeteer';
 
 const ignoredDefaults = ['document-title', 'html-has-lang'];
 
+const buildAxeErrorOutput = violations =>
+  violations
+    .map(
+      v =>
+        `[${v.impact}] ${v.help}
+Id: ${v.id}
+See ${v.helpUrl}
+${v.nodes.reduce((str, { html, target }) => [str, html, ...target].join('\n\n'), '')}`,
+    )
+    .join('\n') || null;
+
 export async function axeCheck(page: any, additionalIgnored: string[] = []) {
   const ignoredRules = [...ignoredDefaults, ...additionalIgnored];
   const results = await new AxePuppeteer(page).disableRules(ignoredRules).analyze();
 
   const appropriateSeverity = results.violations.filter(violation => ['serious', 'critical'].includes(violation.impact));
-
-  expect(appropriateSeverity).toEqual([]);
+  const axeError = buildAxeErrorOutput(appropriateSeverity);
+  expect(axeError).toBe(null);
 }
