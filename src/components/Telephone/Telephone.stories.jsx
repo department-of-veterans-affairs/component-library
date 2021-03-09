448dde1638519431d0e073cdb184970b6787d8cd
Robin Garrison <robin.garrison@adhocteam.us>
2021-03-09 11:41:59 -0600

    Add pattern table & more stories

--- src/components/Telephone/Telephone.stories.jsx
+++ src/components/Telephone/Telephone.stories.jsx
@@ -8,7 +8,7 @@ import {
   Stories,
 } from '@storybook/addon-docs/blocks';
 
-import Telephone, { CONTACTS } from './Telephone';
+import Telephone, { CONTACTS, PATTERNS } from './Telephone';
 import { contactsMap } from './contacts';
 import Table from '../Table/Table';
 
@@ -28,24 +28,61 @@ const Contacts = () => (
   />
 );
 
+const patternFields = [
+  { label: 'Pattern name (PATTERN.x)', value: 'key' },
+  { label: 'Pattern', value: 'value' },
+  { label: 'Description', value: 'description' },
+];
+const patternDescriptions = {
+  '3_DIGIT':
+    'Used for 3-digit numbers (e.g. 711 & 911); automatically set for 3-digit numbers',
+  DEFAULT: (
+    <>
+      Standard telephone format. See the{' '}
+      <a href="https://design.va.gov/content-style-guide/dates-and-numbers#phone-numbers">
+        phone number design specification
+      </a>
+      .
+    </>
+  ),
+  OUTSIDE_US:
+    'Pattern used for numbers where the Veteran is located outside the United States',
+};
+const Patterns = () => (
+  <Table
+    fields={patternFields}
+    data={Object.entries(PATTERNS).map(p => ({
+      key: p[0],
+      value: p[1],
+      description: (
+        <div style={{ maxWidth: '30em' }}>{patternDescriptions[p[0]]}</div>
+      ),
+    }))}
+  />
+);
+
+const Page = () => (
+  <>
+    <Title />
+    <Subtitle />
+    <Description />
+    <Primary />
+    <ArgsTable />
+    <Contacts />
+    <br />
+    <Patterns />
+    <br />
+    <Stories />
+  </>
+);
+
 export default {
   title: 'Components/Telephone',
   component: Telephone,
   parameters: {
     docs: {
-      // Add the contacts table to the docs page
-      page: () => (
-        <>
-          <Title />
-          <Subtitle />
-          <Description />
-          <Primary />
-          <ArgsTable />
-          <Contacts />
-          <br />
-          <Stories />
-        </>
-      ),
+      // Add the contacts & pattern table to the docs page
+      page: Page,
     },
   },
 };
@@ -62,5 +99,14 @@ Default.args = { ...defaultArgs };
 export const DisplayOnly = Template.bind({});
 DisplayOnly.args = { ...defaultArgs, notClickable: true };
 
+export const ThreeDigitNumber = Template.bind({});
+ThreeDigitNumber.args = { contact: '711' };
+
 export const Extension = Template.bind({});
 Extension.args = { ...defaultArgs, extension: '123' };
+
+export const CustomContent = Template.bind({});
+CustomContent.args = {
+  contact: CONTACTS['222_VETS'],
+  children: '877-222-VETS (8387)',
+};
