aebc82a4b234bebd8fa2ba062d684d0261f60410
Brian Martin <brian.martin@empiricalpath.com>
2021-03-15 20:41:17 -0400

    Added test + upped package version

--- src/components/LoadingIndicator/LoadingIndicator.unit.spec.jsx
+++ src/components/LoadingIndicator/LoadingIndicator.unit.spec.jsx
@@ -1,12 +1,15 @@
 import { expect } from 'chai';
 import React from 'react';
+import sinon from 'sinon';
 import { axeCheck, mountToDiv } from '../../helpers/test-helpers';
 
 import LoadingIndicator from './LoadingIndicator.jsx';
 
 describe('<LoadingIndicator>', () => {
   it('should not focus if setFocus is not set', () => {
-    const component = <LoadingIndicator message="Loading" label="aria label here" />;
+    const component = (
+      <LoadingIndicator message="Loading" label="aria label here" />
+    );
     const mountedComponent = mountToDiv(component, 'loadingContainer');
 
     expect(document.activeElement.classList.contains('loading-indicator')).to.be
@@ -15,7 +18,9 @@ describe('<LoadingIndicator>', () => {
   });
 
   it('should focus if setFocus is set', () => {
-    const component = <LoadingIndicator setFocus message="Loading" label="aria label here" />;
+    const component = (
+      <LoadingIndicator setFocus message="Loading" label="aria label here" />
+    );
     const mountedComponent = mountToDiv(component, 'loadingContainer');
 
     expect(document.activeElement.classList.contains('loading-indicator')).to.be
@@ -24,5 +29,40 @@ describe('<LoadingIndicator>', () => {
   });
 
   it('should pass aXe check', () =>
-    axeCheck(<LoadingIndicator message="Loading" label="aria label here"/>));
+    axeCheck(<LoadingIndicator message="Loading" label="aria label here" />));
+
+  describe('analytics event', function () {
+    it('should be triggered when component is unmounted', () => {
+      const component = (
+        <LoadingIndicator message="Loading" label="aria label here" />
+      );
+      const handleAnalyticsEvent = sinon.spy();
+
+      const mountedComponent = mountToDiv(component, 'loadingContainer');
+
+      global.document.body.addEventListener(
+        'component-library-analytics',
+        handleAnalyticsEvent,
+      );
+
+      mountedComponent.unmount();
+
+      global.document.body.removeEventListener(
+        'component-library-analytics',
+        handleAnalyticsEvent,
+      );
+
+      expect(
+        handleAnalyticsEvent.calledWith(
+          sinon.match.has('detail', {
+            componentName: 'LoadingIndicator',
+            action: 'displayed',
+            details: {
+              displayTime: sinon.match.number,
+            },
+          }),
+        ),
+      ).to.be.true;
+    });
+  });
 });
