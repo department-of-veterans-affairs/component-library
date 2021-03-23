74ab7413b263909b4fa997c9de7d7ad300c4530f
Brooks Johnson <brooks.johnson@pm.me>
2021-03-23 09:51:05 -0700

    Use better data for stories

--- src/components/va-table/va-table.stories.tsx
+++ src/components/va-table/va-table.stories.tsx
@@ -6,14 +6,44 @@ export default {
 };
 
 const defaultArgs = {
-  columns: [
-    { label: 'one', value: 'one' },
-    { label: 'two', value: 'two' },
-  ],
   data: [
     {
-      one: 'hello darkness',
-      two: 'my old friend',
+      title: 'Declaration of Independence',
+      description:
+        'Statement adopted by the Continental Congress declaring independence from the British Empire',
+      year: '1776',
+    },
+    {
+      title: 'Bill of Rights',
+      description:
+        'The first ten ammendements of the U.S. Constitution guaranteeing rights and freedoms',
+      year: '1791',
+    },
+    {
+      title: 'Declaration of Sentiments',
+      description:
+        'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
+      year: '1848',
+    },
+    {
+      title: 'Emancipation Proclamation',
+      description:
+        'An executive order granting freedom to slaves in designated southern states.',
+      year: '1863',
+    },
+  ],
+  columns: [
+    {
+      label: 'Document title',
+      value: 'title',
+    },
+    {
+      label: 'Description',
+      value: 'description',
+    },
+    {
+      label: 'Year',
+      value: 'year',
     },
   ],
 };
