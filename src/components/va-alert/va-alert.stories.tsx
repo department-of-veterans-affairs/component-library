0c7b30161abf6933a463320ac6513c837089de6f
Brooks Johnson <brooks.johnson@pm.me>
2021-04-06 14:40:36 -0700

    Add more states

--- src/components/va-alert/va-alert.stories.tsx
+++ src/components/va-alert/va-alert.stories.tsx
@@ -24,5 +24,13 @@ export const Default = Template.bind({});
 Default.args = { ...defaultArgs };
 
 export const Continue = Template.bind({});
-
 Continue.args = { ...defaultArgs, status: 'continue' };
+
+export const Success = Template.bind({});
+Success.args = { ...defaultArgs, status: 'success' };
+
+export const Warning = Template.bind({});
+Warning.args = { ...defaultArgs, status: 'warning' };
+
+export const Error = Template.bind({});
+Error.args = { ...defaultArgs, status: 'error' };
