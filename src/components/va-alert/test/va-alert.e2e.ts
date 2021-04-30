f2250ee009e8feb811a9cd8b75457f849b6ed8be
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2021-04-30 09:19:24 -0700

    Add an alert component (#74)
    * Commit a *very* rough first version of an alert
    
    * Add some colors to global CSS variables
    
    * Begin adding stuff for other statuses
    
    * Add more states
    
    * Change to h3 & set line-height
    
    * Fix basic test & set status default to info
    
    * Add `visible` prop
    
    This is based on the current `<AlertBox>` component
    
    * Allow changing the headline level
    
    * Implement background-only styling
    
    * Implement the close event
    
    * Add analytics event functionality
    
    * Add some margin above the alert body text
    
    * Fix some tests
    
    * Tweak the CSS a bit more
    
    * Solve a11y issue
    
    This issue is with the existing `<AlertBox>` component, but part of the
    reason behind making this web component is to solve it:
    
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/19674
    
    * Simplify some CSS
    
    * Remove some seemingly unimportant CSS
    
    * Include some autogenerated stuff
    
    * Tweak CSS for icon display
    
    * Add uncloseable prop
    
    * Update story for testing analytics event
    
    * Clean up the closeable/uncloseable functionality
    
    * Document the status options better
    
    * Add axeCheck test
    
    * Actually run the axeCheck
    
    * Clean up backgroundOnly styling
    
    * Add hover styling for close button
    
    * Try to fix IE styling issues
    
    * Adjust CSS again
    
    * Add closeable story
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Valarida <chris.valarida@adhocteam.us>
    
    * Add comments for some props
    
    * Update autogenerated file
    
    * Use named slot for headline rather than props
    
    * Ensure that headings have the right font size
    
    * Fix comment syntax for status docs
    
    * Get headline text for use in analytics event
    
    * Update src/components/va-alert/test/va-alert.e2e.ts
    
    Co-authored-by: Chris Valarida <chris.valarida@adhocteam.us>
    
    * Handle missing headline when firing analytics evt
    
    * Add test for the null headline in analytics event
    
    * v0.5.0
    
    Co-authored-by: Chris Valarida <chris.valarida@adhocteam.us>

---
+++ src/components/va-alert/test/va-alert.e2e.ts
@@ -0,0 +1,144 @@
+import { newE2EPage } from '@stencil/core/testing';
+import { axeCheck } from '../../../testing/test-helpers';
+
+describe('va-alert', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-alert></va-alert>');
+    const element = await page.find('va-alert');
+
+    expect(element).toEqualHtml(`
+      <va-alert class="hydrated">
+        <mock:shadow-root>
+          <div class="alert info">
+            <i aria-hidden="true" role="img"></i>
+            <span class="sr-only">Alert:</span>
+            <div class="body">
+              <slot name="headline"></slot>
+              <div class="text">
+                <slot></slot>
+              </div>
+            </div>
+          </div>
+        </mock:shadow-root>
+      </va-alert>
+    `);
+  });
+
+  it('renders an empty div with a "polite" aria-live tag when not visible', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-alert visible="false"></va-alert>');
+    const element = await page.find('va-alert');
+
+    expect(element).toEqualHtml(`
+      <va-alert class="hydrated" visible="false">
+        <mock:shadow-root>
+          <div aria-live="polite"></div>
+        </mock:shadow-root>
+      </va-alert>
+    `);
+  });
+
+  it('passes an axe check', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `<va-alert><h3 slot="headline">Alert</h3>Alert content</va-alert>`,
+    );
+
+    await axeCheck(page);
+  });
+
+  it('only shows a close icon if the closeable prop is passed', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-alert>Alert</va-alert>');
+
+    const element = await page.find('va-alert');
+
+    let button = await page.find('va-alert >>> button');
+    expect(button).toBeNull();
+
+    element.setProperty('closeable', true);
+    await page.waitForChanges();
+    button = await page.find('va-alert >>> button');
+
+    expect(button).not.toBeNull();
+  });
+
+  it('fires a custom "close" event when the close button is clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-alert closeable="true">Content inside</va-alert>',
+    );
+
+    const closeSpy = await page.spyOnEvent('close');
+
+    const button = await page.find('va-alert >>> button');
+    await button.click();
+
+    expect(closeSpy).toHaveReceivedEventTimes(1);
+  });
+
+  it('fires an analytics event when a link is clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-alert><h4 slot="headline">This is an alert</h4><a href="#">This is a link</a></va-alert>',
+    );
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+
+    const link = await page.find('va-alert a');
+    await link.click();
+
+    expect(analyticsSpy).toHaveReceivedEventDetail({
+      action: 'linkClick',
+      componentName: 'AlertBox',
+      details: {
+        headline: 'This is an alert',
+        backgroundOnly: false,
+        clickLabel: 'This is a link',
+        status: 'info',
+        closeable: false,
+      },
+    });
+  });
+
+  it('uses a null headline in the analytics event detail when the heading is absent', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-alert><a href="#">This is a link</a></va-alert>',
+    );
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+
+    const link = await page.find('va-alert a');
+    await link.click();
+
+    expect(analyticsSpy).toHaveReceivedEventDetail({
+      action: 'linkClick',
+      componentName: 'AlertBox',
+      details: {
+        headline: null,
+        backgroundOnly: false,
+        clickLabel: 'This is a link',
+        status: 'info',
+        closeable: false,
+      },
+    });
+  });
+
+  it('does not fire an analytics event when disableAnalytics is passed', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-alert disable-analytics="true"><a href="#">This is a link</a></va-alert>',
+    );
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+
+    const link = await page.find('va-alert a');
+    await link.click();
+
+    expect(analyticsSpy).toHaveReceivedEventTimes(0);
+  });
+});
