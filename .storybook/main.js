8b3f94c42a110b89b88b96330cb31b5f0f9cf5dd
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2021-05-10 16:46:55 -0700

    Add crisis line modal story (#90)
    * Add crisis line modal story
    
    * Tweak the styles
    
    * Remove commented out sass import
    
    This was necessary because before commiting I was importing a sass file
    which had a lot more implicit dependencies, but I ended up trying to be
    as specific and minimal as possible with these imports

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../@(src|stories)/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
@@ -6,4 +8,20 @@ module.exports = {
     '@storybook/addon-links',
     // '@whitespace/storybook-addon-html/register',
   ],
+
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 };
