ae073bc32859875f12d84f06e1d2681b409717b3
Brian Martin <62304138+bsmartin-ep@users.noreply.github.com>
2021-05-11 19:18:31 -0400

    Added analytics to accordion web component (#81)
    * Added analaytics to accordion web component
    
    * Fixed test
    
    * typo
    
    * Fixed test
    
    * Fixed test + click target
    
    * Added level to tests
    
    * Simplified tests
    
    * Bumped package version

--- src/components/va-accordion/va-accordion.stories.tsx
+++ src/components/va-accordion/va-accordion.stories.tsx
@@ -9,31 +9,33 @@ const defaultArgs = {
   multi: false,
   bordered: false,
   subheader: false,
+  level: 2
 };
 
-const Template = ({ multi, bordered }) => html`
+const Template = ({ multi, bordered, level }) => html`
   <va-accordion bordered="${bordered}" multi="${multi}">
-    <va-accordion-item header="First Amendment">
+    <va-accordion-item header="First Amendment" level=${level}>
       Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the
       right of the people peaceably to assemble, and to petition the Government for a redress of grievances.
     </va-accordion-item>
-    <va-accordion-item header="Second Amendment">
+    <va-accordion-item header="Second Amendment" level=${level}>
       A well regulated Militia, being necessary to the security of a free State, the right of the people to keep and bear Arms, shall not be infringed.
     </va-accordion-item>
   </va-accordion>
 `;
 
-const TemplateSubheader = ({ subheader1, subheader2 }) => html `
+const TemplateSubheader = ({ subheader1, subheader2, level }) => html`
   <va-accordion>
-    <va-accordion-item header="First Amendment" subheader=${subheader1}>
+    <va-accordion-item header="First Amendment" subheader=${subheader1} level=${level}>
       Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the
       right of the people peaceably to assemble, and to petition the Government for a redress of grievances.
     </va-accordion-item>
-    <va-accordion-item header="Second Amendment" subheader=${subheader2}>
+    <va-accordion-item header="Second Amendment" subheader=${subheader2} level=${level}>
       A well regulated Militia, being necessary to the security of a free State, the right of the people to keep and bear Arms, shall not be infringed.
     </va-accordion-item>
   </va-accordion>
 `;
+
 
 export const Default = Template.bind({});
 
@@ -49,4 +51,4 @@ Bordered.args = { ...defaultArgs, bordered: true };
 
 export const Subheaders = TemplateSubheader.bind({});
 
-Subheaders.args = { ...defaultArgs, subheader1: 'First Amendment Subheader', subheader2: 'Second Amendment Subheader'};
+Subheaders.args = { ...defaultArgs, subheader1: 'First Amendment Subheader', subheader2: 'Second Amendment Subheader' };
