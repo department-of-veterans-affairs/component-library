50c2f1ea02b0a5215e77395547fe7712838dd63a
Brooks Johnson <brooks.johnson@pm.me>
2021-05-25 18:10:51 -0700

    Move function into utils dir
    Need to figure out how to unit test this

--- src/components/va-select/va-select.tsx
+++ src/components/va-select/va-select.tsx
@@ -1,4 +1,5 @@
 import { Component, Element, Host, Prop, State, h } from '@stencil/core';
+import { getSlottedNodes } from '../../utils/utils';
 
 @Component({
   tag: 'va-select',
@@ -23,31 +24,9 @@ export class VaSelect {
     console.log(e);
   }
 
-  /**
-   * Get all of the slotted children in the root element that match `nodeName`
-   */
-  getSlottedNodes(root: HTMLElement, nodeName: string): Array<Node> {
-    // If the browser is using the shadowDOM, the childNodes should be an array of two things:
-    // A `<style>` element and a `<slot>` element
-    // Chrome, Firefox, Safari, Edge - literally every modern browser will use shadowDOM.
-    // This is purely for IE compatibility
-    const hasShadowDOM =
-      Array.from(root.shadowRoot.childNodes).filter(
-        (node: any) => node.tagName === 'SLOT',
-      ).length > 0;
-
-    const children = hasShadowDOM
-      ? root.shadowRoot.querySelector('slot').assignedNodes()
-      : root.shadowRoot.childNodes;
-
-    return Array.from(children).filter(
-      item => item.nodeName.toLowerCase() === nodeName,
-    );
-  }
-
   componentDidRender() {
     if (!this.options)
-      this.options = this.getSlottedNodes(this.el, 'option').map(
+      this.options = getSlottedNodes(this.el, 'option').map(
         (node: HTMLOptionElement) => {
           return <option value={node.value}>{node.text}</option>;
         },
