e84d3adfd81dd3d4e44bea5c4b90508dda5671c2
Christopher Valarida <chris.valarida@adhocteam.us>
2021-06-23 07:52:43 -0700

    Test stubs

--- src/components/va-checkbox/test/va-checkbox.e2e.ts
+++ src/components/va-checkbox/test/va-checkbox.e2e.ts
@@ -8,4 +8,25 @@ describe('va-checkbox', () => {
     const element = await page.find('va-checkbox');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('renders a label', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-checkbox label="Cool label, right?" />');
+    const element = await page.find('va-checkbox >>> label');
+    expect(element).not.toBeNull();
+  });
+
+  it('renders an error message', async () => {});
+
+  it('renders a required span', async () => {});
+
+  it('appends to an existing aria-describedby for error message', async () => {});
+
+  it('passes an aXe check', async () => {});
+
+  it('fires an analytics event', async () => {});
+
+  it("doesn't fire an analytics event when disableAnalytics is true", async () => {});
+
+  it('passes unknown props to the input element in the shadow DOM', async () => {});
 });
