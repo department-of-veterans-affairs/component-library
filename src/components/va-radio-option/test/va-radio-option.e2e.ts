5dab7b4343dca3d6cf6e8cf809e1dc75e812ff7f
Brooks Johnson <brooks.johnson@pm.me>
2021-07-13 10:40:40 -0700

    Add a test for radio option

--- src/components/va-radio-option/test/va-radio-option.e2e.ts
+++ src/components/va-radio-option/test/va-radio-option.e2e.ts
@@ -8,4 +8,15 @@ describe('va-radio-option', () => {
     const element = await page.find('va-radio-option');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('sets checked to true based on prop', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-radio-option checked label="An option"></va-radio-option>',
+    );
+
+    const inputEl = await page.find('va-radio-option >>> input');
+
+    expect(await inputEl.getProperty('checked')).toBeTruthy();
+  });
 });
