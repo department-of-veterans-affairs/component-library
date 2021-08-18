7900ce46c762fad37abf87be3babf5ef53e9ef49
noahgelman <noahmgelman@gmail.com>
2021-08-18 08:26:53 -0700

    Removed old test case line

--- src/components/va-alert/test/va-alert.e2e.ts
+++ src/components/va-alert/test/va-alert.e2e.ts
@@ -147,7 +147,6 @@ describe('va-alert', () => {
 
     const element = await page.find('va-alert >>> .alert');
 
-    expect(element).toEqualAttribute('role', 'alert');
     expect(element).toEqualAttributes({
       'role': 'alert',
       'aria-live': 'assertive'
