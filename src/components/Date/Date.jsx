3b8820accfc035d1fa1873c87b316a93f21e0d81
Christopher Valarida <chris.valarida@adhocteam.us>
2021-09-07 11:29:12 -0700

    Refactor: Use SimpleDate

--- src/components/Date/Date.jsx
+++ src/components/Date/Date.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-for */
 import PropTypes from 'prop-types';
 import React from 'react';
-import _ from 'lodash';
 
-import Select from '../Select/Select';
-import NumberInput from '../NumberInput/NumberInput';
+import SimpleDate from './SimpleDate';
 
 import {
   minYear,
@@ -14,148 +12,54 @@ import {
   isNotBlankDateField,
   validateCustomFormComponent,
 } from '../../helpers/validations';
-import { months, days } from '../../helpers/options-for-select.js';
 
 /**
  * A date input field that accepts values for month and year
  */
 
 class Date extends React.Component {
-  constructor() {
-    super();
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  UNSAFE_componentWillMount() {
-    this.inputId = _.uniqueId('date-input-');
-  }
-
-  handleChange(path, update) {
-    const date = {
-      ...this.props.date,
-      [path]: update,
-    };
-
-    if (!date.month.value) {
-      date.day.value = '';
-    }
-
-    this.props.onValueChange(date);
-  }
-
   render() {
     const { day, month, year } = this.props.date;
 
-    let daysForSelectedMonth = [];
-    if (month.value) {
-      daysForSelectedMonth = days[month.value];
-    }
-
     // we want to do validations in a specific order, so we show the message
     // that makes the most sense to the user
-    let isValid = true;
     let errorMessage;
     if (isDirtyDate(this.props.date)) {
       // make sure the user enters a full date first, if required
       if (this.props.required && !isNotBlankDateField(this.props.date)) {
-        isValid = false;
         errorMessage = this.props.requiredMessage;
         // make sure the user has entered a minimally valid date
       } else if (!isValidPartialDate(day.value, month.value, year.value)) {
-        isValid = false;
         errorMessage = this.props.invalidMessage;
       } else {
         const validationResult = validateCustomFormComponent(
           this.props.validation,
         );
-        isValid = validationResult.valid;
         errorMessage = validationResult.message;
       }
     } else if (year.value.length >= 4) {
       const yr = parseInt(year.value, 10);
       if (yr < minYear || yr > maxYear) {
-        isValid = false;
         errorMessage = `Please enter a year between ${minYear} and ${maxYear}`;
       }
     }
 
-    let errorSpanId;
-    let errorSpan = '';
-    let fieldsetClass;
-    if (!isValid) {
-      fieldsetClass = 'input-error-date usa-input-error';
-      errorSpanId = `${this.inputId}-error-message`;
-      errorSpan = (
-        <span className="usa-input-error-message" role="alert" id={errorSpanId}>
-          <span className="sr-only">Error</span> {errorMessage}
-        </span>
-      );
-    }
-
-    const ariaDescribedby =
-      [errorSpanId, this.props.ariaDescribedby || ''].join(' ').trim() ||
-      undefined;
-
     return (
-      <fieldset className={fieldsetClass}>
-        <legend className="vads-u-font-size--base vads-u-font-weight--normal">
-          {this.props.label || 'Date of birth'}
-          {this.props.required && (
-            <span className="form-required-span">(*Required)</span>
-          )}
-        </legend>
-        {errorSpan}
-        <div
-          className={isValid ? undefined : 'usa-input-error form-error-date'}
-        >
-          <div className="usa-date-of-birth usa-datefields clearfix">
-            <div className="form-datefield-month">
-              <Select
-                errorMessage={isValid ? undefined : ''}
-                label="Month"
-                name={`${this.props.name}Month`}
-                options={months}
-                value={month}
-                onValueChange={update => {
-                  this.handleChange('month', update);
-                }}
-                ariaDescribedby={ariaDescribedby}
-              />
-            </div>
-            <div className="form-datefield-day">
-              <Select
-                errorMessage={isValid ? undefined : ''}
-                label="Day"
-                name={`${this.props.name}Day`}
-                options={daysForSelectedMonth}
-                value={day}
-                onValueChange={update => {
-                  this.handleChange('day', update);
-                }}
-                ariaDescribedby={ariaDescribedby}
-              />
-            </div>
-            <div className="usa-datefield usa-form-group usa-form-group-year">
-              <NumberInput
-                errorMessage={isValid ? undefined : ''}
-                label="Year"
-                name={`${this.props.name}Year`}
-                max={maxYear}
-                min={minYear}
-                pattern="[0-9]{4}"
-                field={year}
-                onValueChange={update => {
-                  this.handleChange('year', update);
-                }}
-                ariaDescribedby={ariaDescribedby}
-              />
-            </div>
-          </div>
-        </div>
-      </fieldset>
+      <SimpleDate
+        required={this.props.required}
+        errorMessage={errorMessage}
+        label={this.props.label}
+        date={this.props.date}
+        ariaDescribedby={this.props.ariaDescribedby}
+        minYear={minYear}
+        maxYear={maxYear}
+        onValueChange={this.props.onValueChange}
+      />
     );
   }
 }
+
+export { SimpleDate };
 
 Date.propTypes = {
   /**
