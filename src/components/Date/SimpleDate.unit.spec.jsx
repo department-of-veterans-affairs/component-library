99f75dbbf0d00253b6f21b9c09ccaad719592396
Christopher Valarida <chris.valarida@adhocteam.us>
2021-09-13 15:43:47 -0700

    Remove skin-deep import comment

--- src/components/Date/SimpleDate.unit.spec.jsx
+++ src/components/Date/SimpleDate.unit.spec.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import SkinDeep from 'skin-deep';
 import { expect } from 'chai';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
