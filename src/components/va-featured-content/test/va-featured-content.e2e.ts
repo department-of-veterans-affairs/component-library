ed262d16ab7e83f356bdaec626290029d29b75bf
Holden Hinkle <holdenhinkle@gmail.com>
2021-09-22 15:15:28 -0400

    add basic e2e tests

--- src/components/va-featured-content/test/va-featured-content.e2e.ts
+++ src/components/va-featured-content/test/va-featured-content.e2e.ts
@@ -1,11 +1,20 @@
 import { newE2EPage } from '@stencil/core/testing';
+import { axeCheck } from '../../../testing/test-helpers';
 
 describe('va-featured-content', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<va-featured-content></va-featured-content>');
+    await page.setContent('<va-featured-content />');
 
     const element = await page.find('va-featured-content');
+    // expect(element).toHaveClass('feature');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('passes an axe check', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-featured-content />');
+
+    await axeCheck(page);
   });
 });
