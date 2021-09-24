b6b3699979100c03400a899fb919740a0999eef8
Holden Hinkle <holdenhinkle@gmail.com>
2021-09-24 09:13:44 -0400

    update tests

--- src/components/va-featured-content/test/va-featured-content.e2e.ts
+++ src/components/va-featured-content/test/va-featured-content.e2e.ts
@@ -5,10 +5,10 @@ describe('va-featured-content', () => {
   it('renders', async () => {
     const page = await newE2EPage();
     await page.setContent(`
-      <va-featured-content
-        level=3
-        header="If I'm a Veteran, can I get VR&E benefits and services?"
-      >
+      <va-featured-content>
+        <h3 slot="headline">
+          If I'm a Veteran, can I get VR&E benefits and services?
+        </h3>
         <p>You may be eligible for VR&amp;E benefits and services if you're a Veteran, and you meet all of the requirements listed below.</p>
         <p><strong>All of these must be true. You:</strong></p>
         <ul>
@@ -21,13 +21,14 @@ describe('va-featured-content', () => {
     const element = await page.find('va-featured-content');
 
     expect(element).toEqualHtml(`
-      <va-featured-content level="3" header="If I'm a Veteran, can I get VR&amp;E benefits and services?" class="hydrated">
+      <va-featured-content class="hydrated">
         <mock:shadow-root>
           <div class="feature">
-            <h3>If I'm a Veteran, can I get VR&amp;E benefits and services?</h3>
+            <slot name="headline"></slot>
             <slot></slot>
           </div>
         </mock:shadow-root>
+        <h3 slot="headline">If I'm a Veteran, can I get VR&amp;E benefits and services?</h3>
         <p>You may be eligible for VR&amp;E benefits and services if you're a Veteran, and you meet all of the requirements listed below.</p>
         <p><strong>All of these must be true. You:</strong></p>
         <ul>
@@ -39,99 +40,26 @@ describe('va-featured-content', () => {
     `);
   });
 
-  it('renders when level prop is 3', async () => {
+  it("renders the heading passed to the 'headline' slot", async () => {
+    const heading = "If I'm a Veteran, can I get VR&E benefits and services?";
     const page = await newE2EPage();
     await page.setContent(`
-      <va-featured-content
-        level=3
-        header="If I'm a Veteran, can I get VR&E benefits and services?"
-      >
-      />
+      <va-featured-content>
+        <h3 slot="headline">${heading}</h3>
+      </va-featured-content>
     `);
-    const element = await page.find('va-featured-content >>> h3');
+    const element = await page.find('h3');
     expect(element).not.toBeNull();
-  });
-
-  it('renders when level prop is 4', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <va-featured-content
-        level=4
-        header="If I'm a Veteran, can I get VR&E benefits and services?"
-      />
-    `);
-    const element = await page.find('va-featured-content >>> h4');
-    expect(element).not.toBeNull();
-  });
-
-  it('does not render the header when level prop is 1', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <va-featured-content
-        level=1
-        header="If I'm a Veteran, can I get VR&E benefits and services?"
-      />
-    `);
-    const element = await page.find('va-featured-content >>> h1');
-    expect(element).toBeNull();
-  });
-
-  it('does not render the header when level prop is 2', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <va-featured-content
-        level=2
-        header="If I'm a Veteran, can I get VR&E benefits and services?"
-      />
-    `);
-    const element = await page.find('va-featured-content >>> h2');
-    expect(element).toBeNull();
-  });
-
-  it('does not render the header when level prop is 5', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <va-featured-content
-        level=5
-        header="If I'm a Veteran, can I get VR&E benefits and services?"
-      />
-    `);
-    const element = await page.find('va-featured-content >>> h5');
-    expect(element).toBeNull();
-  });
-
-  it('does not render the header when level prop is 6', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <va-featured-content
-        level=6
-        header="If I'm a Veteran, can I get VR&E benefits and services?"
-      />
-    `);
-    const element = await page.find('va-featured-content >>> h6');
-    expect(element).toBeNull();
-  });
-
-  it('sents the header to the value of the header prop', async () => {
-    const header = "Testing";
-    const page = await newE2EPage();
-    await page.setContent(`
-      <va-featured-content
-        level=3
-        header=${header}
-      />
-    `);
-    const element = await page.find('va-featured-content >>> h3');
-    expect(element).toEqualText(header);
+    expect(element).toEqualText(heading);
   });
 
   it('passes an axe check', async () => {
     const page = await newE2EPage();
     await page.setContent(`
-      <va-featured-content
-        level=3
-        header="If I'm a Veteran, can I get VR&E benefits and services?"
-      >
+      <va-featured-content>
+        <h3 slot="headline">
+          If I'm a Veteran, can I get VR&E benefits and services?
+        </h3>
         <p>You may be eligible for VR&amp;E benefits and services if you're a Veteran, and you meet all of the requirements listed below.</p>
         <p><strong>All of these must be true. You:</strong></p>
         <ul>
