3c4cc702185432fc1521bcfede8a70574a323a3f
Holden Hinkle <holdenhinkle@gmail.com>
2021-09-30 09:15:03 -0400

    add render test for help icon

--- src/components/IconHelp/IconHelp.unit.spec.jsx
+++ src/components/IconHelp/IconHelp.unit.spec.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
 import IconHelp from './IconHelp.jsx';
 import { axeCheck } from '../../helpers/test-helpers';
 
 describe('<IconHelp />', () => {
+  it('Should Render', () => {
+    const wrapper = shallow(
+      <IconHelp
+        cssClass="a-class"
+        color="#000000"
+        id="help"
+        role="img"
+        ariaLabel="Help"
+      />,
+    );
+
+    expect(wrapper.exists('.a-class')).to.equal(true);
+
+    wrapper.unmount();
+  });
+
   it('should pass a basic aXe check', () =>
     axeCheck(
       <IconHelp
