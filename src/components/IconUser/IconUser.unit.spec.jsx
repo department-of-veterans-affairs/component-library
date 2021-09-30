17fa3ad5bc30fb68eaf8393c96c17ee439cac788
Holden Hinkle <holdenhinkle@gmail.com>
2021-09-30 10:26:22 -0400

    add new IconHelp tests to IconUser

--- src/components/IconUser/IconUser.unit.spec.jsx
+++ src/components/IconUser/IconUser.unit.spec.jsx
@@ -1,8 +1,72 @@
 import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+
 import IconUser from './IconUser.jsx';
 import { axeCheck } from '../../helpers/test-helpers';
 
 describe('<IconUser />', () => {
+  it('Should Render', () => {
+    const wrapper = shallow(
+      <IconUser
+        cssClass="a-class"
+        color="#000000"
+        id="user"
+        role="img"
+        ariaLabel="user"
+      />,
+    );
+
+    expect(wrapper.exists('.a-class')).to.equal(true);
+
+    wrapper.unmount();
+  });
+
+  it('passes the ariaLabel prop to IconBase', () => {
+    const ariaLabel = 'user';
+    const wrapper = mount(
+      <IconUser
+        cssClass="a-class"
+        color="#000000"
+        id="user"
+        role="img"
+        ariaLabel={ariaLabel}
+      />,
+    );
+    const IconUserComponent = wrapper.find('IconUser');
+    expect(IconUserComponent.props().ariaLabel).to.equal(ariaLabel);
+
+    const IconBaseComponent = wrapper.find('IconBase');
+    expect(IconBaseComponent.props().ariaLabel).to.equal(ariaLabel);
+
+    wrapper.unmount();
+  });
+
+  it('sets the aria-label attribute when the ariaLabel prop is given', () => {
+    const ariaLabel = 'user';
+
+    const icon = ReactTestUtils.renderIntoDocument(
+      <IconUser
+        cssClass="a-class"
+        color="#000000"
+        id="user"
+        role="img"
+        ariaLabel={ariaLabel}
+      />,
+    );
+    var svg = ReactTestUtils.findRenderedDOMComponentWithTag(icon, 'svg');
+    expect(svg.getAttribute('aria-label')).to.equal(ariaLabel);
+  });
+
+  it('does not include the aria-label attribute when the ariaLabel prop is not given', () => {
+    const icon = ReactTestUtils.renderIntoDocument(
+      <IconUser cssClass="a-class" color="#000000" id="user" role="img" />,
+    );
+    var svg = ReactTestUtils.findRenderedDOMComponentWithTag(icon, 'svg');
+    expect(svg.getAttribute('aria-label')).to.equal(null);
+  });
+
   it('should pass a basic aXe check', () =>
     axeCheck(
       <IconUser
@@ -10,7 +74,7 @@ describe('<IconUser />', () => {
         color="#000000"
         id="help"
         role="img"
-        ariaLabel="User"
+        ariaLabel="user"
       />,
     ));
 });
