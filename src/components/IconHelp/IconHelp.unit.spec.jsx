246b85a59babcededa2007e898c827b927aed198
Holden Hinkle <holdenhinkle@gmail.com>
2021-09-30 09:41:27 -0400

    test that arialLabel prop is passed to IconBase

--- src/components/IconHelp/IconHelp.unit.spec.jsx
+++ src/components/IconHelp/IconHelp.unit.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import IconHelp from './IconHelp.jsx';
 import { axeCheck } from '../../helpers/test-helpers';
@@ -18,6 +18,26 @@ describe('<IconHelp />', () => {
     );
 
     expect(wrapper.exists('.a-class')).to.equal(true);
+
+    wrapper.unmount();
+  });
+
+  it('sets aria-label when a valid value for the ariaLabel prop is provided', () => {
+    const ariaLabel = 'Help';
+    const wrapper = mount(
+      <IconHelp
+        cssClass="a-class"
+        color="#000000"
+        id="help"
+        role="img"
+        ariaLabel={ariaLabel}
+      />,
+    );
+    const IconHelpComponent = wrapper.find('IconHelp');
+    expect(IconHelpComponent.props().ariaLabel).to.equal(ariaLabel);
+
+    const IconBaseComponent = wrapper.find('IconBase');
+    expect(IconBaseComponent.props().ariaLabel).to.equal(ariaLabel);
 
     wrapper.unmount();
   });
