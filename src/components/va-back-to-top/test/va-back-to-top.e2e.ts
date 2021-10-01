b5ec0a4415414181f7a1c785d857dccf0d668d86
Brooks Johnson <brooks.johnson@pm.me>
2021-10-01 16:59:40 -0700

    Rework all the tests
    Now they all share the same page structure, and the scrolling heights
    have been given names to indicate the reason for the distance

--- src/components/va-back-to-top/test/va-back-to-top.e2e.ts
+++ src/components/va-back-to-top/test/va-back-to-top.e2e.ts
@@ -1,81 +1,34 @@
 import { newE2EPage } from '@stencil/core/testing';
 import { axeCheck } from '../../../testing/test-helpers';
 
-const pageContent = `
-      <h1>The top</h1>
-      <p>Lorem ipsum</p>
-      <p>Some more</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>Some more</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>Some more</p>
-      <p>Some more</p>
-      `;
+const pageStyles = `
+:root { --reveal-breakpoint: 200px}
+div {
+  margin: 0;
+  padding: 0;
+  height: 1000px;
+}
+footer {
+  height: 500px;
+}
+`;
+
+const pageSetup = async () => {
+  const page = await newE2EPage();
+  await page.setViewport({ width: 800, height: 200 });
+  await page.setContent(`
+    <main>
+    <h1>The top</h1>
+    <div>Placeholder</div>
+    <va-back-to-top></va-back-to-top>
+    </main>
+    <footer>The footer</footer>
+    `);
+
+  await page.addStyleTag({ content: pageStyles });
+
+  return page;
+};
 
 describe('va-back-to-top', () => {
   it('renders', async () => {
@@ -87,82 +40,68 @@ describe('va-back-to-top', () => {
   });
 
   it('reveals when the viewport is below the reveal pixel', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-    <main>
-    ${pageContent}
-    <va-back-to-top></va-back-to-top>
-    </main>
-    `);
+    const page = await pageSetup();
     const button = await page.find('va-back-to-top >>> button');
+    const notQuitePastRevealPoint = 199;
 
     // Button shouldn't be revealed initially
     expect(button).not.toHaveClass('reveal');
 
-    await page.mouse.wheel({ deltaY: 100 });
+    // Scroll until the `<span class="reveal-point">` is just at
+    // the top of the viewport
+    await page.mouse.wheel({ deltaY: notQuitePastRevealPoint });
     await page.waitForChanges();
 
     // Button _still_ shouldn't be revealed after a bit of scrolling
     expect(button).not.toHaveClass('reveal');
 
-    await page.mouse.wheel({ deltaY: 1000 });
+    // Scroll a few pixels more to get past the `--reveal-breakpoint`
+    await page.mouse.wheel({ deltaY: 10 });
     await page.waitForChanges();
 
-    // We've scrolled a lot - button should be revealed
+    // Button should be revealed with `.reveal-point` above the viewport
     expect(button).toHaveClass('reveal');
   });
 
   it('docks when the "dock" is scrolled into view', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-    <main>
-    ${pageContent}
-    <va-back-to-top></va-back-to-top>
-    <footer style="height: 100px">The footer</footer>
-    </main>
-    `);
+    const page = await pageSetup();
     const wrapper = await page.find('va-back-to-top >>> div');
+    const pastPlaceholder = 1000;
 
     expect(wrapper).not.toHaveClass('docked');
 
-    await page.mouse.wheel({ deltaY: 1400 });
+    // 1000px scrolled + 200px viewport height means that the dock is in the viewport
+    await page.mouse.wheel({ deltaY: pastPlaceholder });
     await page.waitForChanges();
+
+    expect(await wrapper.isIntersectingViewport()).toEqual(true);
 
     // We've scrolled a lot - button should be docked
     expect(wrapper).toHaveClass('docked');
   });
 
   it('stays docked even with really long footers', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-    <main>
-    ${pageContent}
-    <va-back-to-top></va-back-to-top>
-    <footer style="height: 2400px">The footer</footer>
-    </main>
-    `);
+    const page = await pageSetup();
     const wrapper = await page.find('va-back-to-top >>> div');
+    const pastDock = 1300; // Placeholder section is 1000px tall & viewport is 200
 
-    await page.mouse.wheel({ deltaY: 3000 });
+    // Scroll past the dock
+    await page.mouse.wheel({ deltaY: pastDock });
     await page.waitForChanges();
 
-    // After scrolling this much, the dock is well above the viewport
+    // After scrolling this much, the dock is above the viewport
+    expect(await wrapper.isIntersectingViewport()).toEqual(false);
     expect(wrapper).toHaveClass('docked');
   });
 
   it('goes to top on click', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-    <main>
-    ${pageContent}
-    <va-back-to-top></va-back-to-top>
-    </main>
-    `);
+    const page = await pageSetup();
     const button = await page.find('va-back-to-top >>> button');
     const topHeading = await page.find('h1');
+    const pastRevealPoint = 300;
 
-    // Make the button visible
-    await page.mouse.wheel({ deltaY: 1000 });
+    // Scroll far enough to make the button visible
+    await page.mouse.wheel({ deltaY: pastRevealPoint });
     await page.waitForChanges();
 
     expect(await topHeading.isIntersectingViewport()).toEqual(false);
@@ -174,42 +113,25 @@ describe('va-back-to-top', () => {
   });
 
   it('passes an axe check', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-    <main>
-    ${pageContent}
-    <va-back-to-top></va-back-to-top>
-    </main>
-    `);
+    const page = await pageSetup();
 
     await axeCheck(page);
   });
 
   it('passes an axe check when revealed', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-    <main>
-    ${pageContent}
-    <va-back-to-top></va-back-to-top>
-    </main>
-    `);
+    const page = await pageSetup();
+    const button = await page.find('va-back-to-top >>> button');
 
-    await page.mouse.wheel({ deltaY: 1000 });
+    await page.mouse.wheel({ deltaY: 300 });
     await page.waitForChanges();
 
+    expect(button).toHaveClass('reveal');
     // The color contrast error appears to be a false negative
     await axeCheck(page, ['color-contrast']);
   });
 
   it('passes an axe check when docked', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-    <main>
-    ${pageContent}
-    <va-back-to-top></va-back-to-top>
-    <footer style="height: 100px">The footer</footer>
-    </main>
-    `);
+    const page = await pageSetup();
 
     await page.mouse.wheel({ deltaY: 1400 });
     await page.waitForChanges();
