bfbf20fe346137a108c63962433615723e4bf881
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-10-01 13:35:10 -0500

    Create ProcessList component (#191)
    * WIP
    
    * Create ProcessList component
    
    * Linting fix
    
    * Use React.Children

---
+++ src/components/ProcessList/ProcessList.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { toWords } from 'number-to-words';
+
+export default class ProcessList extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const children = React.Children.toArray(this.props.children);
+
+    return (
+      <div className="process schemaform-process">
+        <ol role="list">
+          {children.map((child, index) => (
+            <li
+              className={`process-step list-${toWords(index + 1)}`}
+              key={index}
+            >
+              {child}
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
