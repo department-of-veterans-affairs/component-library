0a9a9238fec59ab85a4aee3c80f93c44d42a96e8
Brooks Johnson <brooks.johnson@pm.me>
2021-10-06 15:30:27 -0700

    Clean up the story

--- stories/va-back-to-top.stories.jsx
+++ stories/va-back-to-top.stories.jsx
@@ -10,83 +10,32 @@ export default {
 const Template = () => {
   return (
     <main className="vads-l-col--12 medium-screen:vads-l-col--8">
-      <p>Lorem ipsum</p>
-      <p>Some more</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>Some more</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>Some more</p>
-      <p>Some more</p>
+      <h1>The top</h1>
+      {[...Array(10)].map(i => (
+        <div
+          key={i}
+          style={{
+            height: '300px',
+            background: 'whitesmoke',
+            margin: '2em 0',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          Scroll down
+        </div>
+      ))}
       <va-back-to-top />
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
-      <p>Some more</p>
+      <footer
+        style={{
+          height: '500px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        The footer
+      </footer>
     </main>
   );
 };
