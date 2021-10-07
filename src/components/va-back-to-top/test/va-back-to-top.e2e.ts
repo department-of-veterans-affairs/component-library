ae6be800ebea3f05521a178ea15bf040cde4ca85
Brooks Johnson <brooks.johnson@pm.me>
2021-10-07 16:27:40 -0700

    Update failing tests

--- src/components/va-back-to-top/test/va-back-to-top.e2e.ts
+++ src/components/va-back-to-top/test/va-back-to-top.e2e.ts
@@ -41,11 +41,11 @@ describe('va-back-to-top', () => {
 
   it('reveals when the viewport is below the reveal pixel', async () => {
     const page = await pageSetup();
-    const button = await page.find('va-back-to-top >>> button');
+    const wrapper = await page.find('va-back-to-top >>> div');
     const notQuitePastRevealPoint = 199;
 
     // Button shouldn't be revealed initially
-    expect(button).not.toHaveClass('reveal');
+    expect(wrapper).not.toHaveClass('reveal');
 
     // Scroll until the `<span class="reveal-point">` is just at
     // the top of the viewport
@@ -53,14 +53,14 @@ describe('va-back-to-top', () => {
     await page.waitForChanges();
 
     // Button _still_ shouldn't be revealed after a bit of scrolling
-    expect(button).not.toHaveClass('reveal');
+    expect(wrapper).not.toHaveClass('reveal');
 
     // Scroll a few pixels more to get past the `--reveal-breakpoint`
     await page.mouse.wheel({ deltaY: 10 });
     await page.waitForChanges();
 
     // Button should be revealed with `.reveal-point` above the viewport
-    expect(button).toHaveClass('reveal');
+    expect(wrapper).toHaveClass('reveal');
   });
 
   it('docks when the "dock" is scrolled into view', async () => {
@@ -127,13 +127,13 @@ describe('va-back-to-top', () => {
 
   it('passes an axe check when revealed', async () => {
     const page = await pageSetup();
-    const button = await page.find('va-back-to-top >>> button');
+    const wrapper = await page.find('va-back-to-top >>> div');
     const pastRevealPoint = 300;
 
     await page.mouse.wheel({ deltaY: pastRevealPoint });
     await page.waitForChanges();
 
-    expect(button).toHaveClass('reveal');
+    expect(wrapper).toHaveClass('reveal');
     // The color contrast error appears to be a false negative
     await axeCheck(page, ['color-contrast']);
   });
