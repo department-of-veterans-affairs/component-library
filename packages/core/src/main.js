fd65eaeca93e3e78dcc21c7f36baa910f4230089
Brooks Johnson <brooks.johnson@pm.me>
2021-10-19 15:33:26 -0700

    Simplify the JSON import/export

--- packages/core/src/main.js
+++ packages/core/src/main.js
@@ -1,7 +1,6 @@
 import '@department-of-veterans-affairs/web-components/dist/component-library/component-library.css';
-import webComponentDocs from '@department-of-veterans-affairs/web-components/component-docs';
 
 export * from '@department-of-veterans-affairs/react-components';
 export * from '@department-of-veterans-affairs/web-components';
 export * from '@department-of-veterans-affairs/web-components/loader';
-export {webComponentDocs};
+export {default as webComponentDocs} from '@department-of-veterans-affairs/web-components/component-docs';
