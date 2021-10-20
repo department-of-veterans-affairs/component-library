e56b0a13f2bf9a54018745f2a292c03d4b6cc75e
Brooks Johnson <brooks.johnson@pm.me>
2021-10-19 17:30:57 -0700

    Prettify the core webpack config

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -1,11 +1,11 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: ["./src/main.js"],
+  entry: ['./src/main.js'],
   output: {
-    path: __dirname + "/dist",
-    filename: "app.bundle.js",
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js',
   },
   module: {
     rules: [
@@ -13,33 +13,31 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader, // instead of style-loader
-          'css-loader'
-        ]
-      }
+          'css-loader',
+        ],
+      },
     ],
   },
   resolve: {
     alias: {
       '@department-of-veterans-affairs/web-components/react-bindings':
         path.resolve(__dirname, '../web-components/'),
-      "@department-of-veterans-affairs/web-components": path.resolve(
+      '@department-of-veterans-affairs/web-components': path.resolve(
         __dirname,
-        "../web-components/"
+        '../web-components/',
       ),
-      "@department-of-veterans-affairs/react-components": path.resolve(
+      '@department-of-veterans-affairs/react-components': path.resolve(
         __dirname,
-        "../react-components/"
+        '../react-components/',
       ),
     },
   },
-  plugins: [
-    new MiniCssExtractPlugin(),
-  ],
+  plugins: [new MiniCssExtractPlugin()],
 };
