27c4feeb696bc98ea23fd3001b5c87f0cb0042b9
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2021-11-10 15:48:50 -0800

    docs: Add story for va-featured-content (#217)
    * Add story for va-featured-content
    
    * Remove the story for the `feature` class
    
    We want to direct people to use the component instead of the CSS class

---
+++ packages/storybook/stories/va-featured-content.stories.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { getWebComponentDocs, propStructure } from './wc-helpers';
+
+const featuredContentDocs = getWebComponentDocs('va-featured-content');
+
+export default {
+  title: 'Components/va-featured-content',
+};
+
+const Template = () => (
+  <va-featured-content>
+    <h3 slot="headline">
+      If I'm a Veteran, can I get VR&E benefits and services?
+    </h3>
+    <p>
+      You may be eligible for VR&amp;E benefits and services if you're a
+      Veteran, and you meet all of the requirements listed below.
+    </p>
+    <p>
+      <strong>All of these must be true. You:</strong>
+    </p>
+    <ul>
+      <li>
+        Didn't receive a dishonorable discharge, <strong>and</strong>
+      </li>
+      <li>
+        Have a service-connected disability rating of at least 10% from VA,
+        <strong>and</strong>
+      </li>
+      <li>
+        <a href="#">Apply for VR&amp;E services</a>
+      </li>
+    </ul>
+  </va-featured-content>
+);
+
+export const Default = Template.bind({});
+Default.argTypes = propStructure(featuredContentDocs);
