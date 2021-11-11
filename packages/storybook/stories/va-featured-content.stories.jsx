e660785b325692f24c2138a92fb5003fc75ecf94
Katryn Bowman <katy.bowman@adhocteam.us>
2021-11-11 13:21:17 -0500

    add space to featured content text

--- packages/storybook/stories/va-featured-content.stories.jsx
+++ packages/storybook/stories/va-featured-content.stories.jsx
@@ -25,7 +25,7 @@ const Template = () => (
         Didn't receive a dishonorable discharge, <strong>and</strong>
       </li>
       <li>
-        Have a service-connected disability rating of at least 10% from VA,
+        Have a service-connected disability rating of at least 10% from VA, 
         <strong>and</strong>
       </li>
       <li>
