b6702700f62c7f77e4f9c0c50897b3a102721521
Brooks Johnson <brooks.johnson@pm.me>
2021-11-11 15:48:58 -0800

    Add keyboard control & other things
    Where "other things" is copying over some of the markup structure from
    the React component.

--- packages/web-components/src/components/va-additional-info/va-additional-info.tsx
+++ packages/web-components/src/components/va-additional-info/va-additional-info.tsx
@@ -18,15 +18,25 @@ export class VaAdditionalInfo {
     this.open = !this.open;
   }
 
+  @Listen('keydown', { capture: true })
+  handleKeydown(event): void {
+    if (event.key === ' ') {
+      event.preventDefault();
+      this.toggleOpen();
+    }
+  }
+
   render() {
     return (
       <Host aria-expanded={this.open ? 'true' : 'false'}>
-        <span class="additional-info-title">
-          {this.trigger}
-          <i class="fa-angle-down" role="presentation" />
-        </span>
+        <a role="button" aria-controls="info" tabIndex={0}>
+          <span class="additional-info-title">
+            {this.trigger}
+            <i class="fa-angle-down" role="presentation" />
+          </span>
+        </a>
 
-        <div class={this.open ? 'open' : 'closed'}>
+        <div id="info" class={this.open ? 'open' : 'closed'}>
           <slot></slot>
         </div>
       </Host>
