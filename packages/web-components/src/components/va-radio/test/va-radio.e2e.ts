21a5b3635e6302dbef3c8c59cd388d7a0804a15c
Andres Rivera <andres.rivera.toro@gmail.com>
2021-11-15 12:54:57 -0500

    Add keyboard event tests

--- packages/web-components/src/components/va-radio/test/va-radio.e2e.ts
+++ packages/web-components/src/components/va-radio/test/va-radio.e2e.ts
@@ -126,4 +126,124 @@ describe('va-radio', () => {
 
     expect(changeSpy).toHaveReceivedEventDetail({ value: 'one' });
   });
+
+  it('checks option when using the space key', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `
+      <va-radio>
+        <va-radio-option label="Option 1" value="1"></va-radio-option>
+      </va-radio>
+    `,
+    );
+    const options = await page.findAll('va-radio-option');
+
+    await options[0].press('Space');
+
+    expect(await options[0].getProperty('checked')).toBeTruthy();
+  });
+
+  it('checks next option when using the left arrow key', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `
+      <va-radio>
+        <va-radio-option checked label="Option 1" value="1"></va-radio-option>
+        <va-radio-option label="Option 2" value="2"></va-radio-option>
+        <va-radio-option label="Option 3" value="3"></va-radio-option>
+      </va-radio>
+    `,
+    );
+    const options = await page.findAll('va-radio-option');
+
+    await options[0].press('ArrowLeft');
+
+    expect(await options[2].getProperty('checked')).toBeTruthy();
+  });
+
+  it('checks next option when using the right arrow key', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `
+      <va-radio>
+        <va-radio-option checked label="Option 1" value="1"></va-radio-option>
+        <va-radio-option label="Option 2" value="2"></va-radio-option>
+        <va-radio-option label="Option 3" value="3"></va-radio-option>
+      </va-radio>
+    `,
+    );
+    const options = await page.findAll('va-radio-option');
+
+    await options[0].press('ArrowRight');
+
+    expect(await options[1].getProperty('checked')).toBeTruthy();
+  });
+
+  it('checks next option when using the down arrow key', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `
+      <va-radio>
+        <va-radio-option checked label="Option 1" value="1"></va-radio-option>
+        <va-radio-option label="Option 2" value="2"></va-radio-option>
+      </va-radio>
+    `,
+    );
+    const options = await page.findAll('va-radio-option');
+
+    await options[0].press('ArrowDown');
+
+    expect(await options[1].getProperty('checked')).toBeTruthy();
+  });
+
+  it('checks next option when using the up arrow key', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `
+      <va-radio>
+        <va-radio-option checked label="Option 1" value="1"></va-radio-option>
+        <va-radio-option label="Option 2" value="2"></va-radio-option>
+      </va-radio>
+    `,
+    );
+    const options = await page.findAll('va-radio-option');
+
+    await options[0].press('ArrowUp');
+
+    expect(await options[1].getProperty('checked')).toBeTruthy();
+  });
+
+  it('unchecks current option when using the down arrow key', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `
+      <va-radio>
+        <va-radio-option checked label="Option 1" value="1"></va-radio-option>
+        <va-radio-option label="Option 2" value="2"></va-radio-option>
+      </va-radio>
+    `,
+    );
+    const options = await page.findAll('va-radio-option');
+
+    await options[0].press('ArrowDown');
+
+    expect(await options[0].getProperty('checked')).toBeFalsy();
+  });
+
+  it('unchecks current option when new option is selected using the up arrow key', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `
+      <va-radio>
+        <va-radio-option label="Option 1" value="1"></va-radio-option>
+        <va-radio-option checked label="Option 2" value="2"></va-radio-option>
+      </va-radio>
+    `,
+    );
+    const options = await page.findAll('va-radio-option');
+
+    await options[1].press('ArrowUp');
+
+    expect(await options[1].getProperty('checked')).toBeFalsy();
+  });
 });
