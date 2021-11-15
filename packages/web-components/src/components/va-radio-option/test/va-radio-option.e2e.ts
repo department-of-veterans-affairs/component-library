ebc460e8b57729d63047e830d23b7117edaf4826
Andres Rivera <andres.rivera.toro@gmail.com>
2021-11-15 12:58:26 -0500

    Update radio option test element

--- packages/web-components/src/components/va-radio-option/test/va-radio-option.e2e.ts
+++ packages/web-components/src/components/va-radio-option/test/va-radio-option.e2e.ts
@@ -20,23 +20,6 @@ describe('va-radio-option', () => {
     expect(await inputEl.getProperty('checked')).toBeTruthy();
   });
 
-  it('adds aria-describedby to input when checked', async () => {
-    const page = await newE2EPage();
-    await page.setContent(
-      '<va-radio-option aria-describedby="test" label="A label" value="something" />',
-    );
-
-    const element = await page.find('va-radio-option');
-    const inputEl = await page.find('va-radio-option >>> input');
-
-    expect(inputEl.getAttribute('aria-describedby')).toEqual(null);
-
-    element.setProperty('checked', true);
-    await page.waitForChanges();
-
-    expect(inputEl.getAttribute('aria-describedby')).toEqual('test');
-  });
-
   it('fires event for parent when changed', async () => {
     const page = await newE2EPage();
     await page.setContent(
@@ -44,7 +27,7 @@ describe('va-radio-option', () => {
     );
 
     const changeSpy = await page.spyOnEvent('radioOptionSelected');
-    const inputEl = await page.find('va-radio-option >>> input');
+    const inputEl = await page.find('va-radio-option');
     await inputEl.click();
 
     expect(changeSpy).toHaveReceivedEvent();
