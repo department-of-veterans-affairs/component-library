64a0ed2bde2ac11e461543cd70b5d563be36fd14
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2021-11-16 11:54:24 -0800

    Add loading indicator web component (#207)
    * Initial draft for loading indicator component
    
    Still need to add styling, focus behavior, & analytics
    
    * Add story for loading indicator component
    
    * Copy over CSS from formation
    
    * Add focus behavior
    
    * Add axeCheck test
    
    * Change indicator size to 4rem
    
    * Add functionality for analytics event
    
    There's a bit of awkwardness here. I'm not doing the traditional
    `this.componentLibraryAnalytics.emit(...)`, because we want to fire the
    event once the loading indicator is gone. Doing that in
    `disconnectedCallback` is too late - the node that would fire the event
    has already been removed from the DOM. Using the mutation observer
    allows us to fire the event from the parent instead, which isn't ideal.
    
    * Cast to an appropriate type
    
    * Update story for analytics event
    
    * Use mutation record to determine removal
    
    * Ensure the mutation observer callback runs
    
    * Update the base test
    
    * Clean up stories a bit
    
    * Tweak the size of the spinner
    
    * Remove commented code
    
    * Mark old LoadingIndicator as deprecated
    
    * Set tabindex to -1
    
    This is from staging review feedback:
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/32577
    
    * Slow spinner down & make it smaller
    
    In response to staging review feedback:
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/32583
    
    * Add aria-live="polite"
    
    Staging review feedback:
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/32813
    
    * Fix test
    
    * Bump versions to prepare for release & publish

---
+++ packages/web-components/src/components/va-loading-indicator/test/va-loading-indicator.e2e.ts
@@ -0,0 +1,102 @@
+import { newE2EPage } from '@stencil/core/testing';
+import { axeCheck } from '../../../testing/test-helpers';
+
+describe('va-loading-indicator', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-loading-indicator></va-loading-indicator>');
+
+    const element = await page.find('va-loading-indicator');
+
+    expect(element).toEqualHtml(`
+      <va-loading-indicator aria-live="polite" class="hydrated">
+        <mock:shadow-root>
+          <div aria-label="Loading" class="loading-indicator" role="progressbar" tabindex="-1">
+          </div>
+        </mock:shadow-root>
+      </va-loading-indicator>
+    `);
+  });
+
+  it('should not focus if setFocus is not set', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-loading-indicator></va-loading-indicator>');
+
+    const focusedHandle = await page.evaluateHandle(
+      () => document.activeElement,
+    );
+    const focusedTag = await focusedHandle.evaluate((domElement: Element) => {
+      return domElement.tagName.toLowerCase();
+    });
+
+    expect(focusedTag).toEqual('body');
+  });
+
+  it('should focus if setFocus is set', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-loading-indicator set-focus="true"</va-loading-indicator>',
+    );
+
+    const focusedHandle = await page.evaluateHandle(
+      () => document.activeElement,
+    );
+    const focusedTag = await focusedHandle.evaluate((domElement: Element) => {
+      return domElement.tagName.toLowerCase();
+    });
+
+    expect(focusedTag).toEqual('va-loading-indicator');
+  });
+
+  it('passes an axe check', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-loading-indicator message="Loading" label="aria label here"</va-loading-indicator>',
+    );
+
+    await axeCheck(page);
+  });
+
+  it('fires an analytics event when enableAnalytics is true', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <body id="test">
+        <va-loading-indicator message="Loading" enable-analytics></va-loading-indicator>
+        </body>
+        `);
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+
+    await page.evaluate(() => {
+      let node = document.querySelector('va-loading-indicator');
+      node.parentNode.removeChild(node);
+    });
+
+    const eventDetails = analyticsSpy.events[0].detail;
+
+    expect(analyticsSpy).toHaveReceivedEventTimes(1);
+    expect(eventDetails.action).toEqual('displayed');
+    expect(eventDetails.componentName).toEqual('va-loading-indicator');
+    expect(eventDetails.details.message).toEqual('Loading');
+    expect(typeof eventDetails.details.displayTime).toEqual('number');
+  });
+
+  it('does not fire an analytics event when enableAnalytics is not truthy', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <body id="test">
+        <va-loading-indicator message="Loading"></va-loading-indicator>
+      </body>
+    `);
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+
+    await page.evaluate(() => {
+      let node = document.querySelector('va-loading-indicator');
+      node.parentNode.removeChild(node);
+    });
+    await page.waitForChanges();
+
+    expect(analyticsSpy).toHaveReceivedEventTimes(0);
+  });
+});
