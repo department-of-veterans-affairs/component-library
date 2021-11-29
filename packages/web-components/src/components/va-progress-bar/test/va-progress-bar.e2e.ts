8bb082d15fa4d524692919ae43157c54e8a0dcb6
Andres Rivera <andres.rivera.toro@gmail.com>
2021-11-29 09:35:05 -0500

    test: update render test to check against HTML

--- packages/web-components/src/components/va-progress-bar/test/va-progress-bar.e2e.ts
+++ packages/web-components/src/components/va-progress-bar/test/va-progress-bar.e2e.ts
@@ -4,27 +4,35 @@ import { axeCheck } from '../../../testing/test-helpers';
 describe('va-progress-bar', () => {
   it('renders', async () => {
     const page = await newE2EPage({
-      html: '<va-progress-bar percent="35"></va-progress-bar>',
+      html: '<va-progress-bar label="Progress!" percent="35"></va-progress-bar>',
     });
     const element = await page.find('va-progress-bar');
-    expect(element).toHaveClass('hydrated');
+    expect(element).toEqualHtml(`
+      <va-progress-bar class="hydrated" label="Progress!" percent="35">
+        <mock:shadow-root>
+          <div
+            aria-label="Progress!"
+            aria-valuemax="100"
+            aria-valuemin="0"
+            aria-valuenow="35"
+            class="progress-bar"
+            tabindex="0"
+            role="progressbar"
+          >
+            <div
+              class="progress-bar-inner"
+              style="width: 35%;"
+            />
+          </div>
+        </mock:shadow-root>
+      </va-progress-bar>
+    `);
   });
 
   it('passes an axe check', async () => {
     const page = await newE2EPage();
     await page.setContent('<va-progress-bar percent="35"></va-progress-bar>');
     await axeCheck(page);
-  });
-
-  it(`doesn't fire an analytics event when enable-analytics is not set`, async () => {
-    const page = await newE2EPage();
-    await page.setContent('<va-progress-bar percent="80"></va-progress-bar>');
-    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
-    await page.$eval('va-progress-bar', (element: any) => {
-      element.percent = 100;
-    });
-    await page.waitForChanges();
-    expect(analyticsSpy).not.toHaveReceivedEvent();
   });
 
   it('fires an analytics event when enable-analytics is set and percent is set to 0', async () => {
@@ -52,5 +60,16 @@ describe('va-progress-bar', () => {
     );
     await page.waitForChanges();
     expect(analyticsSpy).toHaveReceivedEvent();
+  });
+
+  it("doesn't fire an analytics event when enable-analytics is false", async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-progress-bar percent="80"></va-progress-bar>');
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+    await page.$eval('va-progress-bar', (element: any) => {
+      element.percent = 100;
+    });
+    await page.waitForChanges();
+    expect(analyticsSpy).not.toHaveReceivedEvent();
   });
 });
