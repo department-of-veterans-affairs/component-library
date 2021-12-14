b0ba2e86cdf95f96d2b75eb180aa921018f28e22
Andres Rivera <andres.rivera.toro@gmail.com>
2021-12-14 13:58:44 -0500

    refactor: remove creation of new Date objects

--- packages/react-components/src/helpers/format-date.js
+++ packages/react-components/src/helpers/format-date.js
@@ -1,53 +1,45 @@
-export const convertToDate = date => {
-  if (!date || !(date instanceof Date || typeof date === 'number')) return;
-  return new Date(date);
-};
-
 export const isDateAfter = (date1, date2) => {
-  const dateObj1 = convertToDate(date1);
-  const dateObj2 = convertToDate(date2);
-  if (!dateObj1 || !dateObj2) return;
+  if (!date1 || !(date1 instanceof Date) || !date2 || !(date2 instanceof Date))
+    return;
 
-  return dateObj1.getTime() > dateObj2.getTime();
+  return date1.getTime() > date2.getTime();
 };
 
 export const isDateBefore = (date1, date2) => {
-  const dateObj1 = convertToDate(date1);
-  const dateObj2 = convertToDate(date2);
-  if (!dateObj1 || !dateObj2) return;
+  if (!date1 || !(date1 instanceof Date) || !date2 || !(date2 instanceof Date))
+    return;
 
-  return dateObj1.getTime() < dateObj2.getTime();
+  return date1.getTime() < date2.getTime();
 };
 
 export const isDateSameDay = (date1, date2) => {
-  const dateObj1 = convertToDate(date1);
-  const dateObj2 = convertToDate(date2);
-  if (!dateObj1 || !dateObj2) return;
+  if (!date1 || !(date1 instanceof Date) || !date2 || !(date2 instanceof Date))
+    return;
 
-  dateObj1.setHours(0, 0, 0, 0);
-  dateObj2.setHours(0, 0, 0, 0);
-
-  return dateObj1.getTime() === dateObj2.getTime();
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDay() === date2.getDay()
+  );
 };
 
 export const formatDate = (date, pattern, timezone) => {
-  const dateObj = convertToDate(date);
-  if (!dateObj) return;
+  if (!date || !(date instanceof Date)) return;
 
   const timeZone = timezone || 'America/New_York';
 
   // Example: Tuesday, June 9, 2020
   if (pattern === 'dateFull') {
-    return dateObj.toLocaleString('en-US', { dateStyle: 'full', timeZone });
+    return date.toLocaleString('en-US', { dateStyle: 'full', timeZone });
   }
 
   // Example: 10:00 AM
   if (pattern === 'timeShort') {
-    return dateObj.toLocaleString('en-US', { timeStyle: 'short', timeZone });
+    return date.toLocaleString('en-US', { timeStyle: 'short', timeZone });
   }
 
   // Example: Tuesday, June 9, 2020 at 10:00 AM
-  return dateObj.toLocaleString('en-US', {
+  return date.toLocaleString('en-US', {
     dateStyle: 'full',
     timeStyle: 'short',
     timeZone,
