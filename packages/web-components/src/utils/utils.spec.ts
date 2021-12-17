9ee0cd8e77ba6c13fa3025f34a7e3ddec5a25bcc
Preston Davies <pdavies88@gmail.com>
2021-12-16 19:50:15 -0500

    chore: update getSlottedNodes utils test suite

--- packages/web-components/src/utils/utils.spec.ts
+++ packages/web-components/src/utils/utils.spec.ts
@@ -20,16 +20,8 @@ describe('format', () => {
   });
 });
 
-describe('getSlottedNodes', () => {
+describe('getSlottedNodes()', () => {
   it('returns an array of nodes from a custom element in the shadow dom', async () => {
-    // Light DOM
-    const documentHTML =
-      '<!doctype html><html><body>' +
-      '<custom-element>' +
-      '<span slot="slot-test">Finally</span>' +
-      '</custom-element>' +
-      '</body></html>';
-    document.body.innerHTML = documentHTML;
     // Shadow DOM Custom Element
     class CustomElement extends window.HTMLElement {
       constructor() {
@@ -46,15 +38,7 @@ describe('getSlottedNodes', () => {
     expect(await slottedNodes[2].nodeName).toEqual('P');
   });
 
-  it('it filters nodes from a custom element in the shadow dom based on nodeName', async () => {
-    // Light DOM
-    const documentHTML =
-      '<!doctype html><html><body>' +
-      '<custom-element>' +
-      '<span slot="slot-test">Finally</span>' +
-      '</custom-element>' +
-      '</body></html>';
-    document.body.innerHTML = documentHTML;
+  it('filters nodes from a custom element in the shadow dom based on nodeName', async () => {
     // Shadow DOM Custom Element
     class CustomElement extends window.HTMLElement {
       constructor() {
@@ -71,27 +55,19 @@ describe('getSlottedNodes', () => {
     expect(await slottedNodes[0].nodeName).toEqual('P');
   });
 
-  it('it gathers nodes in slot', async () => {
-    // Light DOM
-    const documentHTML =
-      '<!doctype html><html><body>' +
-      '<custom-element>' +
-      '<h3 slot="headline">Test Headline</h3>' +
-      '</custom-element>' +
-      '</body></html>';
-    document.body.innerHTML = documentHTML;
+  it('gathers slot nodes in the shadow DOM if slot is used', async () => {
     // Shadow DOM Custom Element
     class CustomElement extends window.HTMLElement {
       constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `<slot name="headline">Updated Test Headline</slot>`;
+        this.shadowRoot.innerHTML = `<div>ABC</div><slot name="headline">Updated Test Headline</slot><p>GHI</p>`;
       }
     }
 
     var mockObject = {
       assignedNodes: () => {
-        return [1, 2, 3];
+        return ['h2', 'a', 'a'];
       },
     };
     window.customElements.define('custom-element', CustomElement);
@@ -103,20 +79,47 @@ describe('getSlottedNodes', () => {
     jest
       .spyOn(defaultElement.shadowRoot, 'querySelector')
       .mockImplementation(selector => {
-        console.log('SELECTOR', selector);
         if (selector === 'slot') {
           return mockObject;
         }
         return defElement_shadowRoot_querySelector(selector);
       });
 
-    console.log('SHADOW', defaultElement.shadowRoot.childNodes);
-    console.log(
-      'LIGHT',
-      document.querySelector('custom-element').shadowRoot.childNodes,
-    );
     const slottedNodes = getSlottedNodes(defaultElement, null);
-    console.log('FUNCTION RESULTS', slottedNodes);
-    expect(await slottedNodes[0].nodeName).toEqual('SLOT');
+    expect(await slottedNodes).toEqual(['h2', 'a', 'a']);
+  });
+
+  it('gathers specific slot nodes in the shadow DOM if slot is used and nodeName is specified', async () => {
+    // Shadow DOM Custom Element
+    class CustomElement extends window.HTMLElement {
+      constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `<div>ABC</div><slot name="headline">Updated Test Headline</slot><p>GHI</p>`;
+      }
+    }
+
+    var mockObject = {
+      assignedNodes: () => {
+        return [{nodeName: 'DIV'}, {nodeName: 'A'}, {nodeName: 'P'}];
+      },
+    };
+    window.customElements.define('custom-element', CustomElement);
+
+    const defaultElement = document.createElement('custom-element');
+
+    const defElement_shadowRoot_querySelector =
+      defaultElement.shadowRoot.querySelector;
+    jest
+      .spyOn(defaultElement.shadowRoot, 'querySelector')
+      .mockImplementation(selector => {
+        if (selector === 'slot') {
+          return mockObject;
+        }
+        return defElement_shadowRoot_querySelector(selector);
+      });
+
+    const slottedNodes = getSlottedNodes(defaultElement, 'a');
+    expect(await slottedNodes).toEqual([ { nodeName: 'A' } ]);
   });
 });
