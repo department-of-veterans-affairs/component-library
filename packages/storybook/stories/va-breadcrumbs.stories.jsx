a3bc58b7cd5a05c59d00377b861972d7e8b25986
Andres Rivera <andres.rivera.toro@gmail.com>
2021-12-22 14:55:52 -0500

    chore: remove mobile first story and unused props

--- packages/storybook/stories/va-breadcrumbs.stories.jsx
+++ packages/storybook/stories/va-breadcrumbs.stories.jsx
@@ -7,20 +7,8 @@ export default {
   title: 'Components/va-breadcrumbs',
 };
 
-const Template = ({
-  label,
-  'disable-analytics': disableAnalytics,
-  'list-id': listId,
-  'mobile-first-prop': mobileFirstProp,
-  'nav-id': navId,
-}) => (
-  <va-breadcrumbs
-    label={label}
-    disable-analytics={disableAnalytics}
-    list-id={listId}
-    mobile-first-prop={mobileFirstProp}
-    nav-id={navId}
-  >
+const Template = ({ label, 'disable-analytics': disableAnalytics }) => (
+  <va-breadcrumbs label={label} disable-analytics={disableAnalytics}>
     <a href="#">Home</a>
     <a href="#">Level One</a>
     <a href="#">Level Two</a>
@@ -30,9 +18,6 @@ const Template = ({
 const defaultArgs = {
   'label': 'Breadcrumb',
   'disable-analytics': false,
-  'list-id': 'breadcrumbs-list-id',
-  'mobile-first-prop': false,
-  'nav-id': 'breadcrumbs-nav-id',
 };
 
 export const Default = Template.bind({});
@@ -40,9 +25,3 @@ Default.args = {
   ...defaultArgs,
 };
 Default.argTypes = propStructure(breadcrumbsDocs);
-
-export const MobileFirst = Template.bind({});
-MobileFirst.args = {
-  ...defaultArgs,
-  'mobile-first-prop': true,
-};
