c47deb0d10bfd75c74ac3dde76dbeb09380fe61c
Andres Rivera <andres.rivera.toro@gmail.com>
2021-12-29 15:25:58 -0500

    chore: add comment

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -1,3 +1,5 @@
+// When no targets are specified, preset-env will transform all ES2015-ES2020 code to be ES5 compatible.
+// https://babeljs.io/docs/en/babel-preset-env#no-targets
 module.exports = {
   presets: ['@babel/preset-env'],
 };
