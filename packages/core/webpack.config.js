521e5a6b15addef4116d67cdc2622403a06fd6cc
Andres Rivera <andres.rivera.toro@gmail.com>
2022-01-05 14:13:55 -0500

    chore: update entry

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/main.js'],
+  entry: './src/main.js',
   target: ['web', 'es5'],
   output: {
     path: __dirname + '/dist',
