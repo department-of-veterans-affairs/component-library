440e3c6e3a46c6e2425b5ccf384e37558066793e
Andres Rivera <andres.rivera.toro@gmail.com>
2022-01-06 19:03:37 -0500

    chore: update component story

--- packages/storybook/stories/OMBInfo.stories.jsx
+++ packages/storybook/stories/OMBInfo.stories.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {OMBInfo} from '@department-of-veterans-affairs/component-library';
+import { OMBInfo } from '@department-of-veterans-affairs/component-library';
 
 export default {
   title: 'Components/OMBInfo',
   component: OMBInfo,
 };
 
-const Template = (args) => <OMBInfo {...args} />;
+const Template = args => <OMBInfo {...args} />;
 
 const defaultArgs = {
   resBurden: 120,
   ombNumber: '12-3456',
   expDate: '12/31/2077',
+  hideResBurdenSection: false,
 };
 
 export const Default = Template.bind({});
