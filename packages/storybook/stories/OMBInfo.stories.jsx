f48cae273240e7b5959d76f5ae4f12a78fb84c14
Andres Rivera <andres.rivera.toro@gmail.com>
2022-01-10 10:35:57 -0500

    chore: add story for custom respondent burden name

--- packages/storybook/stories/OMBInfo.stories.jsx
+++ packages/storybook/stories/OMBInfo.stories.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {OMBInfo} from '@department-of-veterans-affairs/component-library';
+import { OMBInfo } from '@department-of-veterans-affairs/component-library';
 
 export default {
   title: 'Components/OMBInfo',
   component: OMBInfo,
 };
 
-const Template = (args) => <OMBInfo {...args} />;
+const Template = args => <OMBInfo {...args} />;
 
 const defaultArgs = {
   resBurden: 120,
   ombNumber: '12-3456',
   expDate: '12/31/2077',
+  resBurdenName: undefined,
 };
 
 export const Default = Template.bind({});
@@ -22,3 +23,10 @@ WithoutOMBNumber.args = { ...defaultArgs, ombNumber: '' };
 
 export const WithoutResponseBurden = Template.bind({});
 WithoutResponseBurden.args = { ...defaultArgs, resBurden: undefined };
+
+export const WithCustomRespondentBurdenName = Template.bind({});
+WithCustomRespondentBurdenName.args = {
+  ...defaultArgs,
+  resBurden: 90,
+  resBurdenName: 'EXAMPLE',
+};
