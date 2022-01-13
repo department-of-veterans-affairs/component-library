daa01f2d4cd361da5521f2be7af9a9bee013ccce
Brooks Johnson <brooks.johnson@pm.me>
2022-01-13 11:34:23 -0800

    Try a different approach for webpack+babel
    Based off of the code example here:
    https://webpack.js.org/loaders/babel-loader/#usage

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -18,6 +18,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
       {
