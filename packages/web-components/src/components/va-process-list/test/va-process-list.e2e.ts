a8967c91f0c24a8d6a711044d83b54f2e6fa7550
Brooks Johnson <brooks.johnson@pm.me>
2022-01-17 15:11:51 -0800

    Change basic e2e test

--- packages/web-components/src/components/va-process-list/test/va-process-list.e2e.ts
+++ packages/web-components/src/components/va-process-list/test/va-process-list.e2e.ts
@@ -2,12 +2,47 @@ import { newE2EPage } from '@stencil/core/testing';
 import { axeCheck } from '../../../testing/test-helpers';
 
 describe('va-process-list', () => {
-  it('renders', async () => {
+  it('renders slotted nodes into an ordered list', async () => {
     const page = await newE2EPage();
-    await page.setContent('<va-process-list></va-process-list>');
+    await page.setContent(`
+      <va-process-list>
+        <li>
+          <h2>Step one</h2>
+          <p>Some content</p>
+        </li>
+        <li>
+          <h2>Step two</h2>
+          <p>Additional content</p>
+          <ul>
+            <li>Item one</li>
+            <li>Item two</li>
+          </ul>
+        </li>
+      </va-process-list>
+    `);
 
     const element = await page.find('va-process-list');
-    expect(element).toHaveClass('hydrated');
+    expect(element).toEqualHtml(`
+      <va-process-list class="hydrated">
+        <mock:shadow-root>
+          <ol role="list">
+            <slot></slot>
+          </ol>
+        </mock:shadow-root>
+        <li>
+          <h2>Step one</h2>
+          <p>Some content</p>
+        </li>
+        <li>
+          <h2>Step two</h2>
+          <p>Additional content</p>
+          <ul>
+            <li>Item one</li>
+            <li>Item two</li>
+          </ul>
+        </li>
+      </va-process-list>
+    `);
   });
 
   it('passes an axe check', async () => {
