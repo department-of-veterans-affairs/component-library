d2957847df8a6e75d61d640a361cb726077d5804
Brooks Johnson <brooks.johnson@pm.me>
2022-01-17 11:15:35 -0800

    Add axe check

--- packages/web-components/src/components/va-process-list/test/va-process-list.e2e.ts
+++ packages/web-components/src/components/va-process-list/test/va-process-list.e2e.ts
@@ -1,4 +1,5 @@
 import { newE2EPage } from '@stencil/core/testing';
+import { axeCheck } from '../../../testing/test-helpers';
 
 describe('va-process-list', () => {
   it('renders', async () => {
@@ -7,5 +8,18 @@ describe('va-process-list', () => {
 
     const element = await page.find('va-process-list');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('passes an axe check', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+        <va-process-list>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </va-process-list>
+      `);
+
+    await axeCheck(page);
   });
 });
