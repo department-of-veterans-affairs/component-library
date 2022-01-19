3d427b4f1fb430f7d8e284d14b23a46065534ab2
Andres Rivera <andres.rivera.toro@gmail.com>
2022-01-18 21:35:42 -0500

    chore: update story to reflect content style guide

--- packages/storybook/stories/va-breadcrumbs.stories.jsx
+++ packages/storybook/stories/va-breadcrumbs.stories.jsx
@@ -10,8 +10,8 @@ export default {
 const Template = ({ label, 'disable-analytics': disableAnalytics }) => (
   <va-breadcrumbs label={label} disable-analytics={disableAnalytics}>
     <a href="#home">Home</a>
-    <a href="#one">Level One</a>
-    <a href="#two">Level Two</a>
+    <a href="#one">Level one</a>
+    <a href="#two">Level two</a>
   </va-breadcrumbs>
 );
 
