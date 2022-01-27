cdbb16b2321a131184dd85489c63d2e3213756ca
Preston Davies <pdavies88@gmail.com>
2022-01-27 07:50:49 -0500

    chore: update to use .setHours for Date

--- packages/storybook/stories/MaintenanceBanner.stories.jsx
+++ packages/storybook/stories/MaintenanceBanner.stories.jsx
@@ -14,20 +14,6 @@ export default {
   },
 };
 
-function addHoursToDate(objDate, intHours) {
-  var numberOfMlSeconds = objDate.getTime();
-  var addMlSeconds = intHours * 60 * 60 * 1000;
-  var newDateObj = new Date(numberOfMlSeconds + addMlSeconds);
-  return newDateObj;
-}
-
-function subtractTimeFromDate(objDate, intHours) {
-  var numberOfMlSeconds = objDate.getTime();
-  var addMlSeconds = intHours * 60 * 60 * 1000;
-  var newDateObj = new Date(numberOfMlSeconds - addMlSeconds);
-  return newDateObj;
-}
-
 const Template = args => {
   const startTime = args.startsAt;
   const endTime = args.expiresAt;
@@ -48,9 +34,9 @@ const defaultArgs = {
     'We’re working on VA.gov right now. If you have trouble signing in or using tools, check back after we’re finished. Thank you for your patience.',
   warnContent:
     'We’ll be doing some work on VA.gov. The maintenance will last X hours. During that time, you won’t be able to sign in or use tools.',
-  startsAt: new Date(Date.now()),
-  expiresAt: addHoursToDate(new Date(Date.now()), 2),
-  warnStartsAt: subtractTimeFromDate(new Date(Date.now()), 1),
+  startsAt: new Date(),
+  expiresAt: new Date(new Date().setHours(new Date().getHours() + 2)),
+  warnStartsAt: new Date(new Date().setHours(new Date().getHours() - 1)),
 };
 
 export const DuringMaintenance = Template.bind({});
@@ -59,7 +45,7 @@ DuringMaintenance.args = { ...defaultArgs };
 export const BeforeMaintenance = Template.bind({});
 BeforeMaintenance.args = {
   ...defaultArgs,
-  startsAt: addHoursToDate(new Date(Date.now()), 1),
-  expiresAt: addHoursToDate(new Date(Date.now()), 2),
-  warnStartsAt: new Date(Date.now()),
+  startsAt: new Date(new Date().setHours(new Date().getHours() + 1)),
+  expiresAt: new Date(new Date().setHours(new Date().getHours() + 2)),
+  warnStartsAt: new Date(),
 };
