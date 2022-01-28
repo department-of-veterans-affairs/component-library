c6a706624c0a51d7fc04843c804bf2485a6cf6b5
Brooks Johnson <brooks.johnson@pm.me>
2022-01-28 14:50:54 -0800

    Add component documentation

--- packages/web-components/src/components/va-process-list/va-process-list.tsx
+++ packages/web-components/src/components/va-process-list/va-process-list.tsx
@@ -1,5 +1,8 @@
 import { Component, h } from '@stencil/core';
 
+/**
+ * This component expects `<li>` elements as its children.
+ */
 @Component({
   tag: 'va-process-list',
   styleUrl: 'va-process-list.css',
