4e9eb9349692b1d2253e26010e2aea5867daba30
Andres Rivera <andres.rivera.toro@gmail.com>
2022-02-03 17:15:03 -0500

    revert: remove role from host

--- packages/web-components/src/components/va-alert/va-alert.tsx
+++ packages/web-components/src/components/va-alert/va-alert.tsx
@@ -157,7 +157,7 @@ export class VaAlert {
     if (!visible) return <div aria-live="polite" />;
 
     return (
-      <Host role="presentation">
+      <Host>
         <div role={role} aria-live={ariaLive} class={classes}>
           <i aria-hidden="true" role="img"></i>
           <div class="body">
