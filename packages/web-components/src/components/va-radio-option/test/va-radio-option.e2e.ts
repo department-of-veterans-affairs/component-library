1b6fd1ad5e835f1903fce6706b94117de42e5297
Preston Davies <pdavies88@gmail.com>
2022-02-03 13:12:06 -0500

    chore: update testing suite to account for aria-label usage

--- packages/web-components/src/components/va-radio-option/test/va-radio-option.e2e.ts
+++ packages/web-components/src/components/va-radio-option/test/va-radio-option.e2e.ts
@@ -9,6 +9,20 @@ describe('va-radio-option', () => {
     expect(element).toHaveClass('hydrated');
   });
 
+  it('inserts and parses optional props and attributes correctly', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-radio-option id="yes2" label="Yes - Any Veteran" name="yes" value="2"></va-radio-option>');
+
+    const element = await page.find('va-radio-option');
+    expect(element).toEqualHtml(`
+    <va-radio-option id="yes2" label="Yes - Any Veteran" name="yes" value="2" aria-checked="false" aria-label="yes" role="radio" class="hydrated">
+      <mock:shadow-root>
+        <label for="Yes - Any Veteran">Yes - Any Veteran</label>
+      </mock:shadow-root>
+    </va-radio-option>
+  `);
+  });
+
   it('sets checked to true based on prop', async () => {
     const page = await newE2EPage();
     await page.setContent(
