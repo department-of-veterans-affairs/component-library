c1c54d2b4ac6f634c8fb0cbf0d3d64391ddbe85a
Brooks Johnson <brooks.johnson@pm.me>
2022-02-10 15:18:36 -0800

    Add axe-check test

--- packages/web-components/src/components/va-table/test/va-table.e2e.ts
+++ packages/web-components/src/components/va-table/test/va-table.e2e.ts
@@ -1,4 +1,5 @@
 import { newE2EPage } from '@stencil/core/testing';
+import { axeCheck } from '../../../testing/test-helpers';
 
 describe('va-table', () => {
   it('renders', async () => {
@@ -7,5 +8,24 @@ describe('va-table', () => {
 
     const element = await page.find('va-table');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('passes an axe check', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+        <va-table>
+          <th slot="headers">One</th>
+          <th slot="headers">Two</th>
+          <th slot="headers">Three</th>
+          <va-table-row>
+            <td>Dog</td>
+            <td>Cat</td>
+            <td>Mouse</td>
+          </va-table-row>
+        </va-table>
+      `);
+
+    await page.waitForChanges();
+    await axeCheck(page);
   });
 });
