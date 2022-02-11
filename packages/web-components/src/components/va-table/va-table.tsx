9201755d2f98ce7bc69e89e2e15476461e377bbd
Brooks Johnson <brooks.johnson@pm.me>
2022-02-10 16:21:13 -0800

    Set data-label for mobile view

--- packages/web-components/src/components/va-table/va-table.tsx
+++ packages/web-components/src/components/va-table/va-table.tsx
@@ -1,4 +1,5 @@
 import { Component, Prop, Host, h } from '@stencil/core';
+import { getSlottedNodes } from '../../utils/utils';
 
 @Component({
   tag: 'va-table',
@@ -7,16 +8,34 @@ import { Component, Prop, Host, h } from '@stencil/core';
 })
 export class VaTable {
   header!: HTMLTableRowElement;
+  body!: HTMLTableSectionElement;
   @Prop() title: string;
 
   componentDidLoad() {
     const headers = (
       this.header.children[0] as HTMLSlotElement
     ).assignedNodes();
+    const rows = (this.body.children[0] as HTMLSlotElement).assignedNodes();
+
+    console.log('ROWS', rows);
+
+    const columns = [];
 
     headers.forEach(item => {
+      columns.push((item as HTMLElement).textContent);
       (item as Element).setAttribute('role', 'columnheader');
       (item as Element).setAttribute('scope', 'col');
+    });
+
+    rows.forEach(row => {
+      const slotted = getSlottedNodes(row as HTMLElement, 'tr')[0];
+      console.log('SLOTTED', slotted);
+      const cells = (row as HTMLElement).childNodes;
+      console.log(cells);
+
+      cells.forEach((cell, index) => {
+        (cell as Element).setAttribute('data-label', columns[index]);
+      });
     });
   }
 
@@ -28,16 +47,12 @@ export class VaTable {
         <table role="table">
           {title && <caption>{title}</caption>}
           <thead>
-            <tr
-              role="row"
-              ref={el => {
-                this.header = el as HTMLTableRowElement;
-              }}
-            >
+            <tr ref={el => (this.header = el as HTMLTableRowElement)}>
               <slot name="headers"></slot>
             </tr>
           </thead>
-          <tbody>
+
+          <tbody ref={el => (this.body = el as HTMLTableSectionElement)}>
             <slot></slot>
           </tbody>
         </table>
