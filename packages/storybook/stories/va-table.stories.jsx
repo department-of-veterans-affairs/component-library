8847c8d6ff282003f086c01075b2d31804050cf5
Brooks Johnson <brooks.johnson@pm.me>
2022-02-14 14:15:29 -0800

    Refactor the story args

--- packages/storybook/stories/va-table.stories.jsx
+++ packages/storybook/stories/va-table.stories.jsx
@@ -15,8 +15,31 @@ export default {
 };
 
 const Template = args => {
-  const { data, caption, col1, col2, col3 } = args;
-  const columns = [col1, col2, col3];
+  const { caption } = args;
+  const data = [
+    [
+      'Declaration of Independence',
+      'Statement adopted by the Continental Congress declaring independence from the British Empire',
+      '1776',
+    ],
+    [
+      'Bill of Rights',
+      'The first ten ammendements of the U.S. Constitution guaranteeing rights and freedoms',
+      '1791',
+    ],
+    [
+      'Declaration of Sentiments',
+      'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
+      '1848',
+    ],
+    [
+      'Emancipation Proclamation',
+      'An executive order granting freedom to slaves in designated southern states.',
+      '1863',
+    ],
+  ];
+
+  const columns = ['Document title', 'Description', 'Year'];
   return (
     <main>
       <va-table caption={caption}>
@@ -40,31 +63,6 @@ const Template = args => {
 
 const defaultArgs = {
   caption: 'My table',
-  col1: 'Document title',
-  col2: 'Description',
-  col3: 'Year',
-  data: [
-    [
-      'Declaration of Independence',
-      'Statement adopted by the Continental Congress declaring independence from the British Empire',
-      '1776',
-    ],
-    [
-      'Bill of Rights',
-      'The first ten ammendements of the U.S. Constitution guaranteeing rights and freedoms',
-      '1791',
-    ],
-    [
-      'Declaration of Sentiments',
-      'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
-      '1848',
-    ],
-    [
-      'Emancipation Proclamation',
-      'An executive order granting freedom to slaves in designated southern states.',
-      '1863',
-    ],
-  ],
 };
 
 export const Default = Template.bind({});
