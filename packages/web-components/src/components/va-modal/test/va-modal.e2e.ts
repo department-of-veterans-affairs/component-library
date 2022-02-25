add9a0ec5f631c604194e137dffe96d8ea74d599
Andres Rivera <andres.rivera.toro@gmail.com>
2022-02-25 11:28:40 -0500

    test: add va-modal tests

--- packages/web-components/src/components/va-modal/test/va-modal.e2e.ts
+++ packages/web-components/src/components/va-modal/test/va-modal.e2e.ts
@@ -1,11 +1,86 @@
 import { newE2EPage } from '@stencil/core/testing';
+import { axeCheck } from '../../../testing/test-helpers';
 
 describe('va-modal', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<va-modal></va-modal>');
+    await page.setContent(`
+      <va-modal headline="Example Title" visible>
+        <p>
+          A modal may pass any React nodes as children to be displayed within it.
+        </p>
+      </va-modal>
+    `);
 
     const element = await page.find('va-modal');
-    expect(element).toHaveClass('hydrated');
+    expect(element).toEqualHtml(`
+      <va-modal aria-label="va-modal-title" aria-modal="true" class="hydrated va-modal" headline="Example Title" role="dialog" visible>
+        <mock:shadow-root>
+          <div class="va-modal-inner" tabindex="-1">
+            <button aria-label="close Example Title modal" class="va-modal-close" type="button">
+              <i aria-hidden="true" class="fa-times-circle fas"></i>
+            </button>
+            <div class="va-modal-body">
+              <div role="document">
+                <h1 class="va-modal-title vads-u-font-size--h3" id="va-modal-title" tabindex="-1">
+                  Example Title
+                </h1>
+                <slot></slot>
+              </div>
+            </div>
+          </div>
+        </mock:shadow-root>
+        <p>
+          A modal may pass any React nodes as children to be displayed within it.
+        </p>
+      </va-modal>
+    `);
+  });
+
+  it('passes an axe check', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <va-modal headline="Example Title" visible>
+        <p>
+          A modal may pass any React nodes as children to be displayed within it.
+        </p>
+      </va-modal>
+    `);
+
+    await axeCheck(page);
+  });
+
+  it('closeEvent is triggered using the Escape key when modal is visible', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <va-modal headline="Example Title" visible>
+        <p>
+          A modal may pass any React nodes as children to be displayed within it.
+        </p>
+      </va-modal>
+    `);
+
+    const closeEvent = await page.spyOnEvent('closeEvent');
+    const component = await page.find('va-modal');
+    await component.press('Escape');
+
+    expect(closeEvent).toHaveReceivedEvent();
+  });
+
+  it('closeEvent is not triggered using the Escape key when modal is not visible', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <va-modal headline="Example Title">
+        <p>
+          A modal may pass any React nodes as children to be displayed within it.
+        </p>
+      </va-modal>
+    `);
+
+    const closeEvent = await page.spyOnEvent('closeEvent');
+    const component = await page.find('va-modal');
+    await component.press('Escape');
+
+    expect(closeEvent).toHaveReceivedEventTimes(0);
   });
 });
