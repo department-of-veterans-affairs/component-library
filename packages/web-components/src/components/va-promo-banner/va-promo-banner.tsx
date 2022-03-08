403178d332f6bbb14398b4b66c91133c22d08628
Preston Davies <pdavies88@users.noreply.github.com>
2022-03-08 14:21:18 -0500

    35152 Promo Banner Web Component (#297)
    * 35152 Init Promo Banner
    
    * Promo-Banner Styling and Storybook Rendering
    
    * chore: update Promo-Banner Component and Story for Dismissing Banner
    
    * init Promo Banner Testing
    
    * chore: e2e testing Promo Banner
    
    * chore: bump Minor Version
    
    * chore: update type values
    
    * chore: update unit test based on updated prop values
    
    * chore: update comments on render-custom
    
    * chore: remove css browser prefixes
    
    * chore: remove render-custom and replace banner-id with native id attr
    
    * chore: ID attribute test, css flexbox, analytics event innertext
    
    * chore: move gray border on mobile
    
    * chore: set default for target
    
    * chore: remove target prop
    
    * chore: remove target argtype in storybook
    
    * chore: bump minor version
    
    * chore: bump minor version
    
    * chore: consolidate va-promo-banner for flexbox updates
    
    * chore: role banner
    
    * chore: update e2e tests

---
+++ packages/web-components/src/components/va-promo-banner/va-promo-banner.tsx
@@ -0,0 +1,134 @@
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  Host,
+  h,
+  Prop,
+  State,
+} from '@stencil/core';
+
+const DISMISSED_PROMO_BANNERS_LOCAL_STORAGE_KEY = 'DISMISSED_PROMO_BANNERS';
+@Component({
+  tag: 'va-promo-banner',
+  styleUrl: 'va-promo-banner.css',
+  shadow: true,
+})
+export class VaPromoBanner {
+  @Element() el: HTMLElement;
+
+  /**
+   * `href` attribute for the `<a>` tag.
+   */
+  @Prop() href: string;
+
+  /**
+   * Controls which icon gets used. Options are 'announcement', 'news', or 'email-signup'.
+   * */
+  @Prop() type: 'announcement' | 'news' | 'email-signup';
+
+  /**
+   * Analytics tracking function(s) will not be called
+   */
+  @Prop() disableAnalytics: boolean;
+
+  /**
+   * Fires when the component is closed by clicking on the close icon.
+   */
+  @Event({
+    composed: true,
+    bubbles: true,
+  })
+  closeEvent: EventEmitter;
+
+  /**
+   * The event used to track usage of the component. This is emitted when an
+   * anchor link or the dismiss icon is clicked and disableAnalytics is not true.
+   */
+  @Event({
+    eventName: 'component-library-analytics',
+    composed: true,
+    bubbles: true,
+  })
+  componentLibraryAnalytics: EventEmitter;
+
+  /**
+   * Keep track of locally dismissed Banners
+   * */
+  @State() dismissedBanners: Array<String> = [];
+
+  private closeHandler(): void {
+    this.closeEvent.emit();
+    if (this.el.id) {
+      // Derive the updated dismissed banners based on the id attribute set on the component.
+      const updatedDismissedBanners = [...this.dismissedBanners, this.el.id];
+
+      // Update dismissedBanners in state.
+      this.dismissedBanners = updatedDismissedBanners;
+
+      // Add the promo banner ID to local storage.
+      localStorage.setItem(
+        DISMISSED_PROMO_BANNERS_LOCAL_STORAGE_KEY,
+        JSON.stringify(updatedDismissedBanners),
+      );
+    }
+  }
+
+  private handleLinkClick(): void {
+    if (!this.disableAnalytics) {
+      const detail = {
+        componentName: 'va-promo-banner',
+        action: 'linkClick',
+        details: {
+          text: this.el.innerText,
+          href: this.href,
+          type: this.type,
+        },
+      };
+      this.componentLibraryAnalytics.emit(detail);
+    }
+  }
+
+  componentWillLoad() {
+    // Derive dismissed banners from storage.
+    const dismissedBannersString = localStorage.getItem(
+      DISMISSED_PROMO_BANNERS_LOCAL_STORAGE_KEY,
+    );
+    this.dismissedBanners = dismissedBannersString
+      ? JSON.parse(dismissedBannersString)
+      : [];
+  }
+
+  render() {
+    // Derive if the banner is dismissed based on the id attribute set on the component.
+    const isBannerDismissed = this.dismissedBanners?.includes(this.el.id);
+
+    // Do not render if the promo banner is dismissed.
+    if (isBannerDismissed) {
+      return null;
+    }
+
+    return (
+      <Host>
+        <div class="va-banner-body" role="banner">
+          <i aria-hidden="true" class={this.type} role="presentation" />
+          <a
+            class="va-banner-content-link"
+            href={this.href}
+            onClick={() => this.handleLinkClick()}
+          >
+            <slot></slot> <i aria-hidden="true" role="presentation" />
+          </a>
+          <button
+            type="button"
+            aria-label="Dismiss this promo banner"
+            onClick={() => this.closeHandler()}
+          >
+            <i aria-hidden="true" role="presentation" />
+          </button>
+        </div>
+      </Host>
+    );
+  }
+}
