b8c28a969085b3d83e6d6a9d62f89f2464df57d6
Andres Rivera <andres.rivera.toro@gmail.com>
2022-03-18 17:45:56 -0400

    chore: update story

--- packages/storybook/stories/va-search.stories.jsx
+++ packages/storybook/stories/va-search.stories.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { VaSearch } from '@department-of-veterans-affairs/web-components/react-bindings';
 import { getWebComponentDocs, propStructure } from './wc-helpers';
+import { generateEventsDescription } from './events';
 
 const searchDocs = getWebComponentDocs('va-search');
 
 export default {
   title: 'Components/va-search',
+  parameters: {
+    docs: {
+      description: {
+        component: generateEventsDescription(searchDocs),
+      },
+    },
+  },
 };
 
-const Template = () => <va-search />;
+VaSearch.displayName = 'VaSearch';
 
-const defaultArgs = {};
+const Template = ({ action, label, method, submitEvent }) => (
+  <VaSearch
+    action={action}
+    label={label}
+    method={method}
+    onSubmitEvent={submitEvent}
+  />
+);
+
+const defaultArgs = {
+  action: undefined,
+  label: 'Search',
+  method: undefined,
+};
 
 export const Default = Template.bind({});
 Default.args = {
   ...defaultArgs,
+  submitEvent: e => console.log(e),
 };
 Default.argTypes = propStructure(searchDocs);
