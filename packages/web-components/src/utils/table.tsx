6a4882b9b572e646056faf8a8aaefa580487eecf
Brooks Johnson <brooks.johnson@pm.me>
2022-03-18 17:26:28 -0700

    Include the table header

--- packages/web-components/src/utils/table.tsx
+++ packages/web-components/src/utils/table.tsx
@@ -4,16 +4,26 @@ import React from 'react';
 // import { JSX } from '../components';
 export function generateRows(data: any[], field: Object[]): any[] {
   console.log(field);
-  return data.map(row => {
-    console.log('ROW', row);
-    return (
-      /** @ts-ignore */
-      <va-table-row>
-        {row.map(cell => (
-          <span>{cell}</span>
-        ))}
-        {/** @ts-ignore */}
-      </va-table-row>
-    );
-  });
+  return [
+    /** @ts-ignore */
+    <va-table-row slot="headers">
+      {field.map(col => (
+        <span>{col}</span>
+      ))}
+      {/** @ts-ignore */}
+    </va-table-row>,
+
+    ...data.map(row => {
+      console.log('ROW', row);
+      return (
+        /** @ts-ignore */
+        <va-table-row>
+          {row.map(cell => (
+            <span>{cell}</span>
+          ))}
+          {/** @ts-ignore */}
+        </va-table-row>
+      );
+    }),
+  ];
 }
