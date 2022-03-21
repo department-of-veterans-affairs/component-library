1f43bc3ad80c2db336a611e31f65f980661431ca
Brooks Johnson <brooks.johnson@pm.me>
2022-03-21 15:28:17 -0700

    Improve the typing

--- packages/web-components/src/utils/table.tsx
+++ packages/web-components/src/utils/table.tsx
@@ -2,13 +2,28 @@
 // import { h } from '@stencil/core';
 import React from 'react';
 // import { JSX } from '../components';
-export function generateTableChildren(data: any[], field: Object[]): any[] {
-  console.log(field);
+
+/**
+ * This is based off of the React `<Table>` implementation where fields
+ * was an array of objects w/ `label`, `value`, and a few other properties.
+ * For this we only care about `label`.
+ */
+interface FieldType {
+  label: string;
+}
+
+/**
+ * Useful for generating child content for a `<va-table>`
+ *
+ * @param {array} data - An array of arrays, where the inner arrays represent row data
+ * @param {array} fields - The field/column names
+ */
+export function generateTableChildren(data: any[], fields: FieldType[]): any[] {
   return [
     /** @ts-ignore */
     <va-table-row slot="headers">
-      {field.map(col => (
-        <span>{col}</span>
+      {fields.map(col => (
+        <span>{col.label}</span>
       ))}
       {/** @ts-ignore */}
     </va-table-row>,
