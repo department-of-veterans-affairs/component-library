5377e66a206448b5a47f0e004565bee811905f5e
Brooks Johnson <brooks.johnson@pm.me>
2022-03-23 14:45:55 -0700

    Don't truncate variable names
    This was leftover from copying some quicksort pseudocode

--- packages/web-components/src/utils/dom.ts
+++ packages/web-components/src/utils/dom.ts
@@ -25,66 +25,66 @@ function swapChildren(
  *  3. Moves the higher index lower until an item is less than the pivot
  *  4. Swaps those two items
  *  5. Continues until the lower and the higher indices have met/crossed
- * At the end of the funciton, all items from indices `lo` to the pivot index
+ * At the end of the funciton, all items from indices `low` to the pivot index
  * will be smaller than the pivot item, and all items from the pivot index to
- * `hi` will be greater than the pivot position
+ * `high` will be greater than the pivot position
  *
  * @param {NodeList} rows - A live NodeList of elements to sort
- * @param {number} lo - The lower bound index to use for the partition
- * @param {number} hi - The upper bound index to use for the partition
+ * @param {number} low - The lower bound index to use for the partition
+ * @param {number} high - The upper bound index to use for the partition
  * @param {Function} selector - An optional selector to use for selecting values for comparison
  * @param {Function} comparator - An optional comparator to use for comparing elements
  */
 function partition(
   rows: NodeList,
-  lo: number,
-  hi: number,
+  low: number,
+  high: number,
   selector: Function,
   comparator: Function,
 ): number {
-  const pivot = selector(rows[Math.floor((lo + hi) / 2)]);
+  const pivot = selector(rows[Math.floor((low + high) / 2)]);
 
-  while (lo <= hi) {
-    // By default, selector(rows[lo]) < pivot
-    while (comparator(selector(rows[lo]), pivot)) {
-      lo++;
+  while (low <= high) {
+    // By default, selector(rows[low]) < pivot
+    while (comparator(selector(rows[low]), pivot)) {
+      low++;
     }
-    // By default, selector(rows[hi]) > pivot
-    while (comparator(pivot, selector(rows[hi]))) {
-      hi--;
+    // By default, selector(rows[high]) > pivot
+    while (comparator(pivot, selector(rows[high]))) {
+      high--;
     }
 
-    if (lo <= hi) {
-      swapChildren(rows, lo, hi);
-      lo++;
-      hi--;
+    if (low <= high) {
+      swapChildren(rows, low, high);
+      low++;
+      high--;
     }
   }
 
-  return lo;
+  return low;
 }
 
 /**
  * Sorts a NodeList in place.
  * @param {NodeList} rows - A live NodeList of elements to sort
- * @param {number} lo - The lower bound index to use for the partition
- * @param {number} hi - The upper bound index to use for the partition
+ * @param {number} low - The lower bound index to use for the partition
+ * @param {number} high - The upper bound index to use for the partition
  * @param {Function} selector - An optional selector to use for selecting values for comparison
  * @param {Function} comparator - An optional comparator to use for comparing elements
  */
 export function quicksort(
   rows: NodeList,
-  lo: number,
-  hi: number,
+  low: number,
+  high: number,
   selector = row => row.textContent,
   comparator = (a, b) => a < b,
 ): void {
-  if (lo >= hi || lo < 0) return;
+  if (low >= high || low < 0) return;
 
-  const pivot = partition(rows, lo, hi, selector, comparator);
+  const pivot = partition(rows, low, high, selector, comparator);
 
-  quicksort(rows, lo, pivot - 1, selector, comparator); // Left side
-  quicksort(rows, pivot, hi, selector, comparator); // Right side
+  quicksort(rows, low, pivot - 1, selector, comparator); // Left side
+  quicksort(rows, pivot, high, selector, comparator); // Right side
 }
 
 /**
@@ -93,9 +93,9 @@ export function quicksort(
  */
 export function reverseQuicksort(
   rows: NodeList,
-  lo: number,
-  hi: number,
+  low: number,
+  high: number,
   selector = row => row.textContent,
 ): void {
-  quicksort(rows, lo, hi, selector, (a, b) => a > b);
+  quicksort(rows, low, high, selector, (a, b) => a > b);
 }
