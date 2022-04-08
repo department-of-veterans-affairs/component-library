924b8a5dd3d05bcb6acd65e6adab7a75b9e79a56
Preston Davies <pdavies88@users.noreply.github.com>
2022-04-08 14:43:42 -0400

    Update va-number-input.tsx

--- packages/web-components/src/components/va-number-input/va-number-input.tsx
+++ packages/web-components/src/components/va-number-input/va-number-input.tsx
@@ -138,6 +138,7 @@ export class VaNumberInput {
           type="number"
           aria-describedby={describedBy}
           inputmode={inputMode}
+          name={this.name}
           max={this.max}
           min={this.min}
           value={this.value}
