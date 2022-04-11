2f95b3fd6293c9d508f587f1ec5609bb6d035e2a
Brooks Johnson <brooks.johnson@pm.me>
2022-04-11 13:57:27 -0700

    Let prettier format on autosave

--- packages/design-system-dashboard-cli/test/search-files.spec.js
+++ packages/design-system-dashboard-cli/test/search-files.spec.js
@@ -7,20 +7,21 @@ const {
 } = require('../src/search-files');
 
 const env = {
-  "repos": {
-    "vets-website": "mock-vw/",
-    "content-build": "mock-cb/"
-  }
-}
+  repos: {
+    'vets-website': 'mock-vw/',
+    'content-build': 'mock-cb/',
+  },
+};
 const jsxFile = 'const phone = () => (<va-telephone contact={phoneContact} />)';
 
 const json = {
   '../.env.json': JSON.stringify(env),
-    'mock-vw/jsx-file.jsx': jsxFile,
-    'mock-vw/js-file.js': 'some JS',
-    'mock-vw/txt-file.txt': 'Some text', 
-    'mock-cb/liquid-template.liquid': 'A template',
-    'mock-cb/html-layout.html': 'HTML page',
+  'mock-vw/jsx-file.jsx': jsxFile,
+  'mock-vw/js-file.js': 'some JS',
+  // 'mock-vw/js-file.unit.js': 'Testing code',
+  'mock-vw/txt-file.txt': 'Some text',
+  'mock-cb/liquid-template.liquid': 'A template',
+  'mock-cb/html-layout.html': 'HTML page',
 };
 
 jest.mock('fs');
@@ -28,51 +29,50 @@ memfs.vol.fromJSON(json, '.');
 
 describe('readAllModules', () => {
   it('reads only js and jsx files', () => {
-    const files = readAllModules(env.repos['vets-website'])
+    const files = readAllModules(env.repos['vets-website']);
 
-    expect(files.length).toEqual(2)
-    expect(files[0].path).toEqual('mock-vw/js-file.js')
-    expect(files[0].contents).toEqual('some JS')
-    expect(files[1].path).toEqual('mock-vw/jsx-file.jsx')
-    expect(files[1].contents).toEqual(jsxFile)
-  })
+    expect(files.length).toEqual(2);
+    expect(files[0].path).toEqual('mock-vw/js-file.js');
+    expect(files[0].contents).toEqual('some JS');
+    expect(files[1].path).toEqual('mock-vw/jsx-file.jsx');
+    expect(files[1].contents).toEqual(jsxFile);
+  });
 });
 
 describe('readAllTemplates', () => {
   it('reads only liquid and html files', () => {
-    const files = readAllTemplates(env.repos['content-build'])
+    const files = readAllTemplates(env.repos['content-build']);
 
-    expect(files.length).toEqual(2)
-    expect(files[0].path).toEqual('mock-cb/html-layout.html')
-    expect(files[0].contents).toEqual('HTML page')
-    expect(files[1].path).toEqual('mock-cb/liquid-template.liquid')
-    expect(files[1].contents).toEqual('A template')
-  })
+    expect(files.length).toEqual(2);
+    expect(files[0].path).toEqual('mock-cb/html-layout.html');
+    expect(files[0].contents).toEqual('HTML page');
+    expect(files[1].path).toEqual('mock-cb/liquid-template.liquid');
+    expect(files[1].contents).toEqual('A template');
+  });
 });
 
 describe('readFile', () => {
   it('returns an array with a single file matching the structure in `readAllModules` and `readAllTemplates`', () => {
-    const filepath = 'mock-vw/txt-file.txt'
-    const files = readFile(filepath)
+    const filepath = 'mock-vw/txt-file.txt';
+    const files = readFile(filepath);
 
-    expect(files.length).toEqual(1)
-    expect(files[0].path).toEqual(filepath)
-    expect(files[0].contents).toEqual('Some text')
-  })
+    expect(files.length).toEqual(1);
+    expect(files[0].path).toEqual(filepath);
+    expect(files[0].contents).toEqual('Some text');
+  });
 });
 
 describe('search', () => {
   it('filters out files based on a search term', () => {
-    const files = readAllModules(env.repos['vets-website'])
-    const searchTerm = new RegExp('telephone', 'gms')
-    const filteredFiles = search(files, searchTerm)
+    const files = readAllModules(env.repos['vets-website']);
+    const searchTerm = new RegExp('telephone', 'gms');
+    const filteredFiles = search(files, searchTerm);
 
-    console.log(filteredFiles)
-    expect(filteredFiles.length).toEqual(1)
-    expect(filteredFiles[0].path).toEqual('mock-vw/jsx-file.jsx')
-    expect(filteredFiles[0].matches).toEqual([['telephone']])
-  })
-});
+    console.log(filteredFiles);
+    expect(filteredFiles.length).toEqual(1);
+    expect(filteredFiles[0].path).toEqual('mock-vw/jsx-file.jsx');
+    expect(filteredFiles[0].matches).toEqual([['telephone']]);
+  });
 
   it('expects the second arg to be a regex', () => {
     expect(() => {
