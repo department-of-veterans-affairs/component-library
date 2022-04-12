e1d0ac6d68744d1246384bee9d3f6f85d7addf3c
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2022-04-12 08:17:17 -0700

    Add initial tests for dashboard-cli (#357)
    * Setup jest & add tests for search-files module
    
    * Add tests for get-manifest
    
    * Add tests for flatten-data
    
    * Add tests for to-sql
    
    * Set test coverage threshold to 30%
    
    Would like to keep improving this
    
    * Run tests with verbose output
    
    This should help distinguish between the different packages
    
    * Try installing in CI a different way
    
    * Install dependencies in workspace
    
    * Move dashboard install after react build
    
    Trying things out to get CI to pass
    
    * Update lock file
    
    I messed the merge up :upside-down-face:
    
    * Add .yarnrc.yml file
    
    This should fix the install issues
    
    * See if removing extra install breaks things
    
    * Change minimum coverage to 25%
    
    This is just temporary - we will ratchet it up higher in future PRs
    
    * Add second search test for correct arg type
    
    * Let prettier format on autosave
    
    * Add test for to-csv
    
    * Bump coverage minimum to 30%
    
    * Remove a console.log command
    
    * Clean up a bit

---
+++ packages/design-system-dashboard-cli/test/to-sql.spec.js
@@ -0,0 +1,38 @@
+const {
+  insert,
+  update 
+} = require('../src/to-sql');
+
+const flattenedData = [
+  {
+    date: '2022-04-08',
+    component_name: 'alert',
+    some_app: 2,
+    total: 2
+  },
+  {
+    date: '2022-04-08',
+    component_name: 'accordion',
+    some_app: 2,
+    another_app: 1,
+    total: 3
+  },
+]
+
+describe('insert', () => {
+  it('creates an INSERT query', () => {
+    const insertQuery = insert(flattenedData);
+
+    expect(insertQuery).toEqual('INSERT INTO `vsp-analytics-and-insights.platform_design_system.component-usage-count` (date, component_name, some_app, total, another_app) VALUES ("2022-04-08", "alert", 2, 2, 0), ("2022-04-08", "accordion", 2, 3, 1)');
+  });
+});
+
+describe('update', () => {
+  it('creates an UPDATE query for each component', () => {
+    const updateQueries = update(flattenedData).split('\n\n');
+
+    expect(updateQueries.length).toEqual(2);
+    expect(updateQueries[0]).toEqual('UPDATE `vsp-analytics-and-insights.platform_design_system.component-usage-count` SET some_app = 2, total = 2 WHERE component_name = "alert" AND date = "2022-04-08";')
+  expect(updateQueries[1]).toEqual('UPDATE `vsp-analytics-and-insights.platform_design_system.component-usage-count` SET some_app = 2, another_app = 1, total = 3 WHERE component_name = "accordion" AND date = "2022-04-08";')
+  });
+});
