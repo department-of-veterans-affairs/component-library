fca76d9e9f1139346d44b7b4d2a5c2b5af641591
Andres Rivera <andres.rivera.toro@gmail.com>
2022-04-14 11:19:20 -0400

    chore: add suggestions story

--- packages/storybook/stories/va-search.stories.jsx
+++ packages/storybook/stories/va-search.stories.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { VaSearch } from '@department-of-veterans-affairs/web-components/react-bindings';
 import { getWebComponentDocs, propStructure } from './wc-helpers';
 import { generateEventsDescription } from './events';
@@ -22,6 +22,8 @@ const Template = ({
   'button-text': buttonText,
   'input-value': inputValue,
   label,
+  'show-suggestions': showSuggestions,
+  suggestions,
 }) => (
   <VaSearch
     buttonText={buttonText}
@@ -34,13 +36,20 @@ const Template = ({
     onButtonClickEvent={e => console.log(e, 'BUTTON CLICK FIRED')}
     onButtonFocusEvent={e => console.log(e, 'BUTTON FOCUS FIRED')}
     onButtonKeyDownEvent={e => console.log(e, 'BUTTON KEYDOWN FIRED')}
+    onSuggestionKeyDownEvent={e => console.log(e, 'SUGGESTION KEYDOWN FIRED')}
+    onBlur={() => setShowSuggestionsState(false)}
+    onFocus={() => setShowSuggestionsState(true)}
+    suggestions={suggestions}
+    showSuggestions={showSuggestions}
   />
 );
 
 const defaultArgs = {
   'button-text': undefined,
-  'input-value': undefined,
+  'input-value': 'benefits',
   'label': undefined,
+  'suggestions': undefined,
+  'show-suggestions': false,
 };
 
 export const Default = Template.bind({});
@@ -53,4 +62,46 @@ export const WithButtonText = Template.bind({});
 WithButtonText.args = {
   ...defaultArgs,
   'button-text': 'Search',
+};
+
+const SuggestionsTemplate = ({
+  'button-text': buttonText,
+  'input-value': inputValue,
+  label,
+  suggestions,
+}) => {
+  const [showSuggestions, setShowSuggestions] = useState(true);
+  return (
+    <div style={{ height: '200px' }}>
+      <VaSearch
+        buttonText={buttonText}
+        inputValue={inputValue}
+        label={label}
+        onInputBlurEvent={e => console.log(e, 'INPUT BLUR FIRED')}
+        onInputChangeEvent={e => console.log(e, 'INPUT CHANGE FIRED')}
+        onInputFocusEvent={e => console.log(e, 'INPUT FOCUS FIRED')}
+        onInputKeyDownEvent={e => console.log(e, 'INPUT KEYDOWN FIRED')}
+        onButtonClickEvent={e => console.log(e, 'BUTTON CLICK FIRED')}
+        onButtonFocusEvent={e => console.log(e, 'BUTTON FOCUS FIRED')}
+        onButtonKeyDownEvent={e => console.log(e, 'BUTTON KEYDOWN FIRED')}
+        onSuggestionKeyDownEvent={e =>
+          console.log(e, 'SUGGESTION KEYDOWN FIRED')
+        }
+        showSuggestions={showSuggestions}
+        suggestions={suggestions}
+        onBlur={() => setShowSuggestions(false)}
+        onFocus={() => setShowSuggestions(true)}
+      />
+    </div>
+  );
+};
+
+export const WithSuggestions = SuggestionsTemplate.bind({});
+WithSuggestions.args = {
+  ...defaultArgs,
+  suggestions: [
+    'benefits for spouses',
+    'benefits for assisted living',
+    'benefits for family',
+  ],
 };
