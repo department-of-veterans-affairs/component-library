49e25a6421b8c5921e945c1caa225f8da5771212
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2022-04-19 09:09:58 -0700

    Change storybook imports for React components (#373)
    * Remove package alias
    
    * Import web-components functions from main package
    
    * Remove Banner stories
    
    This React component is no longer used in vets-website
    
    * Remove CheckboxGroup stories
    
    This component isn't used in vets-website and we have a web component
    replacement
    
    * Remove PromoBanner stories
    
    No longer used and has a web component replacement
    
    * Import React components directly from source
    
    * Remove trailing space in import
    
    I didn't remove this in the macro I recorded :facepalm:

--- packages/storybook/stories/SystemDownView.stories.jsx
+++ packages/storybook/stories/SystemDownView.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {SystemDownView} from '@department-of-veterans-affairs/component-library';
+import SystemDownView from '../../react-components/src/components/SystemDownView/SystemDownView';
 
 export default {
   title: 'Components/SystemDownView',
