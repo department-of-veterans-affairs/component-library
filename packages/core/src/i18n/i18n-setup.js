96ab99cd477d55a08a40a3add4c94f2019ba926b
Andres Rivera <andres.rivera.toro@gmail.com>
2022-04-22 14:23:26 -0400

    chore: remove once property

--- packages/core/src/i18n/i18n-setup.js
+++ packages/core/src/i18n/i18n-setup.js
@@ -20,33 +20,29 @@ i18next.use(languageDetector).init({
 
 export { i18next };
 
-window.addEventListener(
-  'load',
-  event => {
-    console.log('DOM fully loaded and parsed');
+window.addEventListener('load', event => {
+  console.log('DOM fully loaded and parsed');
 
-    const element =
-      document.querySelector('main') || document.getElementById('docs-root');
-    if (!element) console.log('didnt find main');
+  const element =
+    document.querySelector('main') || document.getElementById('docs-root');
+  if (!element) console.log('didnt find main');
 
-    if (element) {
-      console.log('found main to attach event listener', element);
-      const observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-          if (
-            mutation.type === 'attributes' &&
-            mutation.attributeName === 'lang'
-          ) {
-            console.log('firing changeLanguage');
-            i18next.changeLanguage(element.getAttribute('lang'));
-          }
-        });
+  if (element) {
+    console.log('found main to attach event listener', element);
+    const observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (
+          mutation.type === 'attributes' &&
+          mutation.attributeName === 'lang'
+        ) {
+          console.log('firing changeLanguage');
+          i18next.changeLanguage(element.getAttribute('lang'));
+        }
       });
+    });
 
-      observer.observe(element, {
-        attributes: true,
-      });
-    }
-  },
-  { once: true },
-);
+    observer.observe(element, {
+      attributes: true,
+    });
+  }
+});
