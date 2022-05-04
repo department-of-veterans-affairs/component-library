16c275274d5712f7364e3d3b97b84c7f086db885
Andres Rivera <andres.rivera.toro@gmail.com>
2022-05-03 22:17:37 -0400

    chore: update story to include header and step text

--- packages/storybook/stories/va-segmented-progress-bar.stories.jsx
+++ packages/storybook/stories/va-segmented-progress-bar.stories.jsx
@@ -26,12 +26,37 @@ const Template = ({
   // Wrapper for spacing when viewing in storybook
   // Component can be used without it
   <div style={{ margin: '3em' }}>
+    <div
+      class="schemaform-title"
+      style={{ alignItems: 'baseline', display: 'flex', marginBottom: '3rem' }}
+    >
+      <h1
+        data-testid="form-title"
+        style={{ marginBottom: 0, paddingRight: '10px' }}
+      >
+        Apply for health care
+      </h1>
+      <div
+        class="schemaform-subtitle"
+        data-testid="form-subtitle"
+        style={{ fontSize: '2rem' }}
+      >
+        Form 10-10EZ
+      </div>
+    </div>
     <va-segmented-progress-bar
       enable-analytics={enableAnalytics}
       current={current}
       total={total}
       label={label}
     ></va-segmented-progress-bar>
+    <div class="schemaform-chapter-progress" style={{ paddingLeft: '2rem' }}>
+      <div class="nav-header nav-header-schemaform">
+        <h2 id="nav-form-header" class="vads-u-font-size--h4" tabindex="-1">
+          Step 2 of 6: VA Benefits
+        </h2>
+      </div>
+    </div>
   </div>
 );
 
