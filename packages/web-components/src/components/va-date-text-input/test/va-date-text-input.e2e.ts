de3bbd3027452b1b2514eee0f6680e45fcd9c987
Preston Davies <pdavies88@gmail.com>
2022-05-23 14:42:27 -0400

    843 va-date Text Inputs

---
+++ packages/web-components/src/components/va-date-text-input/test/va-date-text-input.e2e.ts
@@ -0,0 +1,208 @@
+import { newE2EPage } from '@stencil/core/testing';
+import { axeCheck } from '../../../testing/test-helpers';
+
+describe('va-date-text-input', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-date-text-input></va-date-text-input>');
+
+    const element = await page.find('va-date-text-input');
+    expect(element).toHaveClass('hydrated');
+  });
+
+  it('passes an axe check', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      `
+      <va-date-text-input day="5" label="Test Label" month="3" name="test" year="2000"></va-date-text-input>
+      `,
+    );
+    await axeCheck(page);
+  });
+
+  it('renders an error message', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-date-text-input error="This is a mistake" />');
+
+    // Render the error message text
+    const error = await page.find('va-date-text-input >>> span.error-message');
+    expect(error.innerText).toContain('This is a mistake');
+  });
+
+  it('renders a required span', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-date-text-input label="This is a field" required />',
+    );
+
+    const requiredSpan = await page.find(
+      'va-date-text-input >>> span.required',
+    );
+    expect(requiredSpan).not.toBeNull();
+  });
+
+  it('sets a label', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-date-text-input label="This is a label" />');
+
+    const label = await page.find('va-date-text-input >>> legend');
+    expect(label.innerText).toContain('This is a label');
+  });
+
+  it('sets a default date', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-date-text-input value="1999-05-03" />');
+
+    const month = await page.find('va-date-text-input >>> .input-month');
+    const day = await page.find('va-date-text-input >>> .input-day');
+    const year = await page.find('va-date-text-input >>> .input-year');
+
+    expect(month.getAttribute('value')).toBe('05');
+    expect(day.getAttribute('value')).toBe('03');
+    expect(year.getAttribute('value')).toBe('1999');
+  });
+
+  it('updates date based select and input fields', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-date-text-input value="1999-05-03" name="test" />',
+    );
+
+    const date = await page.find('va-date-text-input');
+    const elementMonth = await page.find('va-date-text-input >>> .input-month');
+    const elementDay = await page.find('va-date-text-input >>> .input-day');
+    const elementYear = await page.find('va-date-text-input >>> .input-year');
+    const handleMonth = await page.$('pierce/[name="testMonth"]');
+    const handleDay = await page.$('pierce/[name="testDay"]');
+    const handleYear = await page.$('pierce/[name="testYear"]');
+
+    // Click three times to select all text in input
+    await handleMonth.click({ clickCount: 3 });
+    await handleMonth.press('0');
+    await handleMonth.press('7');
+    // Click three times to select all text in input
+    await handleDay.click({ clickCount: 3 });
+    await handleDay.press('2');
+    await handleDay.press('1');
+    // Click three times to select all text in input
+    await handleYear.click({ clickCount: 3 });
+    await handleYear.press('2');
+    await handleYear.press('0');
+    await handleYear.press('2');
+    await handleYear.press('2');
+    await page.waitForChanges();
+
+    expect(elementMonth.getAttribute('value')).toBe('07');
+    expect(elementDay.getAttribute('value')).toBe('21');
+    expect(elementYear.getAttribute('value')).toBe('2022');
+    expect(date.getAttribute('value')).toBe('2022-07-21');
+  });
+
+  it('year input only allows for 4 characters to be used', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-date-text-input name="test" />');
+
+    const elementYear = await page.find('va-date-text-input >>> .input-year');
+    const handleYear = await page.$('pierce/[name="testYear"]');
+    await handleYear.press('2');
+    await handleYear.press('0');
+    await handleYear.press('2');
+    await handleYear.press('2');
+    await handleYear.press('3');
+
+    await page.waitForChanges();
+    expect(elementYear.getAttribute('value')).toBe('2022');
+  });
+
+  it('day and month input only allows for 2 characters to be used', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-date-text-input name="test" />');
+
+    const elementMonth = await page.find('va-date-text-input >>> .input-month');
+    const handleMonth = await page.$('pierce/[name="testMonth"]');
+    await handleMonth.press('2');
+    await handleMonth.press('0');
+    await handleMonth.press('2');
+    await handleMonth.press('2');
+    await handleMonth.press('3');
+
+    const elementDay = await page.find('va-date-text-input >>> .input-day');
+    const handleDay = await page.$('pierce/[name="testDay"]');
+    await handleDay.press('1');
+    await handleDay.press('2');
+    await handleDay.press('3');
+    await handleDay.press('4');
+    await handleDay.press('5');
+
+    await page.waitForChanges();
+    expect(elementMonth.getAttribute('value')).toBe('20');
+    expect(elementDay.getAttribute('value')).toBe('12');
+  });
+
+  it('fires an invalid message if date is invalid and component is required', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-date-text-input value="1999-05-03" name="test" required="true" />',
+    );
+    const date = await page.find('va-date-text-input');
+    const handleYear = await page.$('pierce/[name="testYear"]');
+
+    // Click three times to select all text in input
+    await handleYear.click({ clickCount: 3 });
+    await handleYear.press('2');
+
+    await page.waitForChanges();
+    expect(date.getAttribute('invalid')).toEqual('Please provide a valid date');
+
+    await handleYear.press('0');
+    await handleYear.press('2');
+    await handleYear.press('2');
+
+    await page.waitForChanges();
+    expect(date.getAttribute('invalid')).toBeNull();
+  });
+
+  it('emits dateBlur event', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      '<va-date-text-input value="1999-05-03" name="test" />',
+    );
+
+    const handleMonth = await page.$('pierce/[name="testMonth"]');
+    const blurSpy = await page.spyOnEvent('dateBlur');
+    await handleMonth.press('Tab');
+
+    expect(blurSpy).toHaveReceivedEvent();
+  });
+
+  it('emits dateChange event when select or input value is updated', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      '<va-date-text-input value="1999-05-03" name="test" />',
+    );
+
+    const handleMonth = await page.$('pierce/[name="testMonth"]');
+    const handleDay = await page.$('pierce/[name="testDay"]');
+    const handleYear = await page.$('pierce/[name="testYear"]');
+    const spy = await page.spyOnEvent('dateChange');
+
+    await handleMonth.press('7');
+    // Click three times to select all text in input
+    await handleDay.click({ clickCount: 3 });
+    await handleDay.press('1');
+    await handleDay.press('2');
+
+    expect(spy).toHaveReceivedEventTimes(2);
+
+    // Click three times to select all text in input
+    await handleYear.click({ clickCount: 3 });
+    await handleYear.press('2');
+    await handleYear.press('0');
+    await handleYear.press('2');
+    await handleYear.press('2');
+
+    expect(spy).toHaveReceivedEventTimes(6);
+  });
+});
---
+++ packages/web-components/src/components/va-date-text-input/va-date-text-input.tsx
@@ -0,0 +1,178 @@
+import { Component, Event, EventEmitter, Host, Prop, h } from '@stencil/core';
+
+import { isFullDate } from '../../utils/date-utils';
+@Component({
+  tag: 'va-date-text-input',
+  styleUrl: 'va-date-text-input.css',
+  shadow: true,
+})
+export class VaDateTextInput {
+  /**
+   * Render marker indicating field is required.
+   */
+  @Prop() required: boolean;
+
+  /**
+   * Label for the field.
+   */
+  @Prop() label!: string;
+
+  /**
+   * Used to create unique name attributes for each input.
+   */
+  @Prop() name!: string;
+
+  /**
+   * The error message to render (if any)
+   * This prop should be leveraged to display any custom validations needed for this component
+   */
+  @Prop() error: string;
+
+  /**
+   * Set the default date value must be in YYYY-MM-DD format.
+   */
+  @Prop({ mutable: true }) value: string;
+
+  /**
+   * Fires when the date input loses focus after its value was changed
+   */
+  @Event({
+    composed: true,
+    bubbles: true,
+  })
+  dateChange: EventEmitter;
+
+  /**
+   * Fires when the date input loses focus
+   */
+  @Event({
+    composed: true,
+    bubbles: true,
+  })
+  dateBlur: EventEmitter;
+
+  private handleDateBlur = (event: FocusEvent) => {
+    this.dateBlur.emit(event);
+  };
+
+  private handleDateChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    let [currentYear, currentMonth, currentDay] = (this.value || '').split('-');
+    if (target.classList.contains('input-month')) {
+      currentMonth = target.value;
+    }
+    if (target.classList.contains('input-day')) {
+      currentDay = target.value;
+    }
+    if (target.classList.contains('input-year')) {
+      currentYear = target.value;
+    }
+
+    this.value = `${currentYear}-${currentMonth ? currentMonth : ''}-${
+      currentDay ? currentDay : ''
+    }`;
+
+    // This event should always fire to allow for validation handling
+    this.dateChange.emit(event);
+  };
+
+  /**
+   * Whether or not an analytics event will be fired.
+   */
+  @Prop() enableAnalytics: boolean;
+
+  /**
+   * The event used to track usage of the component. This is emitted when an
+   * input value changes and enableAnalytics is true.
+   */
+  @Event({
+    eventName: 'component-library-analytics',
+    composed: true,
+    bubbles: true,
+  })
+  componentLibraryAnalytics: EventEmitter;
+
+  render() {
+    const {
+      required,
+      label,
+      name,
+      error,
+      handleDateBlur,
+      handleDateChange,
+      value,
+    } = this;
+
+    // Convert string to number to remove leading 0 on values less than 10
+    const [year, month, day] = (value || '').split('-').map(val => val);
+
+    // Check validity of date if invalid provide message and error state styling
+    const dateInvalid =
+      required && !isFullDate(value) ? 'Please provide a valid date' : null;
+
+    // Setting new attribute to avoid conflicts with only using error attribute
+    // Error attribute should be leveraged for custom error messaging
+    // Fieldset has an implicit aria role of group
+    return (
+      <Host value={value} invalid={dateInvalid}>
+        <fieldset aria-label="Select Month and two digit day XX and four digit year format XXXX">
+          <legend>
+            {label} {required && <span class="required">(*Required)</span>}
+          </legend>
+          {(error || dateInvalid) && (
+            <span class="error-message" role="alert">
+              <span class="sr-only">Error</span> {error || dateInvalid}
+            </span>
+          )}
+          <div class="date-container">
+            <va-text-input
+              label="Month"
+              name={`${name}Month`}
+              maxlength={2}
+              minlength={2}
+              pattern="[0-9]*"
+              // Value must be a string
+              // Checking is NaN if so provide empty string
+              value={month ? month.toString() : ''}
+              onInput={handleDateChange}
+              onBlur={handleDateBlur}
+              class="input-month"
+              inputmode="numeric"
+              type="text"
+            />
+            <va-text-input
+              label="Day"
+              name={`${name}Day`}
+              maxlength={2}
+              minlength={2}
+              pattern="[0-9]*"
+              // Value must be a string
+              // Checking is NaN if so provide empty string
+              value={day ? day.toString() : ''}
+              onInput={handleDateChange}
+              onBlur={handleDateBlur}
+              class="input-day"
+              inputmode="numeric"
+              type="text"
+            />
+            <va-text-input
+              label="Year"
+              name={`${name}Year`}
+              maxlength={4}
+              minlength={4}
+              pattern="[0-9]*"
+              // Value must be a string
+              // Checking is NaN if so provide empty string
+              value={year ? year.toString() : ''}
+              onInput={handleDateChange}
+              onBlur={handleDateBlur}
+              class="input-year"
+              inputmode="numeric"
+              type="text"
+            />
+          </div>
+        </fieldset>
+      </Host>
+    );
+  }
+}
