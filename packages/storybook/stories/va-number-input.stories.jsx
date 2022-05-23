09ac5073f1642de3ea87cc22cf45b6d32d90c51f
Preston Davies <pdavies88@users.noreply.github.com>
2022-05-23 14:24:55 -0400

    Update packages/storybook/stories/va-number-input.stories.jsx
    Co-authored-by: Katy Bowman <katy.bowman@adhocteam.us>

--- packages/storybook/stories/va-number-input.stories.jsx
+++ packages/storybook/stories/va-number-input.stories.jsx
@@ -13,7 +13,7 @@ export default {
         component:
           `<a className="vads-c-action-link--blue" href="https://design.va.gov/components/form/number-input">View guidance for the Number Input component in the Design System</a>` +
           '\n' +
-          'This component uses the native event handlers of onInput and onBlur.' +
+          'This component uses the native onInput and onBlur event handlers.' +
           '\n' +
           '\n' +
           generateEventsDescription(numberInputDocs),
