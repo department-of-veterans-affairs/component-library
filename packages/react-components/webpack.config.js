8ad7e153243e61ffd1dd131cbf6ec03a0c36380a
Andres Rivera <andres.rivera.toro@gmail.com>
2022-05-25 10:23:37 -0400

    chore: add webpack config

---
+++ packages/react-components/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            configFile: './config/babel.config.js',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  externals: {
+    'react': 'react',
+    'react-dom': 'react-dom',
+    'i18next': 'i18next',
+    'i18next-browser-languagedetector': 'i18next-browser-languagedetector',
+    '@department-of-veterans-affairs/web-components':
+      '@department-of-veterans-affairs/web-components',
+  },
+};
