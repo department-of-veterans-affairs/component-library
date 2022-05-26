a4afe09d01dcdff733fe1a47cc035944baa8b191
Preston Davies <pdavies88@users.noreply.github.com>
2022-05-25 20:43:35 -0400

    Update packages/web-components/src/components/va-date-text-input/va-date-text-input.tsx
    Co-authored-by: Katy Bowman <katy.bowman@adhocteam.us>

--- packages/web-components/src/components/va-date-text-input/va-date-text-input.tsx
+++ packages/web-components/src/components/va-date-text-input/va-date-text-input.tsx
@@ -132,7 +132,7 @@ export class VaDateTextInput {
               minlength={2}
               pattern="[0-9]*"
               // Value must be a string
-              // Checking is NaN if so provide empty string
+              // if NaN provide empty string
               value={month ? month.toString() : ''}
               onInput={handleDateChange}
               onBlur={handleDateBlur}
