50613e74dc12c60977c30abbb7708fc52d21968b
Andres Rivera <andres.rivera.toro@gmail.com>
2022-05-27 10:41:51 -0400

    chore: add maturity scale icons to sidebar

--- packages/storybook/.storybook/manager.js
+++ packages/storybook/.storybook/manager.js
@@ -1,5 +1,49 @@
 import { addons } from '@storybook/addons';
+import React from 'react';
+import { additionalDocs } from '../stories/additional-docs';
 
 addons.setConfig({
   enableShortcuts: false,
+  sidebar: {
+    renderLabel: item => {
+      if (item.parent === 'components') {
+        const maturityCategory =
+          additionalDocs[item.name] &&
+          additionalDocs[item.name].maturityCategory;
+
+        let backgroundColor;
+        switch (maturityCategory) {
+          case 'USE':
+            backgroundColor = '#195c27';
+            break;
+          case 'USE WITH CAUTION':
+            backgroundColor = '#eb7f29';
+            break;
+          case "DON'T USE":
+            backgroundColor = '#981b1e';
+            break;
+        }
+
+        if (!backgroundColor) return item.name;
+
+        return (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div
+              style={{
+                height: '10px',
+                width: '10px',
+                backgroundColor,
+                borderRadius: '50%',
+                display: 'inline-block',
+                marginRight: '4px',
+              }}
+            ></div>
+            {item.name}
+          </div>
+        );
+      }
+
+      return <>{item.name}</>;
+    },
+  },
 });
