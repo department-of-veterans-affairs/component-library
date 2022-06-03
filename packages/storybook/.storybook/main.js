307a1dda277dfaf04ab5c0ff2c8bafea0ea73369
Andres Rivera <andres.rivera.toro@gmail.com>
2022-06-03 13:13:53 -0400

    feat: add a11y and storysource addons

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -4,9 +4,24 @@ module.exports = {
   stories: ['../@(src|stories)/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-a11y',
-    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-controls',
     '@storybook/addon-links',
-    '@whitespace/storybook-addon-html'
+    '@whitespace/storybook-addon-html',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        sourceLoaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
+    '@storybook/addon-measure',
+    '@storybook/addon-outline',
+    '@storybook/addon-storysource',
+    '@storybook/addon-toolbars',
+    '@storybook/addon-viewport',
   ],
   core: {
     builder: 'webpack5',
