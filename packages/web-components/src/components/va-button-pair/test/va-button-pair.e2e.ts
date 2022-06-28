8e196d72cff854cc62d2933d05763d7056ce4e7b
Andres Rivera <andres.rivera.toro@gmail.com>
2022-06-28 11:32:38 -0400

    test: add click event tests

--- packages/web-components/src/components/va-button-pair/test/va-button-pair.e2e.ts
+++ packages/web-components/src/components/va-button-pair/test/va-button-pair.e2e.ts
@@ -5,14 +5,31 @@ describe('va-button-pair', () => {
   it('renders', async () => {
     const page = await newE2EPage();
     await page.setContent('<va-button-pair></va-button-pair>');
-
     const element = await page.find('va-button-pair');
     expect(element).toHaveClass('hydrated');
   });
 
   it('passes an axe check', async () => {
     const page = await newE2EPage();
-    await page.setContent('<va-button-pair></va-button-pair>');
+    await page.setContent('<va-button-pair continue></va-button-pair>');
     await axeCheck(page);
+  });
+
+  it('fires primaryClick event when clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-button-pair></va-button-pair>');
+    const primaryClickEvent = await page.spyOnEvent('primaryClick');
+    const primaryButton = await page.find('va-button-pair >>> va-button');
+    await primaryButton.click();
+    expect(primaryClickEvent).toHaveReceivedEventTimes(1);
+  });
+
+  it('fires secondaryClick event when clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-button-pair></va-button-pair>');
+    const secondaryClickEvent = await page.spyOnEvent('secondaryClick');
+    const buttons = await page.findAll('va-button-pair >>> va-button');
+    await buttons[1].click();
+    expect(secondaryClickEvent).toHaveReceivedEventTimes(1);
   });
 });
