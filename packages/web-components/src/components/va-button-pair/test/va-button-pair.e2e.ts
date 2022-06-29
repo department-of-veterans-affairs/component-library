5b1ea6289ccb66f555581374d7641726673ca8cf
Andres Rivera <andres.rivera.toro@gmail.com>
2022-06-29 11:40:24 -0400

    chore: fix tests

--- packages/web-components/src/components/va-button-pair/test/va-button-pair.e2e.ts
+++ packages/web-components/src/components/va-button-pair/test/va-button-pair.e2e.ts
@@ -17,7 +17,7 @@ describe('va-button-pair', () => {
 
   it('fires analytics event when primary button is clicked', async () => {
     const page = await newE2EPage();
-    await page.setContent('<va-button-pair></va-button-pair>');
+    await page.setContent('<va-button-pair confirm></va-button-pair>');
     const analyticsSpy = await page.spyOnEvent('component-library-analytics');
     const primaryButton = await page.find('va-button-pair >>> va-button');
     await primaryButton.click();
@@ -31,21 +31,7 @@ describe('va-button-pair', () => {
 
   it('fires analytics event when secondary button is clicked', async () => {
     const page = await newE2EPage();
-    await page.setContent('<va-button-pair></va-button-pair>');
-    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
-    const buttons = await page.findAll('va-button-pair >>> va-button');
-    await buttons[1].click();
-    expect(analyticsSpy).toHaveReceivedEventDetail({
-      action: 'click',
-      componentName: 'va-button-pair',
-      // TODO: update analytics event details
-      details: {},
-    });
-  });
-
-  it('fires analytics event when secondary button is clicked', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<va-button-pair></va-button-pair>');
+    await page.setContent('<va-button-pair confirm></va-button-pair>');
     const analyticsSpy = await page.spyOnEvent('component-library-analytics');
     const buttons = await page.findAll('va-button-pair >>> va-button');
     await buttons[1].click();
@@ -70,7 +56,7 @@ describe('va-button-pair', () => {
 
   it('fires primaryClick event when clicked', async () => {
     const page = await newE2EPage();
-    await page.setContent('<va-button-pair></va-button-pair>');
+    await page.setContent('<va-button-pair confirm></va-button-pair>');
     const primaryClickEvent = await page.spyOnEvent('primaryClick');
     const primaryButton = await page.find('va-button-pair >>> va-button');
     await primaryButton.click();
@@ -79,7 +65,7 @@ describe('va-button-pair', () => {
 
   it('fires secondaryClick event when clicked', async () => {
     const page = await newE2EPage();
-    await page.setContent('<va-button-pair></va-button-pair>');
+    await page.setContent('<va-button-pair confirm></va-button-pair>');
     const secondaryClickEvent = await page.spyOnEvent('secondaryClick');
     const buttons = await page.findAll('va-button-pair >>> va-button');
     await buttons[1].click();
