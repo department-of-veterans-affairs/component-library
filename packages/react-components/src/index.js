a188144ab33f84f050bc91e5946dfa18c9a8dffd
Andres Rivera <andres.rivera.toro@gmail.com>
2022-07-06 11:31:08 -0400

    chore: remove Banner, CollapsiblePanel, PromoBanner and TextArea

--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -1,11 +1,9 @@
 import AcceptTermsPrompt from './components/AcceptTermsPrompt/AcceptTermsPrompt';
 import AdditionalInfo from './components/AdditionalInfo/AdditionalInfo';
 import AlertBox, { ALERT_TYPE } from './components/AlertBox/AlertBox';
-import Banner from './components/Banner/Banner';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
 import Checkbox from './components/Checkbox/Checkbox';
 import CheckboxGroup from './components/CheckboxGroup/CheckboxGroup';
-import CollapsiblePanel from './components/CollapsiblePanel/CollapsiblePanel';
 import Date from './components/Date/Date';
 import DropDownPanel from './components/DropDownPanel/DropDownPanel';
 import ExpandingGroup from './components/ExpandingGroup/ExpandingGroup';
@@ -24,9 +22,6 @@ import OMBInfo from './components/OMBInfo/OMBInfo';
 import Pagination from './components/Pagination/Pagination';
 import PrivacyAgreement from './components/PrivacyAgreement/PrivacyAgreement';
 import ProgressButton from './components/ProgressButton/ProgressButton';
-import PromoBanner, {
-  PROMO_BANNER_TYPES,
-} from './components/PromoBanner/PromoBanner';
 import RadioButtons from './components/RadioButtons/RadioButtons';
 import Select from './components/Select/Select';
 import SystemDownView from './components/SystemDownView/SystemDownView';
@@ -37,7 +32,6 @@ import Telephone, {
   PATTERNS as PHONE_PATTERNS,
   patternsMap as PHONE_PATTERNS_MAP,
 } from './components/Telephone/Telephone';
-import TextArea from './components/TextArea/TextArea';
 import TextInput from './components/TextInput/TextInput';
 
 export {
@@ -45,11 +39,9 @@ export {
   AdditionalInfo,
   AlertBox,
   ALERT_TYPE,
-  Banner,
   Breadcrumbs,
   Checkbox,
   CheckboxGroup,
-  CollapsiblePanel,
   Date,
   DropDownPanel,
   ExpandingGroup,
@@ -68,8 +60,6 @@ export {
   Pagination,
   PrivacyAgreement,
   ProgressButton,
-  PromoBanner,
-  PROMO_BANNER_TYPES,
   RadioButtons,
   Select,
   SystemDownView,
@@ -79,6 +69,5 @@ export {
   contactsMap,
   PHONE_PATTERNS,
   PHONE_PATTERNS_MAP,
-  TextArea,
   TextInput,
 };
