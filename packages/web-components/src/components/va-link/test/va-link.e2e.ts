33316120db8565ab368a3b31a1a264414c38f47f
Andres Rivera <andres.rivera.toro@gmail.com>
2022-07-11 17:18:19 -0400

    test: add variant links

--- packages/web-components/src/components/va-link/test/va-link.e2e.ts
+++ packages/web-components/src/components/va-link/test/va-link.e2e.ts
@@ -2,7 +2,7 @@ import { newE2EPage } from '@stencil/core/testing';
 import { axeCheck } from '../../../testing/test-helpers';
 
 describe('va-link', () => {
-  it('renders', async () => {
+  it('renders default link', async () => {
     const page = await newE2EPage();
     await page.setContent(
       '<va-link href="https://www.va.gov">Find out if you qualify for this program and how to apply</va-link>',
@@ -18,6 +18,86 @@ describe('va-link', () => {
         </a>
       </mock:shadow-root>
       Find out if you qualify for this program and how to apply
+    </va-link>
+    `);
+  });
+
+  it('renders active link', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-link active href="https://www.va.gov">Share your VA medical records</va-link>',
+    );
+
+    const element = await page.find('va-link');
+    expect(element).toEqualHtml(`
+    <va-link active class="hydrated" href="https://www.va.gov">
+      <mock:shadow-root>
+        <a href="https://www.va.gov">
+          <slot></slot>
+          <i aria-hidden="true"></i>
+        </a>
+      </mock:shadow-root>
+      Share your VA medical records
+    </va-link>
+    `);
+  });
+
+  it('renders download link', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-link download filename="10-10ez.pdf" href="https://www.va.gov">Download VA form 10-10EZ(PDF, 5 pages)</va-link>',
+    );
+
+    const element = await page.find('va-link');
+    expect(element).toEqualHtml(`
+    <va-link class="hydrated" download filename="10-10ez.pdf" href="https://www.va.gov">
+      <mock:shadow-root>
+        <a download="10-10ez.pdf" href="https://www.va.gov">
+          <i aria-hidden="true"></i>
+          <slot></slot>
+        </a>
+      </mock:shadow-root>
+      Download VA form 10-10EZ(PDF, 5 pages)
+    </va-link>
+    `);
+  });
+
+  it('renders video link', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-link video href="https://www.va.gov">Go to the video about VA disability compensation on YouTube</va-link>',
+    );
+
+    const element = await page.find('va-link');
+    expect(element).toEqualHtml(`
+    <va-link class="hydrated" video href="https://www.va.gov">
+      <mock:shadow-root>
+        <a href="https://www.va.gov" rel="noopener" target="_blank">
+          <i aria-hidden="true"></i>
+          <slot></slot>
+        </a>
+      </mock:shadow-root>
+      Go to the video about VA disability compensation on YouTube
+    </va-link>
+    `);
+  });
+
+  it('renders channel link', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-link channel href="https://www.va.gov">VA YouTube</va-link>',
+    );
+
+    const element = await page.find('va-link');
+    expect(element).toEqualHtml(`
+    <va-link class="hydrated" channel href="https://www.va.gov">
+      <mock:shadow-root>
+        <a href="https://www.va.gov" rel="noopener" target="_blank">
+          <i aria-hidden="true"></i>
+          <slot></slot>
+        </a>
+      </mock:shadow-root>
+      VA YouTube
     </va-link>
     `);
   });
