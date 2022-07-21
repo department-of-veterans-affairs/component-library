19b2a9b939c0b11d70ed776173f3643d9dc1172d
Andres Rivera <andres.rivera.toro@gmail.com>
2022-07-20 21:46:07 -0400

    chore: handle modal content and slot logic

--- packages/web-components/src/components/va-omb-info/va-omb-info.tsx
+++ packages/web-components/src/components/va-omb-info/va-omb-info.tsx
@@ -1,4 +1,5 @@
-import { Component, Host, h, Prop, State } from '@stencil/core';
+import { Component, Host, h, Prop, State, Element } from '@stencil/core';
+import { getSlottedNodes } from '../../utils/utils';
 
 @Component({
   tag: 'va-omb-info',
@@ -6,6 +7,9 @@ import { Component, Host, h, Prop, State } from '@stencil/core';
   shadow: true,
 })
 export class VaOmbInfo {
+  @Element() el: HTMLElement;
+
+  modalContents: HTMLDivElement | null = null;
   /**
    * If `true`, privacy act statement modal will be visible.
    */
@@ -37,10 +41,69 @@ export class VaOmbInfo {
     this.visible = !this.visible;
   };
 
+  private handleSlotChange = () => {
+    if (getSlottedNodes(this.el, null)?.length) return;
+
+    /* eslint-disable i18next/no-literal-string */
+    this.modalContents = (
+      <div>
+        <h3>Privacy Act Statement</h3>
+        {this.resBurden && (
+          <p>
+            <span>Respondent Burden:</span> We need this information to
+            determine your eligibility for {this.benefitType} (38 U.S.C. 3471).
+            Title 38, United States Code, allows us to ask for this information.
+            We estimate that you will need an average of {this.resBurden}{' '}
+            minutes to review the instructions, find the information, and
+            complete this form. The VA cannot conduct or sponsor a collection of
+            information unless a valid OMB (Office of Management and Budget)
+            control number is displayed. You are not required to respond to a
+            collection of information if this number is not displayed. Valid OMB
+            control numbers can be located on the OMB Internet Page at
+            www.reginfo.gov/public/do/PRAMain. If desired, you can call{' '}
+            <a href="+18008271000">1-800-827-1000</a> to get information on
+            where to send comments or suggestions about this form.
+          </p>
+        )}
+        <p>
+          <span>Privacy Act Notice:</span> The VA will not disclose information
+          collected on this form to any source other than what has been
+          authorized under the Privacy Act of 1974 or title 38, Code of Federal
+          Regulations, section 1.576 for routine uses (e.g., the VA sends
+          educational forms or letters with a veteran’s identifying information
+          to the Veteran’s school or training establishment to (1) assist the
+          veteran in the completion of claims forms or (2) for the VA to obtain
+          further information as may be necessary from the school for VA to
+          properly process the Veteran’s education claim or to monitor his or
+          her progress during training) as identified in the VA system of
+          records, 58VA21/22/28, Compensation, Pension, Education, and
+          Vocational Rehabilitation and Employment Records - VA, and published
+          in the Federal Register. Your obligation to respond is required to
+          obtain or retain
+          {this.benefitType}. Giving us your SSN account information is
+          voluntary. Refusal to provide your SSN by itself will not result in
+          the denial of benefits. The VA will not deny an individual benefits
+          for refusing to provide his or her SSN unless the disclosure of the
+          SSN is required by a Federal Statute of law enacted before January 1,
+          1975, and still in effect. The requested information is considered
+          relevant and necessary to determine the maximum benefits under the
+          law. While you do not have to respond, VA cannot process your claim
+          for education assistance unless the information is furnished as
+          required by existing law (38 U.S.C. 3471). The responses you submit
+          are considered confidential (38 U.S.C. 5701). Any information provided
+          by applicants, recipients, and others may be subject to verification
+          through computer matching programs with other agencies.
+        </p>
+      </div>
+    );
+    /* eslint-enable i18next/no-literal-string */
+  };
+
   render() {
     const {
-      benefitType,
       expDate,
+      handleSlotChange,
+      modalContents,
       ombNumber,
       resBurden,
       toggleModalVisible,
@@ -64,14 +127,19 @@ export class VaOmbInfo {
           Expiration date: <span>{expDate}</span>
         </div>
         <div>
-          <button onClick={toggleModalVisible}>Privacy Act Statement</button>
+          <va-button
+            onClick={toggleModalVisible}
+            text="Privacy Act Statement"
+          />
         </div>
-        {/* <slot></slot> */}
         <va-modal
           modalTitle="Privacy Act Statement"
           onCloseEvent={toggleModalVisible}
           visible={visible}
-        ></va-modal>
+        >
+          <slot onSlotchange={handleSlotChange}></slot>
+          {modalContents}
+        </va-modal>
       </Host>
     );
   }
