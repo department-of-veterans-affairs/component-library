765c518be998e8eab56a43cb38159537104aeb3c
Andres Rivera <andres.rivera.toro@gmail.com>
2022-07-21 23:26:20 -0400

    chore: add story and examples

---
+++ packages/storybook/stories/va-omb-info.stories.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { getWebComponentDocs, propStructure, StoryDocs } from './wc-helpers';
+
+const ombInfoDocs = getWebComponentDocs('va-omb-info');
+
+export default {
+  title: 'Components/va-omb-info',
+  parameters: {
+    componentSubtitle: `OMB info web component`,
+    docs: {
+      page: () => <StoryDocs data={ombInfoDocs} />,
+    },
+  },
+};
+
+const defaultArgs = {
+  'benefit-type': undefined,
+  'exp-date': undefined,
+  'omb-number': undefined,
+  'res-burden': undefined,
+};
+
+const Template = ({
+  'benefit-type': benefitType,
+  'exp-date': expDate,
+  'omb-number': ombNumber,
+  'res-burden': resBurden,
+}) => {
+  return (
+    <va-omb-info
+      benefit-type={benefitType}
+      exp-date={expDate}
+      omb-number={ombNumber}
+      res-burden={resBurden}
+    />
+  );
+};
+
+export const Default = Template.bind(null);
+Default.args = {
+  ...defaultArgs,
+  'benefit-type': undefined,
+  'exp-date': '12/31/2077',
+  'omb-number': '12-3456',
+  'res-burden': 120,
+};
+Default.argTypes = propStructure(ombInfoDocs);
+
+export const WithoutOMBNumber = Template.bind(null);
+WithoutOMBNumber.args = {
+  ...defaultArgs,
+  'exp-date': '12/31/2077',
+  'res-burden': 120,
+};
+
+export const WithoutResponseBurden = Template.bind(null);
+WithoutResponseBurden.args = {
+  ...defaultArgs,
+  'exp-date': '12/31/2077',
+  'omb-number': '12-3456',
+};
+
+export const WithCustomRespondentBurdenBenefitType = Template.bind(null);
+WithCustomRespondentBurdenBenefitType.args = {
+  ...defaultArgs,
+  'benefit-type': 'EXAMPLE BENEFITS',
+  'exp-date': '12/31/2077',
+  'omb-number': '12-3456',
+  'res-burden': 90,
+};
