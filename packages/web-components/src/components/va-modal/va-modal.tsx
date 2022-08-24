313f689371d692a74b6566470524875cadec16b7
Andrés Rivera Toro <andres.rivera.toro@gmail.com>
2022-08-24 18:52:30 -0400

    Fix va-modal for Cypress (#517)
    * chore: reflect visible and change html structure
    
    * chore: update prop to reflect
    
    * chore: increment patch version

--- packages/web-components/src/components/va-modal/va-modal.tsx
+++ packages/web-components/src/components/va-modal/va-modal.tsx
@@ -127,7 +127,7 @@ export class VaModal {
   /**
    * If the modal is visible or not
    */
-  @Prop() visible?: boolean = false;
+  @Prop({ reflect: true }) visible?: boolean = false;
 
   /**
    * Local state to track if the shift key is pressed
@@ -354,59 +354,51 @@ export class VaModal {
       : 'Close modal';
 
     return (
-      <Host
-      aria-label={ariaLabel}
-      aria-modal="true"
-      role={ariaRole(status)}
-      >
-        <div
-          class="va-modal"
-        >
-          <div class={wrapperClass} tabIndex={-1}>
-            <button
-              aria-label={btnAriaLabel}
-              class="va-modal-close"
-              onClick={e => this.handleClose(e)}
-              ref={el => (this.closeButton = el as HTMLButtonElement)}
-              type="button"
-            >
-              <i aria-hidden="true" />
-            </button>
-            <div class={bodyClass}>
-              <div role="document">
-                {modalTitle && (
-                  <h1 class={titleClass} tabIndex={-1}>
-                    {modalTitle}
-                  </h1>
-                )}
-                <slot></slot>
-              </div>
-              {((primaryButtonClick && primaryButtonText) ||
-                (secondaryButtonClick && secondaryButtonText)) && (
-                <div
-                  class="alert-actions"
-                  ref={el => (this.alertActions = el as HTMLDivElement)}
-                >
-                  {primaryButtonClick && primaryButtonText && (
-                    <button
-                      onClick={e => this.handlePrimaryButtonClick(e)}
-                      type="button"
-                    >
-                      {primaryButtonText}
-                    </button>
-                  )}
-                  {secondaryButtonClick && secondaryButtonText && (
-                    <button
-                      class="button-secondary"
-                      onClick={e => this.handleSecondaryButtonClick(e)}
-                      type="button"
-                    >
-                      {secondaryButtonText}
-                    </button>
-                  )}
-                </div>
+      <Host aria-label={ariaLabel} aria-modal="true" role={ariaRole(status)}>
+        <div class={wrapperClass} tabIndex={-1}>
+          <button
+            aria-label={btnAriaLabel}
+            class="va-modal-close"
+            onClick={e => this.handleClose(e)}
+            ref={el => (this.closeButton = el as HTMLButtonElement)}
+            type="button"
+          >
+            <i aria-hidden="true" />
+          </button>
+          <div class={bodyClass}>
+            <div role="document">
+              {modalTitle && (
+                <h1 class={titleClass} tabIndex={-1}>
+                  {modalTitle}
+                </h1>
               )}
+              <slot></slot>
             </div>
+            {((primaryButtonClick && primaryButtonText) ||
+              (secondaryButtonClick && secondaryButtonText)) && (
+              <div
+                class="alert-actions"
+                ref={el => (this.alertActions = el as HTMLDivElement)}
+              >
+                {primaryButtonClick && primaryButtonText && (
+                  <button
+                    onClick={e => this.handlePrimaryButtonClick(e)}
+                    type="button"
+                  >
+                    {primaryButtonText}
+                  </button>
+                )}
+                {secondaryButtonClick && secondaryButtonText && (
+                  <button
+                    class="button-secondary"
+                    onClick={e => this.handleSecondaryButtonClick(e)}
+                    type="button"
+                  >
+                    {secondaryButtonText}
+                  </button>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </Host>
