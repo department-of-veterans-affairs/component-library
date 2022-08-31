8f86ae8116dbbfb2ca6b6519e1eeaa2755d0e263
Jami Gibbs <jami.gibbs@adhocteam.us>
2022-08-31 13:50:12 -0500

    Updating tests for slot-wrap element.

--- packages/web-components/src/components/va-alert-expandable/test/va-alert-expandable.e2e.ts
+++ packages/web-components/src/components/va-alert-expandable/test/va-alert-expandable.e2e.ts
@@ -24,7 +24,7 @@ describe('va-alert-expandable', () => {
               <i class="fa-angle-down" role="presentation"></i>
             </div>
           </a>
-          <div id="alert-body" class="alert-expandable-body closed" style="--calc-max-height:calc(38px + 2rem);"><div id="slot-wrap"><slot></slot></div></div>
+          <div id="alert-body" class="alert-expandable-body closed" style="--calc-max-height:calc(20px + 2rem);"><div id="slot-wrap"><slot></slot></div></div>
         </div>
         </mock:shadow-root>
       </va-alert-expandable>
@@ -225,8 +225,8 @@ describe('va-alert-expandable', () => {
     const calcMaxHeight = await handle.evaluate((domElement: HTMLElement) =>
       domElement.style.getPropertyValue('--calc-max-height'),
     );
-    // 50px from height + 20px from inline padding + 38px from #slot-wrap element.
+    // 50px from height + 20px from inline padding + 39px from #slot-wrap element.
     // margin-bottom and margin-top is set to 0 for first slotted child.
-    expect(calcMaxHeight).toEqual('calc(108px + 2rem)');
+    expect(calcMaxHeight).toEqual('calc(109px + 2rem)');
   });
 });
