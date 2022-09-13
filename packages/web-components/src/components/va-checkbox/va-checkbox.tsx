ea98fda7e5f6c600b866c900f7b000aab9f7a57b
Jami Gibbs <jami.gibbs@adhocteam.us>
2022-09-13 15:16:52 -0500

    Cleaning up formatting.

--- packages/web-components/src/components/va-checkbox/va-checkbox.tsx
+++ packages/web-components/src/components/va-checkbox/va-checkbox.tsx
@@ -136,8 +136,9 @@ export class VaCheckbox {
           onChange={this.handleChange}
         />
         <label htmlFor="checkbox-element">
-          <span>{label}
-          {required && <span class="required">(*Required)</span>}</span>
+          <span>
+            {label} {required && <span class="required">(*Required)</span>}
+          </span>
         </label>
       </Host>
     );
