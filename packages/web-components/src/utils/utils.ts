c2a693c352e1e11bce842962714a115c61682b86
Brooks Johnson <brooks.johnson@pm.me>
2022-10-10 09:09:52 -0700

    Fix the indentation
    We still don't have prettier setup

--- packages/web-components/src/utils/utils.ts
+++ packages/web-components/src/utils/utils.ts
@@ -30,10 +30,10 @@ export function getSlottedNodes(
       (node: any) => node.tagName === 'SLOT',
     ).length > 0;
 
-    // This will only get the first slot on a component
-    const children = hasShadowDOM
-      ? root.shadowRoot.querySelector('slot').assignedNodes()
-      : root.shadowRoot.childNodes;
+  // This will only get the first slot on a component
+  const children = hasShadowDOM
+    ? root.shadowRoot.querySelector('slot').assignedNodes()
+    : root.shadowRoot.childNodes;
 
   return nodeName !== null ? Array.from(children).filter(
     item => item.nodeName.toLowerCase() === nodeName,
