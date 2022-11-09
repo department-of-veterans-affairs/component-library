dcfbbb2ed06f150d22be777c593a1c9f673b09b1
Jami Gibbs <jami.gibbs@adhocteam.us>
2022-11-09 14:59:29 -0600

    Remove global settings scss.

--- packages/web-components/stencil.config.ts
+++ packages/web-components/stencil.config.ts
@@ -10,9 +10,6 @@ export const config: Config = {
   plugins: [
     sass({ 
       includePaths: ["../../node_modules/@uswds/uswds/packages/"],
-      // injectGlobalPaths: [
-      //   'src/global/settings.scss',
-      // ]
     }),
     postcss({
       plugins: [url({ url: 'inline' })],
