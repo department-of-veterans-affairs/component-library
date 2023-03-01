34b6c9f37b9c325cd5f69f676363813656455d21
Andrew Steele <andrew.steele@adhocteam.us>
2023-03-01 13:01:16 -0500

    Mitigate Radio and Radio Option Formation Problems (#634)
    * Formation overrides for va-text-input component
    
    * Adding usa-hint font size override
    
    * Updating storybook build
    
    * Revert "Updating storybook build"
    
    This reverts commit 694c98aeafdd8350cd5a62317430a1623cad73dc.
    
    * Renaming base-font-size to base-v3-font-size
    
    * Version bumps
    
    * Update package.json
    
    Reverting "main" version, unneeded.
    
    * Further improvements to text-input and storybook story
    
    * Formation mitigation for Radio and Radio Option
    
    * Version bump
    
    * Improved radio option buttons
    
    * Fix to radio option label

--- packages/storybook/stories/va-radio-uswds.stories.jsx
+++ packages/storybook/stories/va-radio-uswds.stories.jsx
@@ -33,7 +33,7 @@ const vaRadioConst = args => {
     label,
     hint,
     required,
-    uswds,
+    uswds = true,
     'label-header-level': labelHeaderLevel,
     ...rest
   } = args;
@@ -92,13 +92,21 @@ const I18nTemplate = args => {
 
   return (
     <div>
-      <button style={{fontSize: '1rem'}} onClick={e => setLang('es')}>Español</button>
-      <button style={{fontSize: '1rem'}} onClick={e => setLang('en')}>English</button>
-      <button style={{fontSize: '1rem'}} onClick={e => setLang('tl')}>Tagalog</button>
-      <br /><br />
+      <button style={{ fontSize: '16px' }} onClick={e => setLang('es')}>
+        Español
+      </button>
+      <button style={{ fontSize: '16px' }} onClick={e => setLang('en')}>
+        English
+      </button>
+      <button style={{ fontSize: '16px' }} onClick={e => setLang('tl')}>
+        Tagalog
+      </button>
+      <br />
+      <br />
       {vaRadioConst(args)}
     </div>
-)};
+  );
+};
 
 const ReactBindingExample = ({
   'enable-analytics': enableAnalytics,
