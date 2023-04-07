7da4d51fd85e258f230a966745ee665e405a2624
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-04-07 12:35:20 -0500

    Add tests for va-text-input

--- packages/web-components/src/components/va-text-input/test/va-text-input.e2e.ts
+++ packages/web-components/src/components/va-text-input/test/va-text-input.e2e.ts
@@ -355,7 +355,7 @@ describe('va-text-input', () => {
     expect(await inputEl.getProperty('autocomplete')).toBe('email');
   });
 
-  // Begin USWDS v3 test
+  // Begin USWDS tests
   it('uswds v3 renders', async () => {
     const page = await newE2EPage();
 
@@ -374,6 +374,300 @@ describe('va-text-input', () => {
         </mock:shadow-root>
       </va-text-input>
     `);
+  });
+
+  it('uswds renders an error message', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input error="This is a mistake" uswds />');
+
+    // Render the error message text
+    const error = await page.find('va-text-input >>> .usa-error-message');
+    const input = await page.find('va-text-input >>> input');
+    expect(error.innerText).toContain('This is a mistake');
+    expect(input.getAttribute('aria-invalid')).toEqual('true');
+  });
+
+  it('uswds sets aria-invalid based on invalid prop', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input invalid uswds />');
+
+    const input = await page.find('va-text-input >>> input');
+    expect(input.getAttribute('aria-invalid')).toEqual('true');
+  });
+
+  it('uswds adds aria-describedby for error message', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input uswds />');
+    // Check that error is empty
+    const el = await page.find('va-text-input');
+    const inputEl = await page.find('va-text-input >>> input');
+    expect(inputEl.getAttribute('aria-describedby')).toBeNull();
+    // Render the error message text as empty string
+    el.setProperty('error', '');
+    await page.waitForChanges();
+    expect(inputEl.getAttribute('aria-describedby')).toBeNull();
+    // Render the error message text as real value
+    el.setProperty('error', 'Testing Error');
+    await page.waitForChanges();
+    expect(inputEl.getAttribute('aria-describedby')).not.toBeNull();
+    expect(inputEl.getAttribute('aria-describedby')).toContain('error-message');
+  });
+
+  it('uswds adds aria-describedby input-message id', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input message-aria-describedby="example message" uswds />');
+    const el = await page.find('va-text-input');
+    const inputEl = await page.find('va-text-input >>> input');
+
+    // Render the example message aria-describedby id.
+    expect(inputEl.getAttribute('aria-describedby')).not.toBeNull();
+    expect(inputEl.getAttribute('aria-describedby')).toContain('input-message');
+
+    // If an error and aria-describedby-message is set, id's exist in aria-describedby.
+    el.setProperty('error', 'Testing Error');
+    await page.waitForChanges();
+    expect(inputEl.getAttribute('aria-describedby')).not.toBeNull();
+    expect(inputEl.getAttribute('aria-describedby')).toContain('error-message');
+    expect(inputEl.getAttribute('aria-describedby')).toContain('input-message');
+  });
+
+  it('uswds renders a required span', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-text-input label="This is a field" required uswds />',
+    );
+
+    const requiredSpan = await page.find(
+      'va-text-input >>> label > span.usa-label--required',
+    );
+    // The actual text value depends on the language of the document
+    expect(requiredSpan).not.toBeNull();
+  });
+
+  it('uswds renders hint text', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input hint="This is hint text" uswds />');
+
+    // Render the hint text
+    const hintTextElement = await page.find('va-text-input >>> .usa-hint');
+    expect(hintTextElement.innerText).toContain('This is hint text');
+  });
+
+  it('uswds passes an aXe check', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      '<va-text-input required label="This is a test" error="With an error message" message-aria-describeby="with extra aria message" uswds />',
+    );
+
+    await axeCheck(page);
+  });
+
+  it('uswds fires an analytics event when enableAnalytics is true', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      '<va-text-input label="Input Field" enable-analytics uswds />',
+    );
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+
+    const inputEl = await page.find('va-text-input >>> input');
+    await inputEl.press('1');
+    await inputEl.press('2');
+    await inputEl.press('3');
+    await inputEl.press('Tab');
+
+    expect(analyticsSpy).toHaveReceivedEventDetail({
+      action: 'blur',
+      componentName: 'va-text-input',
+      details: {
+        label: 'Input Field',
+        value: '123',
+      },
+    });
+  });
+
+  it('uswds emits blur event uswds', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-text-input label="Input Field" uswds />');
+
+    const inputEl = await page.find('va-text-input >>> input');
+    const blurSpy = await page.spyOnEvent('blur');
+    await inputEl.press('Tab');
+
+    expect(blurSpy).toHaveReceivedEvent();
+  });
+
+  it('uswds emits input event with value updated', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-text-input label="Input Field" uswds />');
+
+    const inputEl = await page.find('va-text-input >>> input');
+    const inputSpy = await page.spyOnEvent('input');
+
+    // Act
+    await inputEl.press('a');
+    const firstValue = await page.$eval(
+      'va-text-input',
+      (comp: HTMLInputElement) => comp.value,
+    );
+    await inputEl.press('s');
+    const secondValue = await page.$eval(
+      'va-text-input',
+      (comp: HTMLInputElement) => comp.value,
+    );
+
+    // Assert
+    expect(inputSpy).toHaveReceivedEventTimes(2);
+    expect(firstValue).toEqual('a');
+    expect(secondValue).toEqual('as');
+  });
+
+  it("uswds doesn't fire analytics events", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-text-input label="Input Field" uswds />');
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+    const inputEl = await page.find('va-text-input >>> input');
+    await inputEl.press('1');
+    await inputEl.press('Tab');
+
+    expect(analyticsSpy).not.toHaveReceivedEvent();
+  });
+
+  it('uswds adds a character limit with descriptive text', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-text-input minlength="2" maxlength="3" value="22" uswds />',
+    );
+
+    // Level-setting expectations
+    const inputEl = await page.find('va-text-input >>> input');
+    expect(await inputEl.getProperty('value')).toBe('22');
+    expect(await page.find('va-text-input >>> small')).toBeNull();
+
+    // Test the functionality
+    await inputEl.press('2');
+    expect(await inputEl.getProperty('value')).toBe('222');
+    expect((await page.find('va-text-input >>> small')).innerText).toContain(
+      'max-chars',
+    );
+
+    // Click three times to select all text in input
+    await inputEl.click({ clickCount: 3 });
+    await inputEl.press('2');
+    expect(await inputEl.getProperty('value')).toBe('2');
+    expect((await page.find('va-text-input >>> small')).innerText).toContain(
+      'min-chars',
+    );
+  });
+
+  it('uswds ignores negative maxlength values', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input maxlength="-5" uswds />');
+
+    // Level-setting expectations
+    const inputEl = await page.find('va-text-input >>> input');
+    expect(await page.find('va-text-input >>> small')).toBeNull();
+
+    // Test the functionality
+    await inputEl.type('Hello, nice to meet you');
+    expect(await inputEl.getProperty('value')).toBe('Hello, nice to meet you');
+    expect(await page.find('va-text-input >>> small')).toBeNull();
+  });
+
+  it('uswds ignores a maxlength of zero', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input maxlength="0" uswds />');
+
+    // Level-setting expectations
+    const inputEl = await page.find('va-text-input >>> input');
+    expect(await page.find('va-text-input >>> small')).toBeNull();
+
+    // Test the functionality
+    await inputEl.type('Hello, nice to meet you');
+    expect(await inputEl.getProperty('value')).toBe('Hello, nice to meet you');
+    expect(await page.find('va-text-input >>> small')).toBeNull();
+  });
+
+  it('uswds allows manually setting the type attribute', async () => {
+    const allowedInputTypes = [
+      'email',
+      'number',
+      'search',
+      'tel',
+      'text',
+      'url',
+    ];
+    for (const inputType of allowedInputTypes) {
+      const page = await newE2EPage();
+      await page.setContent(`<va-text-input type="${inputType}" uswds />`);
+      const inputEl = await page.find('va-text-input >>> input');
+      expect(inputEl.getAttribute('type')).toBe(inputType);
+    }
+  });
+
+  it('uswds defaults to text when the type attribute is invalid or unsupported', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input type="textual" uswds />');
+
+    // Level-setting expectations
+    const inputEl = await page.find('va-text-input >>> input');
+    expect(inputEl.getAttribute('type')).toBe('text');
+  });
+
+  it('uswds allows manually setting the inputmode attribute', async () => {
+    const inputModes = [
+      'decimal',
+      'email',
+      'none',
+      'numeric',
+      'search',
+      'tel',
+      'text',
+      'url',
+    ];
+    for (const inputMode of inputModes) {
+      const page = await newE2EPage();
+      await page.setContent(`<va-text-input inputmode="${inputMode}" uswds />`);
+      const inputEl = await page.find('va-text-input >>> input');
+      expect(inputEl.getAttribute('inputmode')).toBe(inputMode);
+    }
+  });
+
+  it('uswds displays a green border around input when success is true', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-text-input success uswds />');
+
+    const input = await page.find('va-text-input >>> input');
+    // rgb(0, 169, 28) is equal to #00A91C from the USWDS v3 system.
+    expect((await input.getComputedStyle()).borderBottomColor).toEqual(
+      'rgb(0, 169, 28)',
+    );
+    expect((await input.getComputedStyle()).borderLeftColor).toEqual(
+      'rgb(0, 169, 28)',
+    );
+    expect((await input.getComputedStyle()).borderRightColor).toEqual(
+      'rgb(0, 169, 28)',
+    );
+    expect((await input.getComputedStyle()).borderTopColor).toEqual(
+      'rgb(0, 169, 28)',
+    );
+  });
+
+  it('uswds checks for autocomplete attribute', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-text-input autocomplete="email" uswds />',
+    );
+
+    // Level-setting expectations
+    const inputEl = await page.find('va-text-input >>> input');
+    expect(await inputEl.getProperty('autocomplete')).toBe('email');
   });
 
 });
