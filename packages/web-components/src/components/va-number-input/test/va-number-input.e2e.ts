1a640e9f849ad648d2904526290d8443f5b356b7
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-04-10 16:27:20 -0500

    Add va-number-input USWDS v3 variation (#669)
    * Create v3 version of number input with usa-input style.
    
    * Add tests
    
    * Add tests for va-text-input
    
    * Bump version

--- packages/web-components/src/components/va-number-input/test/va-number-input.e2e.ts
+++ packages/web-components/src/components/va-number-input/test/va-number-input.e2e.ts
@@ -175,4 +175,171 @@ describe('va-number-input', () => {
     const currencyTextElement = await page.find('va-number-input >>> div > span');
     expect(currencyTextElement.innerText).toContain('$');
   });
+
+  // Begin USWDS tests
+
+  it('uswds renders', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-number-input label="Hello, world" uswds />');
+    const element = await page.find('va-number-input');
+
+    expect(element).toEqualHtml(`
+      <va-number-input class="hydrated" label="Hello, world" uswds="">
+        <mock:shadow-root>
+          <label for="inputField" class="usa-label">
+            Hello, world
+          </label>
+          <span id="input-error-message" role="alert"></span>
+          <input aria-invalid="false" class="usa-input" id="inputField" type="number">
+        </mock:shadow-root>
+      </va-number-input>
+    `);
+  });
+
+  it('uswds renders an error message', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-number-input error="This is a mistake" uswds />');
+
+    // Render the error message text
+    const error = await page.find('va-number-input >>> .usa-error-message');
+    const input = await page.find('va-number-input >>> input');
+    expect(error.innerText).toContain('This is a mistake');
+    expect(input.getAttribute('aria-invalid')).toEqual('true');
+  });
+
+  it('uswds renders hint text', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-number-input hint="This is hint text" uswds />');
+
+    // Render the hint text
+    const hintTextElement = await page.find('va-number-input >>> span.usa-hint');
+    expect(hintTextElement.innerText).toContain('This is hint text');
+  });
+
+  it('uswds renders a required span', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<va-number-input label="This is a field" required uswds />',
+    );
+
+    const requiredSpan = await page.find(
+      'va-number-input >>> label > span.usa-label--required',
+    );
+    // The actual text value depends on the language of the document
+    expect(requiredSpan).not.toBeNull();
+  });
+
+  it('uswds passes an aXe check', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      '<va-number-input required label="This is a test" error="With an error message" uswds />',
+    );
+
+    await axeCheck(page);
+  });
+
+  it('uswds fires an analytics event when enableAnalytics is true', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      '<va-number-input label="Input Field" enable-analytics uswds />',
+    );
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+
+    const inputEl = await page.find('va-number-input >>> input');
+    await inputEl.press('1');
+    await inputEl.press('2');
+    await inputEl.press('3');
+    await inputEl.press('Tab');
+
+    expect(analyticsSpy).toHaveReceivedEventDetail({
+      action: 'blur',
+      componentName: 'va-number-input',
+      details: {
+        label: 'Input Field',
+        value: '123',
+      },
+    });
+  });
+
+  it('uswds emits blur event', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-number-input label="Input Field" uswds />');
+
+    const inputEl = await page.find('va-number-input >>> input');
+    const blurSpy = await page.spyOnEvent('blur');
+    await inputEl.press('Tab');
+
+    expect(blurSpy).toHaveReceivedEvent();
+  });
+
+  it('uswds emits input event with value updated', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-number-input label="Input Field" uswds />');
+
+    const inputEl = await page.find('va-number-input >>> input');
+    const inputSpy = await page.spyOnEvent('input');
+    // Act
+    await inputEl.press('1');
+    const firstValue = await page.$eval(
+      'va-number-input',
+      (comp: HTMLInputElement) => comp.value,
+    );
+    await inputEl.press('2');
+    const secondValue = await page.$eval(
+      'va-number-input',
+      (comp: HTMLInputElement) => comp.value,
+    );
+
+    // Assert
+    expect(inputSpy).toHaveReceivedEventTimes(2);
+    expect(firstValue).toEqual('1');
+    expect(secondValue).toEqual('12');
+  });
+
+  it("uswds doesn't fire analytics events", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<va-number-input label="Input Field" uswds />');
+
+    const analyticsSpy = await page.spyOnEvent('component-library-analytics');
+    const inputEl = await page.find('va-number-input >>> input');
+    await inputEl.press('1');
+    await inputEl.press('Tab');
+
+    expect(analyticsSpy).not.toHaveReceivedEvent();
+  });
+
+  it('uswds defaults to type of number', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-number-input uswds />');
+
+    // Level-setting expectations
+    const inputEl = await page.find('va-number-input >>> input');
+    expect(inputEl.getAttribute('type')).toBe('number');
+  });
+
+  it('uswds sets a range based on min and max attributes', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-number-input min="0" max="4" uswds />');
+
+    const inputEl = await page.find('va-number-input >>> input');
+    expect(inputEl.getAttribute('min')).toBe('0');
+    expect(inputEl.getAttribute('max')).toBe('4');
+  });
+
+  it('uswds allows manually setting the inputmode attribute', async () => {
+    const inputModes = ['decimal', 'numeric'];
+    for (const inputMode of inputModes) {
+      const page = await newE2EPage();
+      await page.setContent(`<va-number-input inputmode="${inputMode}" uswds />`);
+      const inputEl = await page.find('va-number-input >>> input');
+      expect(inputEl.getAttribute('inputmode')).toBe(inputMode);
+    }
+  });
 });
