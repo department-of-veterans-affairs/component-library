08dcac205d04a48d98385bc0d4949dd6126b390f
Andrew Steele <andrew.steele@adhocteam.us>
2023-04-17 13:41:07 -0400

    Code coverage for new 'writeReactOwners' function

---
+++ packages/design-system-dashboard-cli/test/write-react-owners-to-csv.spec.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const writeCompPathOwnersToCSV = require('../src/write-react-owners-to-csv');
+const today = require('../src/today');
+
+const SampleData = {
+  AdditionalInfo: [
+    '/Users/name/Projects/vets-website/src/applications/coronavirus-vaccination-expansion/config/address/addressSchema.js',
+    '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Accessories.jsx',
+    '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Batteries.jsx',
+  ],
+  AlertBox: [
+    '/Users/name/Projects/vets-website/src/applications/coronavirus-vaccination-expansion/config/va-location/DynamicRadioWidget.jsx',
+    '/Users/name/Projects/vets-website/src/applications/coronavirus-vaccination/components/Form.jsx',
+  ],
+  FakeComponent: [
+    '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Accessories.jsx',
+    '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Batteries.jsx',
+  ],
+};
+
+describe('writeCompPathOwnersToCSV', () => {
+  it('writes a CSV file', () => {
+    writeCompPathOwnersToCSV(SampleData);
+    const csvPath = path.resolve(
+      __dirname,
+      `../../../component-apps-and-owners-${today}.csv`
+    );
+    console.log('csvPath:', csvPath);
+    const csvFile = fs.existsSync(csvPath);
+
+    expect(csvFile).toBeTruthy();
+  });
+});
