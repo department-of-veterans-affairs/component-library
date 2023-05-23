ff4167dbc3dde8fb5ff61e213d005a45ecf597ea
Harshil <harshil.moradia@adhoc.team>
2023-05-23 13:25:48 -0400

    feat: Additional info v3 (#703)
    * feat: Additional info v3
    
    * cleanup
    
    * converted css rem to px
    
    * version bump
    
    * reverting core version

---
+++ packages/storybook/stories/va-additional-info-uswds.stories.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { getWebComponentDocs, propStructure, StoryDocs } from './wc-helpers';
+
+const additionalInfoDocs = getWebComponentDocs('va-additional-info');
+
+export default {
+  title: 'USWDS/Additional info USWDS',
+  id: 'uswds/va-additional-info',
+  parameters: {
+    componentSubtitle: `va-additional-info web component`,
+    docs: {
+      page: () => <StoryDocs data={additionalInfoDocs} />,
+    },
+    actions: {
+      handles: ['component-library-analytics'],
+    },
+  },
+};
+
+const defaultArgs = {
+  'uswds': true,
+  'trigger': 'Additional Information',
+  'disable-border': false,
+  'disable-analytics': false,
+};
+
+const Template = ({
+  uswds,
+  trigger,
+  'disable-border': disableBorder,
+  'disable-analytics': disableAnalytics,
+}) => {
+  return (
+    <>
+      <p>Surrounding content.</p>
+      <va-additional-info
+        uswds={uswds}
+        trigger={trigger}
+        disable-analytics={disableAnalytics}
+        disable-border={disableBorder}
+      >
+        <div>Here are some popular pets to consider</div>
+        <ul>
+          <li>Dogs</li>
+          <li>Cats</li>
+          <li>Fish</li>
+          <li>Birds</li>
+        </ul>
+      </va-additional-info>
+      <p>Surrounding content.</p>
+    </>
+  );
+};
+
+export const Default = Template.bind(null);
+Default.args = { ...defaultArgs };
+Default.argTypes = propStructure(additionalInfoDocs);
+
+export const NoBorder = Template.bind(null);
+NoBorder.args = {
+  ...defaultArgs,
+  'disable-border': true,
+};
--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -55,6 +55,10 @@ export namespace Components {
           * The text to trigger the expansion
          */
         "trigger": string;
+        /**
+          * Whether or not the component will use USWDS v3 styling.
+         */
+        "uswds"?: boolean;
     }
     interface VaAlert {
         /**
@@ -1506,6 +1510,10 @@ declare namespace LocalJSX {
           * The text to trigger the expansion
          */
         "trigger": string;
+        /**
+          * Whether or not the component will use USWDS v3 styling.
+         */
+        "uswds"?: boolean;
     }
     interface VaAlert {
         /**
