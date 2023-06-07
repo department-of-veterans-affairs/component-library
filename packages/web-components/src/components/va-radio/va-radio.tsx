849861d202750a4f5e3cd9ac84f0d64969d6602a
Andrew Steele <andrew.steele@adhocteam.us>
2023-06-07 10:32:04 -0400

    Adding fieldsets to radio and checkbox groups (#712)
    * Adding fieldsets to V1 radio and checkbox groups
    Also adding some CSS resets for fieldsets and legends
    
    * Web-comp version bump
    
    * Moving radiogroup role to fieldset

--- packages/web-components/src/components/va-radio/va-radio.tsx
+++ packages/web-components/src/components/va-radio/va-radio.tsx
@@ -231,14 +231,20 @@ export class VaRadio {
         'usa-label--error': error
       });
       return (
-        <Host
-          aria-invalid={error ? 'true' : 'false'}
-          aria-label={ariaLabel}
-        >
-          <fieldset class="usa-form usa-fieldset">
+        <Host aria-invalid={error ? 'true' : 'false'} aria-label={ariaLabel}>
+          <fieldset class="usa-form usa-fieldset" role="radiogroup">
             <legend class={legendClass} part="legend">
-              {HeaderLevel ? <HeaderLevel part="header">{label}</HeaderLevel> : label}
-              {required && <span class="usa-label--required" part="required"> {i18next.t('required')}</span>}
+              {HeaderLevel ? (
+                <HeaderLevel part="header">{label}</HeaderLevel>
+              ) : (
+                label
+              )}
+              {required && (
+                <span class="usa-label--required" part="required">
+                  {' '}
+                  {i18next.t('required')}
+                </span>
+              )}
             </legend>
             {hint && <span class="usa-hint">{hint}</span>}
             <span class="usa-error-message" role="alert">
@@ -251,27 +257,33 @@ export class VaRadio {
             <slot></slot>
           </fieldset>
         </Host>
-       )
+      );
     } else {
       return (
-        <Host
-          role="radiogroup"
-          aria-invalid={error ? 'true' : 'false'}
-          aria-label={ariaLabel}
-        >
-          <legend part="legend">
-            {HeaderLevel ? <HeaderLevel part="header">{label}</HeaderLevel> : label}
-            {required && <span class="required" part="required">{i18next.t('required')}</span>}
-          </legend>
-          {hint && <span class="hint-text">{hint}</span>}
-          <span id="error-message" role="alert">
-            {error && (
-              <Fragment>
-                <span class="sr-only">{i18next.t('error')}</span> {error}
-              </Fragment>
-            )}
-          </span>
-          <slot></slot>
+        <Host aria-invalid={error ? 'true' : 'false'} aria-label={ariaLabel}>
+          <fieldset role="radiogroup">
+            <legend part="legend">
+              {HeaderLevel ? (
+                <HeaderLevel part="header">{label}</HeaderLevel>
+              ) : (
+                label
+              )}
+              {required && (
+                <span class="required" part="required">
+                  {i18next.t('required')}
+                </span>
+              )}
+            </legend>
+            {hint && <span class="hint-text">{hint}</span>}
+            <span id="error-message" role="alert">
+              {error && (
+                <Fragment>
+                  <span class="sr-only">{i18next.t('error')}</span> {error}
+                </Fragment>
+              )}
+            </span>
+            <slot></slot>
+          </fieldset>
         </Host>
       );
     }
