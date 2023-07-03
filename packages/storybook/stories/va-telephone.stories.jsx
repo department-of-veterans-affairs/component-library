9b99d00627bed7ac0171f5bcdb582985e2ce599e
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-07-03 14:05:29 -0500

    Remove Table React component (#725)
    * Remove Table React component
    
    * Adjust telephone table header alignment
    
    * Additonal Table cleanup
    
    * Refactor Storybook contacts table for va-telephone
    
    * Revert alignment override

--- packages/storybook/stories/va-telephone.stories.jsx
+++ packages/storybook/stories/va-telephone.stories.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   CONTACTS,
   contactsMap,
-  Table,
 } from '@department-of-veterans-affairs/component-library';
 import { getWebComponentDocs, propStructure, StoryDocs } from './wc-helpers';
 
@@ -10,20 +9,31 @@ const telephoneDocs = getWebComponentDocs('va-telephone');
 
 // This builds the available "CONTACTS" list table
 // Descriptions are available in the contacts.js file
-const fields = [
+const columns = [
   { label: 'Property name (CONTACTS.x)', value: 'key' },
-  { label: 'Phone number', value: 'value' },
+  { label: 'Phone number', value: 'phone' },
   { label: 'Description', value: 'description' },
 ];
+const data = Object.entries(contactsMap).map(c => (
+  {key: c[0], phoneNumber: c[1].phoneNumber, description: c[1].description}
+));
+
 const Contacts = () => (
-  <Table
-    fields={fields}
-    data={Object.entries(contactsMap).map(c => ({
-      key: c[0],
-      value: <va-telephone contact={c[1].phoneNumber} />,
-      description: <div style={{ maxWidth: '30em' }}>{c[1].description}</div>,
-    }))}
-  />
+  <va-table>
+    <va-table-row slot="headers">
+      {columns.map((col) => (
+        <span key={col.value}>{col.label}</span>
+      ))}
+    </va-table-row>
+
+    {data.map(row => (
+      <va-table-row key={row.key}>
+        <span>{row.key}</span>
+        <span><va-telephone contact={row.phoneNumber}></va-telephone></span>
+        <span>{row.description}</span>
+      </va-table-row>
+    ))}
+  </va-table>
 );
 
 export default {
