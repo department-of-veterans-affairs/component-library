6c8c5b700171c9e321b1bcf2b00c0ee2076d555b
Allison Lu <allisonlu820@gmail.com>
2023-07-18 13:51:18 -0700

    minor documentation update

--- packages/web-components/src/components/va-notification/va-notification.tsx
+++ packages/web-components/src/components/va-notification/va-notification.tsx
@@ -66,7 +66,7 @@ export class VaNotification {
   @Prop() headlineLevel?: string = '3';
 
   /**
-   * Date and time for notification. This will be incorporated into a unique aria-describedby label
+   * Date and time for notification. This will also be incorporated into a unique aria-describedby label.
    */
     @Prop() dateTime?: string;
 
