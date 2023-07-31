b2de727fcc59240f0de835c7d430295552000bf3
Rob Garrison <robin.garrison@adhocteam.us>
2023-07-31 11:41:03 -0500

    va-telephone | use less-modern regex pattern (#775)
    * va-telephone: use less-modern regex pattern
    
    * Bump version

--- packages/web-components/src/components/va-telephone/test/va-telephone.e2e.ts
+++ packages/web-components/src/components/va-telephone/test/va-telephone.e2e.ts
@@ -42,6 +42,16 @@ describe('va-telephone', () => {
     await axeCheck(page);
   });
 
+  it('ignores non-digits in the contact prop', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<va-telephone contact="(877) 955-1234"></va-telephone>');
+
+    const link = await page.find('va-telephone >>> a');
+    expect(link.getAttribute('aria-label')).toEqual('8 7 7. 9 5 5. 1 2 3 4.');
+    expect(link.getAttribute('href')).toEqual('tel:+18779551234');
+    expect(link.innerText).toEqual('877-955-1234');
+  });
+
   it('handles a 10 digit contact prop', async () => {
     const page = await newE2EPage();
     await page.setContent('<va-telephone contact="8779551234"></va-telephone>');
