777dc90507278664bfc88fdb36c37882fec114d9
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-08-16 15:52:59 -0500

    Add figma token file

--- packages/css-library/config.js
+++ packages/css-library/config.js
@@ -1,6 +1,24 @@
+const { transform } = require('@divriots/style-dictionary-to-figma');
+
 module.exports = {
   source: ['tokens/**/*.json'],
+  format: {
+    figmaTokensPlugin: ({ dictionary }) => {
+      const transformedTokens = transform(dictionary.tokens, { defaultTokenset: false, cleanMeta: ['filePath', 'isSource', 'original', 'attributes', 'category', 'type', 'path'] });
+      return JSON.stringify(transformedTokens, null, 2);
+    },
+  },
   platforms: {
+    figma: {
+      transformGroup: 'js',
+      buildPath: 'dist/tokens/figma/',
+      files: [
+        {
+          destination: 'variables.json',
+          format: 'figmaTokensPlugin',
+        },
+      ],
+    },
     css: {
       transformGroup: 'css',
       buildPath: 'dist/tokens/css/',
