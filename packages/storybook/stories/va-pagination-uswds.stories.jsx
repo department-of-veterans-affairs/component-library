71b8f47187f11ac98567c8b2b296cdb27521c9e7
Ian <harrison.ian.t@gmail.com>
2023-08-16 15:24:53 -0500

    add more stories

--- packages/storybook/stories/va-pagination-uswds.stories.jsx
+++ packages/storybook/stories/va-pagination-uswds.stories.jsx
@@ -43,3 +43,37 @@ Default.args = {
 };
 Default.argTypes = propStructure(paginationDocs);
 
+export const WithMiddle = Template.bind(null);
+WithMiddle.args = {
+  ...defaultArgs, page: 10
+}
+
+export const WithLast = Template.bind(null);
+WithLast.args = {
+  ...defaultArgs, page: 24
+}
+
+export const WithUnboundedFirst = Template.bind(null);
+WithUnboundedFirst.args = {
+  ...defaultArgs, unbounded: true
+}
+
+export const WithUnboundedMiddle = Template.bind(null);
+WithUnboundedMiddle.args = {
+  ...defaultArgs, unbounded: true, page: 10
+}
+
+export const WithSevenOrLess = Template.bind(null);
+WithSevenOrLess.args = {
+  ...defaultArgs, pages: 7
+}
+
+export const WithSevenOrLessMiddle = Template.bind(null);
+WithSevenOrLessMiddle.args = {
+  ...defaultArgs, pages: 7, page: 3
+}
+
+export const WithSevenOrLessLast = Template.bind(null);
+WithSevenOrLessLast.args = {
+  ...defaultArgs, pages: 7, page: 7
+}
