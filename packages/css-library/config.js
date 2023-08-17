af89360caa987da1ce36cc11b1b316df543de0f5
Alex Taker <a.taker@gmail.com>
2023-08-17 11:14:13 -0400

    Figma token added to style dictionary build

--- packages/css-library/config.js
+++ packages/css-library/config.js
@@ -1,6 +1,25 @@
+const { transform } = require('@divriots/style-dictionary-to-figma');
+
 module.exports = {
   source: ['tokens/**/*.json'],
+  format: {
+    figmaTokensPlugin: ({ dictionary }) => {
+      // currently Figma only supports syncing colors, so only transform dictionary.tokens.color
+      const transformedTokens = transform(dictionary.tokens.color, { defaultTokenset: false, cleanMeta: ['filePath', 'isSource', 'original', 'attributes', 'category', 'type', 'path'] });
+      return JSON.stringify(transformedTokens, null, 2);
+    },
+  },
   platforms: {
+    figma: {
+      transformGroup: 'js',
+      buildPath: 'dist/tokens/figma/',
+      files: [
+        {
+          destination: 'variables.json',
+          format: 'figmaTokensPlugin',
+        },
+      ],
+    },
     css: {
       transformGroup: 'css',
       buildPath: 'dist/tokens/css/',
