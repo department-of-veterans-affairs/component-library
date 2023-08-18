f58b6442bcbc5a42959d3c201619916404d48e18
Ian <harrison.ian.t@gmail.com>
2023-08-18 08:51:44 -0500

    update component attributes

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -679,10 +679,6 @@ export namespace Components {
          */
         "hint"?: string;
         /**
-          * The inputmode attribute.
-         */
-        "inputmode"?: 'decimal' | 'numeric';
-        /**
           * The label for the text input.
          */
         "label"?: string;
@@ -2461,10 +2457,6 @@ declare namespace LocalJSX {
           * Optional hint text.
          */
         "hint"?: string;
-        /**
-          * The inputmode attribute.
-         */
-        "inputmode"?: 'decimal' | 'numeric';
         /**
           * The label for the text input.
          */
