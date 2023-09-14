25d24338fcc32a5a8f6fda49809c46c8fd7791c3
Alex Taker <a.taker@gmail.com>
2023-09-14 14:19:11 -0400

    Add tests

--- packages/web-components/src/components/va-minimal-header/test/va-minimal-header.spec.tsx
+++ packages/web-components/src/components/va-minimal-header/test/va-minimal-header.spec.tsx
@@ -5,12 +5,45 @@ describe('va-minimal-header', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [VaMinimalHeader],
-      html: `<va-minimal-header></va-minimal-header>`,
+      html: `<va-minimal-header header='Header'></va-minimal-header>`,
     });
     expect(page.root).toEqualHtml(`
-      <va-minimal-header>
+      <va-minimal-header header='Header'>
         <mock:shadow-root>
-          <slot></slot>
+          <va-official-gov-banner></va-official-gov-banner>
+          <va-crisis-line-modal></va-crisis-line-modal>
+          <div class="va-flex">
+            <a href="/">
+              <div class="va-logo"></div>
+            </a>
+            <div class="header-container">
+              <h1>Header</h1>
+            </div>
+          </div>
+        </mock:shadow-root>
+      </va-minimal-header>
+    `);
+  });
+
+  it('renders with a subheader', async () => {
+    const page = await newSpecPage({
+      components: [VaMinimalHeader],
+      html: `<va-minimal-header header='Header' subheader='Subheader'></va-minimal-header>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <va-minimal-header header='Header' subheader='Subheader'>
+        <mock:shadow-root>
+          <va-official-gov-banner></va-official-gov-banner>
+          <va-crisis-line-modal></va-crisis-line-modal>
+          <div class="va-flex">
+            <a href="/">
+              <div class="va-logo"></div>
+            </a>
+            <div class="header-container">
+              <h1>Header</h1>
+              <h2>Subheader</h2>
+            </div>
+          </div>
         </mock:shadow-root>
       </va-minimal-header>
     `);
