dcae5dad5634b7913e390b56ae002689480e5ea5
Ian <harrison.ian.t@gmail.com>
2023-10-10 20:34:42 -0500

    more refs

--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -1,4 +1,3 @@
-import AlertBox, { ALERT_TYPE } from './components/AlertBox/AlertBox';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
 import ExpandingGroup from './components/ExpandingGroup/ExpandingGroup';
 import IconBase from './components/IconBase/IconBase';
