2c2a352ebbc544c4ca80c5fca8aa8d88a3c60d68
Ian <harrison.ian.t@gmail.com>
2023-10-10 22:02:32 -0500

    remove ref

--- packages/design-system-dashboard-cli/test/write-react-owners-to-csv.spec.js
+++ packages/design-system-dashboard-cli/test/write-react-owners-to-csv.spec.js
@@ -1,10 +1,6 @@
 const writeCompPathOwnersToCSV = require('../src/write-react-owners-to-csv');
 
 const SampleData = {
-  AlertBox: [
-    '/Users/name/Projects/vets-website/src/applications/coronavirus-vaccination-expansion/config/va-location/DynamicRadioWidget.jsx',
-    '/Users/name/Projects/vets-website/src/applications/coronavirus-vaccination/components/Form.jsx',
-  ],
   FakeComponent: [
     '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Accessories.jsx',
     '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Batteries.jsx',
