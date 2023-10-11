a86bf12ce0348bbe572e09a469097c043677434f
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-10-11 14:19:26 -0500

    revert

--- packages/design-system-dashboard-cli/test/flatten-data.spec.js
+++ packages/design-system-dashboard-cli/test/flatten-data.spec.js
@@ -1,57 +1,57 @@
-// const memfs = require('memfs');
-// const flattenData = require('../src/flatten-data');
+const memfs = require('memfs');
+const flattenData = require('../src/flatten-data');
 
-// const dataMock = {
-//   AlertComponent: {
-//     total: 8,
-//     'App one': 2,
-//     'App two': 3,
-//     '100App': 3,
-//   },
-//   AccordionComponent: {
-//     total: 2,
-//     'App one': 2,
-//   },
-//   InputComponent: {
-//     total: 3,
-//     'App two': 3
-//   }
-// }
-// const date = '2000-01-01';
+const dataMock = {
+  AlertComponent: {
+    total: 8,
+    'App one': 2,
+    'App two': 3,
+    '100App': 3,
+  },
+  AccordionComponent: {
+    total: 2,
+    'App one': 2,
+  },
+  InputComponent: {
+    total: 3,
+    'App two': 3
+  }
+}
+const date = '2000-01-01';
 
-// describe('flattenData', () => {
-//   it('returns an array of objects', () => {
-//     const flattened = flattenData(dataMock, date)
+describe('flattenData', () => {
+  it('returns an array of objects', () => {
+    const flattened = flattenData(dataMock, date)
 
-//     expect(flattened).toBeInstanceOf(Array)
-//     flattened.forEach(item =>
-//       expect(item).toBeInstanceOf(Object)
-//     );
-//   })
+    expect(flattened).toBeInstanceOf(Array)
+    flattened.forEach(item =>
+      expect(item).toBeInstanceOf(Object)
+    );
+  })
 
-//   it('puts the second argument (date) into each item', () => {
-//     const flattened = flattenData(dataMock, date)
+  it('puts the second argument (date) into each item', () => {
+    const flattened = flattenData(dataMock, date)
 
-//     flattened.forEach(item =>
-//       expect(item.date).toEqual(date)
-//     );
-//   });
+    flattened.forEach(item =>
+      expect(item.date).toEqual(date)
+    );
+  });
 
-//   it('has a structure including component_name and each app', () => {
-//     const flattened = flattenData(dataMock, date)
+  it('has a structure including component_name and each app', () => {
+    const flattened = flattenData(dataMock, date)
 
-//     const accordionComponent = flattened.find(item => item.component_name === 'AccordionComponent')
-//     expect(accordionComponent.component_name).toEqual('AccordionComponent')
-//     expect(accordionComponent.app_one).toEqual(2)
-//     expect(accordionComponent.total).toEqual(2)
-//   });
+    const accordionComponent = flattened.find(item => item.component_name === 'AccordionComponent')
+    expect(accordionComponent.component_name).toEqual('AccordionComponent')
+    expect(accordionComponent.app_one).toEqual(2)
+    expect(accordionComponent.total).toEqual(2)
+  });
 
-//   it('prepends an underscore before keys that start with a number', () => {
-//     const flattened = flattenData(dataMock, date)
+  it('prepends an underscore before keys that start with a number', () => {
+    const flattened = flattenData(dataMock, date)
 
-//     const numeric = flattened.find(item => Object.keys(item).some(key => key.includes('100')))
-//     expect(numeric._100app).toEqual(3)
-//   });
+    const numeric = flattened.find(item => Object.keys(item).some(key => key.includes('100')))
+    expect(numeric._100app).toEqual(3)
+  });
 
-// });
+});
 
--- packages/design-system-dashboard-cli/test/write-react-owners-to-csv.spec.js
+++ packages/design-system-dashboard-cli/test/write-react-owners-to-csv.spec.js
@@ -1,14 +1,18 @@
-// const writeCompPathOwnersToCSV = require('../src/write-react-owners-to-csv');
+const writeCompPathOwnersToCSV = require('../src/write-react-owners-to-csv');
 
-// const SampleData = {
-//   FakeComponent: [
-//     '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Accessories.jsx',
-//     '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Batteries.jsx',
-//   ],
-// };
+const SampleData = {
+  AlertBox: [
+    '/Users/name/Projects/vets-website/src/applications/coronavirus-vaccination-expansion/config/va-location/DynamicRadioWidget.jsx',
+    '/Users/name/Projects/vets-website/src/applications/coronavirus-vaccination/components/Form.jsx',
+  ],
+  FakeComponent: [
+    '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Accessories.jsx',
+    '/Users/name/Projects/vets-website/src/applications/disability-benefits/2346/components/Batteries.jsx',
+  ],
+};
 
-// describe('writeCompPathOwnersToCSV', () => {
-//   it.skip('writes a CSV file', () => {
-//     expect(() => writeCompPathOwnersToCSV(SampleData)).not.toThrow();
-//   });
-// });
+describe('writeCompPathOwnersToCSV', () => {
+  it('writes a CSV file', () => {
+    expect(() => writeCompPathOwnersToCSV(SampleData)).not.toThrow();
+  });
+});
