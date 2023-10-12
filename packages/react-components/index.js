69b99850b8303eb4136bb3e5e7896734af16c67c
Ray Messina <48040208+rmessina1010@users.noreply.github.com>
2023-10-12 14:02:37 -0500

    Remove LoadingIndicator React component (#879)
    * removal of components
    
    * removes React LoadingIndicator from index files
    
    * updates react-component version number to 21.0.0

--- packages/react-components/index.js
+++ packages/react-components/index.js
@@ -2,7 +2,6 @@ import Breadcrumbs from './Breadcrumbs';
 import ExpandingGroup from './ExpandingGroup';
 import IconBase from './IconBase';
 import IconSearch from './IconSearch';
-import LoadingIndicator from './LoadingIndicator';
 import Modal from './Modal';
 import TextInput from './TextInput';
 
@@ -13,7 +12,6 @@ export {
   ExpandingGroup,
   IconBase,
   IconSearch,
-  LoadingIndicator,
   Modal,
   TextInput,
 };
