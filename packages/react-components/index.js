d79989f6d9cb1091880fd7714218c270150cccf4
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-10-17 11:11:54 -0500

    Revert "AlertBox and MaintenanceBanner: remove deprecated components (#902)"
    This reverts commit 1dc8b7e46d83187cff922ffce78aa4eefe3a56c4.
    
    # Conflicts:
    #	packages/design-system-dashboard-cli/coverage/lcov-report/write-react-owners-to-csv.js.html

--- packages/react-components/index.js
+++ packages/react-components/index.js
@@ -1,9 +1,11 @@
+import AlertBox, { ALERT_TYPE } from './AlertBox';
 import Breadcrumbs from './Breadcrumbs';
 import Checkbox from './Checkbox';
 import ExpandingGroup from './ExpandingGroup';
 import IconBase from './IconBase';
 import IconSearch from './IconSearch';
 import LoadingIndicator from './LoadingIndicator';
+import MaintenanceBanner from './MaintenanceBanner';
 import Modal from './Modal';
 import OMBInfo from './OMBInfo';
 import TextInput from './TextInput';
@@ -11,12 +13,15 @@ import TextInput from './TextInput';
 import './i18n-setup';
 
 export {
+  AlertBox,
+  ALERT_TYPE,
   Breadcrumbs,
   Checkbox,
   ExpandingGroup,
   IconBase,
   IconSearch,
   LoadingIndicator,
+  MaintenanceBanner,
   Modal,
   OMBInfo,
   TextInput,
--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -1,20 +1,25 @@
+import AlertBox, { ALERT_TYPE } from './components/AlertBox/AlertBox';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
 import Checkbox from './components/Checkbox/Checkbox';
 import ExpandingGroup from './components/ExpandingGroup/ExpandingGroup';
 import IconBase from './components/IconBase/IconBase';
 import IconSearch from './components/IconSearch/IconSearch';
 import LoadingIndicator from './components/LoadingIndicator/LoadingIndicator';
+import MaintenanceBanner from './components/MaintenanceBanner/MaintenanceBanner';
 import Modal from './components/Modal/Modal';
 import OMBInfo from './components/OMBInfo/OMBInfo';
 import TextInput from './components/TextInput/TextInput';
 
 export {
+  AlertBox,
+  ALERT_TYPE,
   Breadcrumbs,
   Checkbox,
   ExpandingGroup,
   IconBase,
   IconSearch,
   LoadingIndicator,
+  MaintenanceBanner,
   Modal,
   OMBInfo,
   TextInput,
