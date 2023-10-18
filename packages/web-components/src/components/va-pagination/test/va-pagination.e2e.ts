bcb6d1f9ee0fd3410eb217881c2c0beacfbcf024
Kerry Powell <kerry.powell@va.gov>
2023-10-18 09:07:25 -0600

    va-pagination: add box-sizing to pagination items and overflow (#912)
    * add box sizing to pagination items and overflow
    
    * Fix failing test
    
    * 4.45.33

--- packages/web-components/src/components/va-pagination/test/va-pagination.e2e.ts
+++ packages/web-components/src/components/va-pagination/test/va-pagination.e2e.ts
@@ -259,13 +259,13 @@ describe('uswds - va-pagination', () => {
         <mock:shadow-root>
           <nav aria-label="Pagination" class="usa-pagination">
             <ul class="usa-pagination__list">
-              <li class="usa-pagination__item usa-pagination__page-no">
+              <li class="usa-pagination__item usa-pagination__page-no va-pagination__item">
                 <a aria-label="page 1, first page" href="javascript:void(0)" class="usa-pagination__button usa-current">1</a>
               </li>
-              <li class="usa-pagination__item usa-pagination__page-no">
+              <li class="usa-pagination__item usa-pagination__page-no va-pagination__item">
                 <a aria-label="page 2" href="javascript:void(0)" class="usa-pagination__button">2</a>
               </li>
-              <li class="usa-pagination__item usa-pagination__page-no">
+              <li class="usa-pagination__item usa-pagination__page-no va-pagination__item">
                 <a aria-label="page 3, last page" href="javascript:void(0)" class="usa-pagination__button">3</a>
               </li>
               <li class="usa-pagination__item usa-pagination__arrow" aria-label="Next page">
