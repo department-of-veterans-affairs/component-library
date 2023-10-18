2c7cbf5fbd2236f183bd4837cf032a3bde605974
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-10-18 09:15:33 -0500

    OMBInfo: Remove deprecated React component  (#915)
    * Revert "Revert "OMBInfo: remove React component (#854)""
    
    This reverts commit 526f0784eb81462c4b9d73eca0de07c236ea8733.
    
    * updating version

--- packages/react-components/index.js
+++ packages/react-components/index.js
@@ -7,7 +7,6 @@ import IconSearch from './IconSearch';
 import LoadingIndicator from './LoadingIndicator';
 import MaintenanceBanner from './MaintenanceBanner';
 import Modal from './Modal';
-import OMBInfo from './OMBInfo';
 import TextInput from './TextInput';
 
 import './i18n-setup';
@@ -23,6 +22,5 @@ export {
   LoadingIndicator,
   MaintenanceBanner,
   Modal,
-  OMBInfo,
   TextInput,
 };
