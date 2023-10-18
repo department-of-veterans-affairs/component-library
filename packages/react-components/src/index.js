94e9eaf0ad2a230814f8592ebb802e57bb6915a0
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-10-18 13:51:28 -0500

    Revert "Revert "AlertBox and MaintenanceBanner: remove deprecated components (#902)""
    This reverts commit d79989f6d9cb1091880fd7714218c270150cccf4.

--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -1,24 +1,19 @@
-import AlertBox, { ALERT_TYPE } from './components/AlertBox/AlertBox';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
 import Checkbox from './components/Checkbox/Checkbox';
 import ExpandingGroup from './components/ExpandingGroup/ExpandingGroup';
 import IconBase from './components/IconBase/IconBase';
 import IconSearch from './components/IconSearch/IconSearch';
 import LoadingIndicator from './components/LoadingIndicator/LoadingIndicator';
-import MaintenanceBanner from './components/MaintenanceBanner/MaintenanceBanner';
 import Modal from './components/Modal/Modal';
 import TextInput from './components/TextInput/TextInput';
 
 export {
-  AlertBox,
-  ALERT_TYPE,
   Breadcrumbs,
   Checkbox,
   ExpandingGroup,
   IconBase,
   IconSearch,
   LoadingIndicator,
-  MaintenanceBanner,
   Modal,
   TextInput,
 };
