03d3fdd59f2effc623b74a7c80e9a690d5a6bb5c
Ian <ian.harrison@adhocteam.us>
2023-11-15 07:29:18 -0600

    Va-minimal-footer: add component (#937)
    * add va-minimal-footer
    
    * use svg
    
    * add alt attr to img
    
    * update tests
    
    * update alt/title attrs and tests
    
    * update story
    
    * add empty destructuring
    
    * restore index.html
    
    * restore index.html
    
    * remove .pngs not needed
    
    * update styles
    
    * keep padding consistent across screen sizes

---
+++ packages/storybook/stories/va-minimal-footer.stories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { getWebComponentDocs, propStructure, StoryDocs } from './wc-helpers';
+
+const minimalFooterDocs = getWebComponentDocs('va-minimal-footer');
+
+export default {
+  title: 'Components/Minimal Footer',
+  id: 'components/va-minimal-footer',
+  parameters: {
+    componentSubtitle: `va-minimal-footer web component`,
+    docs: {
+      page: () => <StoryDocs data={minimalFooterDocs} />,
+    },
+  },
+};
+
+
+const Template = ({}) => {
+  return (
+    <va-minimal-footer />
+  );
+};
+
+export const Default = Template.bind(null);
+Default.argTypes = propStructure(minimalFooterDocs);
