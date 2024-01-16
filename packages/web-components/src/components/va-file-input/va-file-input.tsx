2d2b3fa4f2118ad79ad1ee32abffcff546c7f7dc
Jami Gibbs <jami.gibbs@adhocteam.us>
2024-01-16 17:45:43 -0600

    add onChange callback on element

--- packages/web-components/src/components/va-file-input/va-file-input.tsx
+++ packages/web-components/src/components/va-file-input/va-file-input.tsx
@@ -199,6 +199,7 @@ export class VaFileInput {
             accept={accept}
             multiple={multiple}
             aria-describedby={ariaDescribedbyIds}
+            onChange = {this.handleChange}
           />
         </Host>
       );
