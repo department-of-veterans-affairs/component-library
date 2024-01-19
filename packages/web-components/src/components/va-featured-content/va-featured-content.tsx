f81d481cc5df8b3cff57ae3b9094c98479f9df63
Ray Messina <rmessina10@gmail.com>
2024-01-19 08:58:28 -0600

    re introduces aria-label from headlineText

--- packages/web-components/src/components/va-featured-content/va-featured-content.tsx
+++ packages/web-components/src/components/va-featured-content/va-featured-content.tsx
@@ -14,22 +14,13 @@ export class VaFeaturedContent {
   /**
    * Whether or not the component will use USWDS v3 styling.
    */
-
-  /**
-   *  In styleUrl,  import/prep pertinent styles.
-   *  Those are rules any that begin with:
-   *  .usa-summary-box__heading,
-   *  .usa-summary-box__text 
-   *  Please note any styles that rely on ancesters
-   *  of the above will not be applied
-   **/
-
   @Prop() uswds?: boolean = false;
 
   /**
    * Local state for slot=headline's text.
    * Used to place an aria-label for role="region" with the same text as the heading.
    */
+
   @State() headlineText: string = null;
 
   @Element() el: HTMLElement;
@@ -48,16 +39,11 @@ export class VaFeaturedContent {
       .querySelectorAll('slot')
     const headline = nodes[0];
     const content = nodes[1];
-    
-    headline.classList.add('usa-summary-box__heading');
-    content.classList.add('usa-summary-box__text');
-    // Here I move the content of each of 
-    // the targeted slot elements into the shadow DOM
-    headline.append(...headline.assignedElements());
-    content.append(...content.assignedElements());
 
+    
     let childElements = Array.from(this.el.children);
     this.headlineText = childElements.find(element => element.slot === "headline").textContent.trim();
+
   }
 
   render() {
@@ -68,7 +54,9 @@ export class VaFeaturedContent {
           <div class="usa-summary-box" role="region" aria-label={this.headlineText}>
             <div class="usa-summary-box__body">
               <slot name="headline"></slot>
-              <slot />
+              <div class="usa-summary-box__text">
+                <slot></slot>
+              </div>
             </div>
           </div>
         </Host>
