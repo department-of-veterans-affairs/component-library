cae68552e0d79290818a2d794ce07d5a46657c4d
Ray Messina <rmessina10@gmail.com>
2024-01-19 08:31:53 -0600

    test revert

--- packages/web-components/src/components/va-featured-content/va-featured-content.tsx
+++ packages/web-components/src/components/va-featured-content/va-featured-content.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, Element } from '@stencil/core';
+import { Component, Host, h, Prop, Element, State } from '@stencil/core';
 
 /**
  * @componentName Featured content
@@ -14,9 +14,30 @@ export class VaFeaturedContent {
   /**
    * Whether or not the component will use USWDS v3 styling.
    */
+
+  /**
+   *  In styleUrl,  import/prep pertinent styles.
+   *  Those are rules any that begin with:
+   *  .usa-summary-box__heading,
+   *  .usa-summary-box__text 
+   *  Please note any styles that rely on ancesters
+   *  of the above will not be applied
+   **/
+
   @Prop() uswds?: boolean = false;
 
+  /**
+   * Local state for slot=headline's text.
+   * Used to place an aria-label for role="region" with the same text as the heading.
+   */
+  @State() headlineText: string = null;
+
   @Element() el: HTMLElement;
+
+  componentWillLoad() {
+    let childElements = Array.from(this.el.children);
+    this.headlineText = childElements.find(element => element.slot === "headline").textContent.trim();
+  }
 
   componentDidLoad() {
     if (!this.uswds) {
@@ -30,6 +51,13 @@ export class VaFeaturedContent {
     
     headline.classList.add('usa-summary-box__heading');
     content.classList.add('usa-summary-box__text');
+    // Here I move the content of each of 
+    // the targeted slot elements into the shadow DOM
+    headline.append(...headline.assignedElements());
+    content.append(...content.assignedElements());
+
+    let childElements = Array.from(this.el.children);
+    this.headlineText = childElements.find(element => element.slot === "headline").textContent.trim();
   }
 
   render() {
@@ -37,12 +65,10 @@ export class VaFeaturedContent {
     if (uswds) {
       return (
         <Host>
-          <div class="usa-summary-box" role="region">
+          <div class="usa-summary-box" role="region" aria-label={this.headlineText}>
             <div class="usa-summary-box__body">
               <slot name="headline"></slot>
-              <div class="usa-summary-box__text">
-                <slot></slot>
-              </div>
+              <slot />
             </div>
           </div>
         </Host>
