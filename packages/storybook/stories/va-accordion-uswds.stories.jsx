dc61ef6ff1db73d7d2b0690db802ec028b273164
Rob Garrison <robin.garrison@adhocteam.us>
2024-03-28 14:43:49 -0500

    Update accordion styling (#1098)
    * Add part attrib to header button & content
    
    * Update storybook
    
    * Remove uswds prop in storybook
    
    * Remove all accordion uswds props in storybook

--- packages/storybook/stories/va-accordion-uswds.stories.jsx
+++ packages/storybook/stories/va-accordion-uswds.stories.jsx
@@ -33,20 +33,20 @@ const Template = args => (
     </va-accordion-item>
     <va-accordion-item id="second" header="Second Amendment">
         <p>
-          A well regulated Militia, being necessary to the security of a free State, 
+          A well regulated Militia, being necessary to the security of a free State,
           the right of the people to keep and bear Arms, shall not be infringed.
         </p>
     </va-accordion-item>
     <va-accordion-item id="third" header="Third Amendment">
         <p>
-          No Soldier shall, in time of peace be quartered in any house, without the 
+          No Soldier shall, in time of peace be quartered in any house, without the
           consent of the Owner, nor in time of war, but in a manner to be prescribed by law.
         </p>
     </va-accordion-item>
   </va-accordion>
 );
 
-const BorderedTemplate = args => {  
+const BorderedTemplate = args => {
   return (
     <va-accordion {...args}>
       <va-accordion-item id="first" bordered="true" header="First Amendment">
@@ -59,13 +59,13 @@ const BorderedTemplate = args => {
       </va-accordion-item>
       <va-accordion-item id="second" bordered="true" header="Second Amendment">
           <p>
-            A well regulated Militia, being necessary to the security of a free State, 
+            A well regulated Militia, being necessary to the security of a free State,
             the right of the people to keep and bear Arms, shall not be infringed.
           </p>
       </va-accordion-item>
       <va-accordion-item id="third" bordered="true" header="Third Amendment">
           <p>
-            No Soldier shall, in time of peace be quartered in any house, without the 
+            No Soldier shall, in time of peace be quartered in any house, without the
             consent of the Owner, nor in time of war, but in a manner to be prescribed by law.
           </p>
       </va-accordion-item>
@@ -85,13 +85,13 @@ const TemplateSubheader = args => (
     </va-accordion-item>
     <va-accordion-item id="second" header="Second Amendment" subheader="Subheader">
         <p>
-          A well regulated Militia, being necessary to the security of a free State, 
+          A well regulated Militia, being necessary to the security of a free State,
           the right of the people to keep and bear Arms, shall not be infringed.
         </p>
     </va-accordion-item>
     <va-accordion-item id="third" header="Third Amendment" subheader="Subheader">
         <p>
-          No Soldier shall, in time of peace be quartered in any house, without the 
+          No Soldier shall, in time of peace be quartered in any house, without the
           consent of the Owner, nor in time of war, but in a manner to be prescribed by law.
         </p>
     </va-accordion-item>
@@ -102,7 +102,7 @@ const TemplateIconHeaders = args => (
   <va-accordion {...args}>
     <va-accordion-item id="first" header="First Amendment" subheader="Subheader">
       <i slot="icon" className="fas fa-info-circle vads-u-color--green"/>
-      <i aria-hidden="true" className="fas fa-envelope" slot="subheader-icon"/>  
+      <i aria-hidden="true" className="fas fa-envelope" slot="subheader-icon"/>
       <p>
         Congress shall make no law respecting an establishment of religion, or
         prohibiting the free exercise thereof; or abridging the freedom of speech,
@@ -112,17 +112,17 @@ const TemplateIconHeaders = args => (
     </va-accordion-item>
     <va-accordion-item id="second" header="Second Amendment" subheader="Subheader">
       <i slot="icon" className="fas fa-info-circle vads-u-color--green"/>
-      <i aria-hidden="true" className="fas fa-envelope" slot="subheader-icon"/>  
+      <i aria-hidden="true" className="fas fa-envelope" slot="subheader-icon"/>
       <p>
-        A well regulated Militia, being necessary to the security of a free State, 
+        A well regulated Militia, being necessary to the security of a free State,
         the right of the people to keep and bear Arms, shall not be infringed.
       </p>
     </va-accordion-item>
     <va-accordion-item id="third" header="Third Amendment" subheader="Subheader">
       <i slot="icon" className="fas fa-info-circle vads-u-color--green"/>
-      <i aria-hidden="true" className="fas fa-envelope" slot="subheader-icon"/>  
+      <i aria-hidden="true" className="fas fa-envelope" slot="subheader-icon"/>
       <p>
-        No Soldier shall, in time of peace be quartered in any house, without the 
+        No Soldier shall, in time of peace be quartered in any house, without the
         consent of the Owner, nor in time of war, but in a manner to be prescribed by law.
       </p>
     </va-accordion-item>
@@ -143,14 +143,14 @@ const TemplateHeadlineSlot = args => (
     <va-accordion-item id="second">
       <h6 slot="headline">Second Amendment</h6>
       <p>
-        A well regulated Militia, being necessary to the security of a free State, 
+        A well regulated Militia, being necessary to the security of a free State,
         the right of the people to keep and bear Arms, shall not be infringed.
       </p>
     </va-accordion-item>
     <va-accordion-item id="third">
       <h6 slot="headline">Third Amendment</h6>
       <p>
-        No Soldier shall, in time of peace be quartered in any house, without the 
+        No Soldier shall, in time of peace be quartered in any house, without the
         consent of the Owner, nor in time of war, but in a manner to be prescribed by law.
       </p>
     </va-accordion-item>
@@ -170,13 +170,13 @@ const TemplateLevel = args => (
     </va-accordion-item>
     <va-accordion-item level="5" id="second" header="Second Amendment">
       <p>
-        A well regulated Militia, being necessary to the security of a free State, 
+        A well regulated Militia, being necessary to the security of a free State,
         the right of the people to keep and bear Arms, shall not be infringed.
       </p>
     </va-accordion-item>
     <va-accordion-item level="5" id="third" header="Third Amendment">
         <p>
-          No Soldier shall, in time of peace be quartered in any house, without the 
+          No Soldier shall, in time of peace be quartered in any house, without the
           consent of the Owner, nor in time of war, but in a manner to be prescribed by law.
         </p>
     </va-accordion-item>
@@ -235,43 +235,43 @@ const ManyItemsTemplate = args => (
     </va-accordion-item>
     <va-accordion-item id="second" header="Second Amendment">
         <p>
-          A well regulated Militia, being necessary to the security of a free State, 
+          A well regulated Militia, being necessary to the security of a free State,
           the right of the people to keep and bear Arms, shall not be infringed.
         </p>
     </va-accordion-item>
     <va-accordion-item id="third" header="Third Amendment">
         <p>
-          No Soldier shall, in time of peace be quartered in any house, without the 
+          No Soldier shall, in time of peace be quartered in any house, without the
           consent of the Owner, nor in time of war, but in a manner to be prescribed by law.
         </p>
     </va-accordion-item>
     <va-accordion-item id="fourth" header="Fourth Amendment">
         <p>
-        The right of the people to be secure in their persons, houses, papers, and effects, against unreasonable searches and seizures, 
-        shall not be violated, and no Warrants shall issue, but upon probable cause, supported by Oath or affirmation, and particularly 
+        The right of the people to be secure in their persons, houses, papers, and effects, against unreasonable searches and seizures,
+        shall not be violated, and no Warrants shall issue, but upon probable cause, supported by Oath or affirmation, and particularly
         describing the place to be searched, and the persons or things to be seized.
         </p>
     </va-accordion-item>
     <va-accordion-item id="fifth" header="Fifth Amendment">
         <p>
-        No person shall be held to answer for a capital, or otherwise infamous crime, unless on a presentment or indictment of a Grand Jury, 
-        except in cases arising in the land or naval forces, or in the Militia, when in actual service in time of War or public danger; 
-        nor shall any person be subject for the same offence to be twice put in jeopardy of life or limb; nor shall be compelled in any 
+        No person shall be held to answer for a capital, or otherwise infamous crime, unless on a presentment or indictment of a Grand Jury,
+        except in cases arising in the land or naval forces, or in the Militia, when in actual service in time of War or public danger;
+        nor shall any person be subject for the same offence to be twice put in jeopardy of life or limb; nor shall be compelled in any
         criminal case to be a witness against himself, nor be deprived of life, liberty, or property, without due process of law; nor
         shall private property be taken for public use, without just compensation.
         </p>
     </va-accordion-item>
     <va-accordion-item id="sixth" header="Sixth Amendment">
         <p>
-        In all criminal prosecutions, the accused shall enjoy the right to a speedy and public trial, by an impartial jury of the State 
-        and district wherein the crime shall have been committed, which district shall have been previously ascertained by law, and to 
-        be informed of the nature and cause of the accusation; to be confronted with the witnesses against him; to have compulsory process 
+        In all criminal prosecutions, the accused shall enjoy the right to a speedy and public trial, by an impartial jury of the State
+        and district wherein the crime shall have been committed, which district shall have been previously ascertained by law, and to
+        be informed of the nature and cause of the accusation; to be confronted with the witnesses against him; to have compulsory process
         for obtaining witnesses in his favor, and to have the Assistance of Counsel for his defence.
         </p>
     </va-accordion-item>
     <va-accordion-item id="seventh" header="Seventh Amendment">
         <p>
-        In Suits at common law, where the value in controversy shall exceed twenty dollars, the right of trial by jury shall be preserved, 
+        In Suits at common law, where the value in controversy shall exceed twenty dollars, the right of trial by jury shall be preserved,
         and no fact tried by a jury, shall be otherwise re-examined in any Court of the United States, than according to the rules of the common law.
         </p>
     </va-accordion-item>
