1f5e662a03982c72c7bb0762f701d44580ceba34
Ian <harrison.ian.t@gmail.com>
2024-05-15 09:33:18 -0500

    update story title

--- packages/storybook/stories/va-number-input.stories.jsx
+++ packages/storybook/stories/va-number-input.stories.jsx
@@ -4,7 +4,7 @@ import { getWebComponentDocs, propStructure, StoryDocs } from './wc-helpers';
 const numberInputDocs = getWebComponentDocs('va-number-input');
 
 export default {
-  title: 'Deprecated/Number input V1',
+  title: 'Deprecated/Number input',
   id: 'components/va-number-input',
   parameters: {
     componentSubtitle: `va-number-input web component`,
