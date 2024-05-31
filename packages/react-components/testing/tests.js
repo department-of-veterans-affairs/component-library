aeb12c02aa90bc0fd3966f80df00f44b688700ea
Ian <harrison.ian.t@gmail.com>
2024-05-31 09:52:37 -0500

    Revert "try individual installs"
    This reverts commit 485adf6ac7fa852bee1cc3f4a650c0f4d2f4813f.

--- packages/react-components/testing/tests.js
+++ packages/react-components/testing/tests.js
@@ -7,17 +7,3 @@ Enzyme.configure({ adapter: new Adapter() });
 
 const testsContext = require.context('../src/', true, /\.spec\.js[x]{0,1}$/);
 testsContext.keys().forEach(testsContext);
-
-window.process = {
-  env: {
-    NODE_ENV: 'production'
-  }
-}
-
-const process = {
-  env: {
-    NODE_ENV: 'prod'
-  }
-}
-
-console.log(process);
