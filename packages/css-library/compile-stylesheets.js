99ea406e44f7202b4538e2ab3e575b5ad6d99a0d
Kerry Powell <kerry.powell@adhocteam.us>
2024-06-04 09:05:05 -0600

    refine css-library build process

--- packages/css-library/compile-stylesheets.js
+++
@@ -1,9 +0,0 @@
-const util = require('util');
-const fs = require('fs');
-
-const compileStylesheets = async () => {
-    fs.copyFileSync(`${__dirname}/src/stylesheets/_mixins.scss`, `${__dirname}/dist/stylesheets/_mixins.scss`);
-    fs.copyFileSync(`${__dirname}/src/stylesheets/formation-overrides/_variables.scss`, `${__dirname}/dist/stylesheets/formation-overrides/_variables.scss`);
-}
-
-compileStylesheets();
