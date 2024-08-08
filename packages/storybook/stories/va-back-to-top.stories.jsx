ebd5180b3909e2de8cee4abba7eeb1e10f573dbe
Kerry Powell <kerry.powell@adhocteam.us>
2024-08-08 14:19:45 -0600

    revert files changed by mistake

--- packages/storybook/stories/va-back-to-top.stories.jsx
+++ packages/storybook/stories/va-back-to-top.stories.jsx
@@ -22,7 +22,7 @@ const Template = (defaultArgs) => {
       <div className="usa-grid usa-grid-full">
         <div className="usa-width-three-fourths">
           <h1>The top</h1>
-          {[...Array(2)].map(i => (
+          {[...Array(10)].map(i => (
             <div
               key={i}
               style={{
@@ -35,7 +35,6 @@ const Template = (defaultArgs) => {
               }}
             >
               Scroll down
-              <a href="">link</a>
             </div>
           ))}
           <va-back-to-top />
