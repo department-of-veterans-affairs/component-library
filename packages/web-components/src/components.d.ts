6800abbdd433b43179fb6bd2ca200017fc9c07bd
Andrew Steele <andrew.steele@adhocteam.us>
2024-08-12 14:45:16 -0400

    Removing V1 code from button and button-pair

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -220,10 +220,6 @@ export namespace Components {
           * The text displayed on the button. If `continue` or `back` is true, the value of text is ignored.
          */
         "text"?: string;
-        /**
-          * Whether or not the component will use USWDS v3 styling.
-         */
-        "uswds"?: boolean;
     }
     interface VaButtonIcon {
         "buttonType": keyof typeof this.buttonTypeMap;
@@ -261,10 +257,6 @@ export namespace Components {
           * If `true`, button pair will use Update and Cancel for button text.
          */
         "update"?: boolean;
-        /**
-          * Whether or not the component will use USWDS v3 styling.
-         */
-        "uswds"?: boolean;
     }
     interface VaCard {
         /**
@@ -2311,10 +2303,6 @@ declare namespace LocalJSX {
           * The text displayed on the button. If `continue` or `back` is true, the value of text is ignored.
          */
         "text"?: string;
-        /**
-          * Whether or not the component will use USWDS v3 styling.
-         */
-        "uswds"?: boolean;
     }
     interface VaButtonIcon {
         "buttonType"?: keyof typeof this.buttonTypeMap;
@@ -2368,10 +2356,6 @@ declare namespace LocalJSX {
           * If `true`, button pair will use Update and Cancel for button text.
          */
         "update"?: boolean;
-        /**
-          * Whether or not the component will use USWDS v3 styling.
-         */
-        "uswds"?: boolean;
     }
     interface VaCard {
         /**
