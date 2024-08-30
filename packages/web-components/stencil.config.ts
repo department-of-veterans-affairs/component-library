b5923408e716a12962483eb7e524735ca9883853
Jami Gibbs <jami.gibbs@adhocteam.us>
2024-08-30 09:49:21 -0500

    update react-binding references

--- packages/web-components/stencil.config.ts
+++ packages/web-components/stencil.config.ts
@@ -32,7 +32,7 @@ export const config: Config = {
   },
   outputTargets: [
     reactOutputTarget({
-      outDir: './dist/react-bindings/'
+      outDir: './react-bindings/'
     }),
     {
       type: 'dist',
