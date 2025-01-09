bf5c074afb57049f3da4bd63c65376632ef96b4e
Kerry Powell <1776069+powellkerry@users.noreply.github.com>
2025-01-09 13:54:14 -0700

    va-date: remove aria-labels that were innacurate for select fields and not needed

--- packages/web-components/src/components/va-date/va-date.tsx
+++ packages/web-components/src/components/va-date/va-date.tsx
@@ -267,7 +267,6 @@ export class VaDate {
               onBlur={this.handleMonthBlur}
               invalid={this.invalidMonth}
               class="select-month"
-              aria-label="Please select a month"
               error={this.monthTouched && this.invalidMonth ? error : null}
               showError={false}
             >
@@ -290,7 +289,6 @@ export class VaDate {
                 onBlur={this.handleDayBlur}
                 invalid={this.invalidDay}
                 class="select-day"
-                aria-label="Please select a day"
                 error={this.dayTouched && this.invalidDay ? this.error : null}
                 showError={false}
               >
