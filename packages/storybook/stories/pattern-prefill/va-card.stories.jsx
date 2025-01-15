6e59a8336135b872ffbc9076b09750c8814a8deb
Belle P <belle@Belle-Poopongpanit.local>
2025-01-15 10:58:39 -0800

    Updated prefill pattern components to align with latest changes in Prefill pattern

--- packages/storybook/stories/pattern-prefill/va-card.stories.jsx
+++ packages/storybook/stories/pattern-prefill/va-card.stories.jsx
@@ -16,40 +16,81 @@ export default {
 };
 
 const defaultArgs = {
-  'show-shadow': false,
-  'background': false,
-  'canEdit': false,
+  canEdit: true,
+  children: '',
 };
 
-const Template = ({
-  'show-shadow': showShadow,
-  'background': background,
-  canEdit,
-}) => (
-  <va-card show-shadow={showShadow} background={background} class="hydrated">
+const Template = ({ canEdit, children }) => {
+  return (
+    <va-card class="hydrated" canEdit={canEdit}>
+      {children}
+      {canEdit && (
+        <div class="vads-u-margin-y--1">
+          <va-link
+            active
+            label="Edit mailing address"
+            href="/mock-form-ae-design-patterns/1/task-yellow/veteran-information/edit-mailing-address"
+            text="Edit"
+          />
+        </div>
+      )}
+    </va-card>
+  );
+};
+
+export const Editable = Template.bind(null);
+Editable.args = {
+  ...defaultArgs,
+  canEdit: true,
+  children: (
+    <>
+      <h4 class="vads-u-font-size--h3 vads-u-width--auto vads-u-margin-top--0 vads-u-margin-bottom--2">
+        Mailing address
+      </h4>
+      <div
+        class="dd-privacy-hidden vads-u-margin-y--2"
+        data-dd-action-name="street"
+      >
+        123 Spooner St.
+      </div>
+      <div
+        class="dd-privacy-hidden vads-u-margin-y--2"
+        data-dd-action-name="city, state and zip code"
+      >
+        Fulton, NY 97063
+      </div>
+    </>
+  ),
+};
+
+export const Uneditable = Template.bind(null);
+Uneditable.args = {
+  ...defaultArgs,
+  canEdit: false,
+  children: (
     <div class="vads-u-padding-left--1 vads-u-padding-y--1">
+      <h4 class="vads-u-font-size--h3 vads-u-margin-bottom--2 vads-u-margin-top--0">
+        Personal information
+      </h4>
       <dl class="vads-u-padding-y--0 vads-u-margin-y--0">
         <div class="vads-u-margin-bottom--2">
           <dt class="vads-u-visibility--screen-reader">Full name:</dt>
           <dd
-            class="vads-u-font-weight--bold dd-privacy-mask vads-u-font-family--serif"
+            class="dd-privacy-hidden vads-u-font-family--serif"
             data-dd-action-name="Full name"
           >
-            Maverick P Mitchell
+            <strong>Name:</strong> Maverick P Mitchell
           </dd>
         </div>
         <div class="vads-u-margin-bottom--2">
           <dt class="vads-u-display--inline-block vads-u-font-weight--bold vads-u-margin-right--0p5">
-            Social Security:
+            Last 4 digits of Social Security number:
           </dt>
           <dd
             class="vads-u-display--inline-block dd-privacy-mask vads-u-font-family--sans"
             data-dd-action-name="Social Security:"
           >
-            <span>
-              <span aria-hidden="true">●●●–●●–8321</span>
-              <span class="sr-only">ending with 8 3 2 1</span>
-            </span>
+            6784
           </dd>
         </div>
         <div>
@@ -60,13 +101,13 @@ const Template = ({
             class="dd-privacy-mask medium-screen:vads-u-display--inline-block vads-u-display--none"
             data-dd-action-name="Date of birth"
           >
-            March 04, 1949
+            March 4, 1949
           </dd>
           <dd
             class="dd-privacy-mask vads-u-display--inline-block medium-screen:vads-u-display--none"
             data-dd-action-name="Date of birth"
           >
-            Mar 04, 1949
+            Mar 4, 1949
           </dd>
         </div>
         <div>
@@ -81,29 +122,6 @@ const Template = ({
           </dd>
         </div>
       </dl>
-      {!canEdit && (
-        <div class="vads-u-margin-y--1">
-          <va-link
-          active
-          label="Edit mailing address"
-          href="/mock-form-ae-design-patterns/1/task-yellow/veteran-information/edit-mailing-address"
-          text="Edit"
-          />
-        </div>
-      )}
     </div>
-  </va-card>
-);
-
-export const Editable = Template.bind(null);
-Editable.args = {
-  ...defaultArgs,
-  canEdit: false,
-};
-
-export const Uneditable = Template.bind(null);
-Uneditable.args = {
-  ...defaultArgs,
-  background: true,
-  canEdit: true,
+  ),
 };
