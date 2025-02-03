06efa8407f7c9a071a7a75e8da3f976fa22076b9
Trevor Pierce <934879+1Copenut@users.noreply.github.com>
2025-02-03 10:43:02 -0600

    Updated Storybook instances and e2e tests.
    * Updated Storybook instances to five as default maxPageListLength.
    * Added tests for mobile display and mobile axe check.
    * Added comment for mobile testing device.

--- packages/storybook/stories/va-pagination-uswds.stories.jsx
+++ packages/storybook/stories/va-pagination-uswds.stories.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import { getWebComponentDocs, propStructure, StoryDocs } from './wc-helpers';
 import { VaPagination } from '@department-of-veterans-affairs/web-components/react-bindings';
 
@@ -11,22 +11,22 @@ export default {
     componentSubtitle: 'va-pagination web component',
     docs: {
       page: () => <StoryDocs storyDefault={Default} data={paginationDocs} />,
-    }
-  }
-}
+    },
+  },
+};
 
 const defaultArgs = {
-  page: 1,
-  pages: 24,
-  'max-page-list-length': 7,
-  unbounded: false
-}
+  'page': 1,
+  'pages': 24,
+  'max-page-list-length': 5,
+  'unbounded': false,
+};
 
 const Template = ({
   'page': currentPage,
   pages,
   'max-page-list-length': maxPageListLength,
-  unbounded
+  unbounded,
 }) => {
   const [page, setPage] = useState(currentPage);
   const handlePageSelect = event => {
@@ -41,7 +41,7 @@ const Template = ({
       onPageSelect={handlePageSelect}
     />
   );
-}
+};
 
 export const Default = Template.bind(null);
 Default.args = {
@@ -51,38 +51,48 @@ Default.argTypes = propStructure(paginationDocs);
 
 export const WithMiddle = Template.bind(null);
 WithMiddle.args = {
-  ...defaultArgs, page: 10
-}
+  ...defaultArgs,
+  page: 10,
+};
 
 export const WithLast = Template.bind(null);
 WithLast.args = {
-  ...defaultArgs, page: 24
-}
+  ...defaultArgs,
+  page: 24,
+};
 
 export const WithUnboundedFirst = Template.bind(null);
 WithUnboundedFirst.args = {
-  ...defaultArgs, unbounded: true
-}
+  ...defaultArgs,
+  unbounded: true,
+};
 
 export const WithUnboundedMiddle = Template.bind(null);
 WithUnboundedMiddle.args = {
-  ...defaultArgs, unbounded: true, page: 10
-}
+  ...defaultArgs,
+  unbounded: true,
+  page: 10,
+};
 
-export const WithSevenOrLess = Template.bind(null);
-WithSevenOrLess.args = {
-  ...defaultArgs, pages: 7
-}
+export const WithFiveOrLess = Template.bind(null);
+WithFiveOrLess.args = {
+  ...defaultArgs,
+  pages: 5,
+};
 
-export const WithSevenOrLessMiddle = Template.bind(null);
-WithSevenOrLessMiddle.args = {
-  ...defaultArgs, pages: 7, page: 3
-}
+export const WithFiveOrLessMiddle = Template.bind(null);
+WithFiveOrLessMiddle.args = {
+  ...defaultArgs,
+  pages: 5,
+  page: 3,
+};
 
-export const WithSevenOrLessLast = Template.bind(null);
-WithSevenOrLessLast.args = {
-  ...defaultArgs, pages: 7, page: 7
-}
+export const WithFiveOrLessLast = Template.bind(null);
+WithFiveOrLessLast.args = {
+  ...defaultArgs,
+  pages: 5,
+  page: 5,
+};
 
 export const Internationalization = () => {
   const [lang, setLang] = useState('en');
@@ -95,13 +105,25 @@ export const Internationalization = () => {
 
   return (
     <div>
-      <va-button onClick={() => setLang('es')} text="Español"/>
-      <va-button onClick={() => setLang('en')} text="English"/>
+      <va-button onClick={() => setLang('es')} text="Español" />
+      <va-button onClick={() => setLang('en')} text="English" />
       <div style={{ marginTop: '20px' }}>
         <h4>Default</h4>
-        <VaPagination page={page1} pages={24} maxPageListLength={7} onPageSelect={(event) => setPage1(event.detail.page) }/>
+        <VaPagination
+          page={page1}
+          pages={24}
+          maxPageListLength={5}
+          onPageSelect={event => setPage1(event.detail.page)}
+        />
         <h4>Unbounded</h4>
-        <VaPagination page={page2} pages={24} maxPageListLength={7} unbounded onPageSelect={(event) => setPage2(event.detail.page)}/>
+        <VaPagination
+          page={page2}
+          pages={24}
+          maxPageListLength={5}
+          unbounded
+          onPageSelect={event => setPage2(event.detail.page)}
+        />
       </div>
     </div>
-)};
+  );
+};
