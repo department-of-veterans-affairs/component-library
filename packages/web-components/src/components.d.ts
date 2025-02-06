bf8f8098531c4363d14fc493a5baee9f6f6e57dd
Jami Gibbs <jami.gibbs@adhocteam.us>
2025-02-06 13:10:03 -0600

    va-button-pair: add support for submitting form (#1482)
    * find form element when va-button is a child component
    
    * revert submit prop values
    
    * resize storybook arg table link font size
    
    * add tests
    
    * refactor

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -282,7 +282,7 @@ export namespace Components {
          */
         "secondary"?: boolean;
         /**
-          * Having this attribute present will set the type of this button as 'submit'. The va-button element must be within a `form` element for this functionality to take place A value of: `prevent` will trigger the onsubmit callback on the form, but won't submit the form; `skip` will submit the form but not trigger the onsubmit callback; All other values will trigger the onsubmit and onclick callbacks, then submit the form; in that order.
+          * Having this attribute present will set the [button type](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit) to `submit`. The va-button component must be within a `form` element. **Prop options:** `prevent` will trigger the onsubmit callback on the form, but won't submit the form. `skip` will submit the form but not trigger the form onsubmit callback. All other values will trigger the onsubmit and onclick callbacks, then submit the form, in that order.
          */
         "submit"?: string;
         /**
@@ -339,7 +339,7 @@ export namespace Components {
          */
         "secondaryLabel"?: string;
         /**
-          * Having this attribute present will set the type of this button as 'submit'. The va-button element must be within a `form` element for this functionality to take place A value of: `prevent` will trigger the onsubmit callback on the form, but won't submit the form; `skip` will submit the form but not trigger the onsubmit callback; All other values will trigger the onsubmit and onclick callbacks, then submit the form; in that order.
+          * Having this attribute present will set the [button type](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit) to `submit`. The va-button component must be within a `form` element. **Prop options:** `prevent` will trigger the onsubmit callback on the form, but won't submit the form. `skip` will submit the form but not trigger the form onsubmit callback. All other values will trigger the onsubmit and onclick callbacks, then submit the form, in that order.
          */
         "submit"?: string;
         /**
@@ -3556,7 +3556,7 @@ declare namespace LocalJSX {
          */
         "secondary"?: boolean;
         /**
-          * Having this attribute present will set the type of this button as 'submit'. The va-button element must be within a `form` element for this functionality to take place A value of: `prevent` will trigger the onsubmit callback on the form, but won't submit the form; `skip` will submit the form but not trigger the onsubmit callback; All other values will trigger the onsubmit and onclick callbacks, then submit the form; in that order.
+          * Having this attribute present will set the [button type](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit) to `submit`. The va-button component must be within a `form` element. **Prop options:** `prevent` will trigger the onsubmit callback on the form, but won't submit the form. `skip` will submit the form but not trigger the form onsubmit callback. All other values will trigger the onsubmit and onclick callbacks, then submit the form, in that order.
          */
         "submit"?: string;
         /**
@@ -3629,7 +3629,7 @@ declare namespace LocalJSX {
          */
         "secondaryLabel"?: string;
         /**
-          * Having this attribute present will set the type of this button as 'submit'. The va-button element must be within a `form` element for this functionality to take place A value of: `prevent` will trigger the onsubmit callback on the form, but won't submit the form; `skip` will submit the form but not trigger the onsubmit callback; All other values will trigger the onsubmit and onclick callbacks, then submit the form; in that order.
+          * Having this attribute present will set the [button type](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit) to `submit`. The va-button component must be within a `form` element. **Prop options:** `prevent` will trigger the onsubmit callback on the form, but won't submit the form. `skip` will submit the form but not trigger the form onsubmit callback. All other values will trigger the onsubmit and onclick callbacks, then submit the form, in that order.
          */
         "submit"?: string;
         /**
