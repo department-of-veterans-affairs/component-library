7d1d39032b0be7ba2bb59b9d795785fd08f7f166
Rob Garrison <136959+Mottie@users.noreply.github.com>
2025-02-07 15:31:15 -0600

    Move part around label text

--- packages/web-components/src/components/va-checkbox/va-checkbox.tsx
+++ packages/web-components/src/components/va-checkbox/va-checkbox.tsx
@@ -268,12 +268,8 @@ export class VaCheckbox {
             data-indeterminate={indeterminate && !checked}
             aria-checked={indeterminate && !checked ? 'mixed' : checked}
           />
-          <label
-            htmlFor="checkbox-element"
-            class="va-checkbox__label"
-            part="label"
-          >
-            {label}&nbsp;
+          <label htmlFor="checkbox-element" class="va-checkbox__label">
+            <span part="label">{label}</span>&nbsp;
             {required && (
               <span class="usa-label--required">{i18next.t('required')}</span>
             )}
