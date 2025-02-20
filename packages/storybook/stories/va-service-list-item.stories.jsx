9a58152d127b4330335359f5ec5518c279b4028f
Belle P <belle@Belle-Poopongpanit.local>
2025-02-20 12:07:36 -0800

    Fixed Properties table in Storybook

--- packages/storybook/stories/va-service-list-item.stories.jsx
+++ packages/storybook/stories/va-service-list-item.stories.jsx
@@ -76,30 +76,62 @@ MaximalBase.argTypes = {
   ...propStructure(serviceListItemDocs),
   serviceDetails: {
     description: 'Details about the service',
+    control: { type: 'object' },
+    table: { category: 'Properties', type: { summary: 'ServiceDetails | string' } },
   },
   icon: {
     description: 'The icon associated with the service',
+    control: { type: 'text' },
+    table: { category: 'Properties', type: { summary: 'string' } },
   },
   serviceName: {
     description: 'The name of the service',
+    control: { type: 'text' },
+    table: { category: 'Properties', type: { summary: 'string' } },
   },
   serviceNameHeadingLevel: {
     description: 'The heading level for the service name (defaults to h3)',
     control: { type: 'number', min:2, max:6, },
+    table: { category: 'Properties', type: { summary: 'number' }, defaultValue: { summary: 3 } },
   },
   serviceLink: {
     description: 'The link to the service page',
+    control: { type: 'text' },
+    table: { category: 'Properties', type: { summary: 'string' } },
   },
   serviceStatus: {
     description: 'The status of the service',
+    control: { type: 'text' },
+    table: { category: 'Properties', type: { summary: 'string' } },
   },
   action: {
     description: 'Action associated with the service',
     control: { type: 'object' },
     type: { name: 'object', required: false },
+    table: { category: 'Properties', type: { summary: 'ServiceAction | string' } },
   },
   optionalLink: {
     description: 'An optional link related to the service',
+    control: { type: 'object' },
+    table: { category: 'Properties', type: { summary: 'OptionalLink | string' } },
+  },
+  'service-details': {
+    table: { disable: true },
+  },
+  'service-name': {
+    table: { disable: true },
+  },
+  'service-link': {
+    table: { disable: true },
+  },
+  'optional-link': {
+    table: { disable: true },
+  },
+  'service-status': {
+    table: { disable: true },
+  },
+  'service-name-heading-level': {
+    table: { disable: true },
   },
 };
 
