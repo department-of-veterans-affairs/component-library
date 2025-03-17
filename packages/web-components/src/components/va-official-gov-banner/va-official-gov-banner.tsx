30970583b9c3081bdcf02ebe97dbdb561868a581
George Song <george@gsong.dev>
2025-03-17 09:28:54 -0700

    style(web-components): format va-official-gov-banner component
    - Improve code formatting and consistency
    - Remove unnecessary whitespaces
    - Add missing commas
    - Ensure consistent indentation
    - Enhance readability of the component code
    - No functional changes were made to the component's logic

--- packages/web-components/src/components/va-official-gov-banner/va-official-gov-banner.tsx
+++ packages/web-components/src/components/va-official-gov-banner/va-official-gov-banner.tsx
@@ -6,7 +6,7 @@ import {
   Event,
   Prop,
   Element,
-  forceUpdate
+  forceUpdate,
 } from '@stencil/core';
 import { i18next } from '../..';
 import { Build } from '@stencil/core';
@@ -63,34 +63,38 @@ export class VaOfficialGovBanner {
     i18next.off('languageChanged');
   }
 
-  componentDidLoad(){
+  componentDidLoad() {
     // Initial loading of formatted text.
     this.govSiteExplanationText();
     this.govSiteLockText();
   }
 
   private handleClick = () => {
-    const content = this.el.shadowRoot?.querySelector('.content') as HTMLElement;
+    const content = this.el.shadowRoot?.querySelector(
+      '.content',
+    ) as HTMLElement;
     const button = this.el.shadowRoot?.querySelector('button') as HTMLElement;
-    const header = this.el.shadowRoot?.querySelector('div#header') as HTMLElement;
+    const header = this.el.shadowRoot?.querySelector(
+      'div#header',
+    ) as HTMLElement;
 
     button.setAttribute(
       'aria-expanded',
-      button.getAttribute('aria-expanded') === 'true'
-        ? 'false'
-        : 'true'
+      button.getAttribute('aria-expanded') === 'true' ? 'false' : 'true',
     );
 
     header.classList.toggle('expanded');
     // Toggle the hidden attribute on the content.
-    content.hidden ? content.removeAttribute('hidden') : content.setAttribute('hidden', 'true');
+    content.hidden
+      ? content.removeAttribute('hidden')
+      : content.setAttribute('hidden', 'true');
 
     if (!this.disableAnalytics) {
       const isOpen = button.getAttribute('aria-expanded') === 'true';
       const detail = {
         componentName: 'va-official-gov-banner',
         action: isOpen ? 'expand' : 'collapse',
-      }
+      };
       this.componentLibraryAnalytics.emit(detail);
     }
   };
@@ -101,28 +105,35 @@ export class VaOfficialGovBanner {
    * and the innerHTML of the element will be updated.
    */
   private govSiteExplanationText = () => {
-    const el = this.el.shadowRoot?.querySelector('.gov-site-explanation-text') as HTMLElement;
+    const el = this.el.shadowRoot?.querySelector(
+      '.gov-site-explanation-text',
+    ) as HTMLElement;
     if (el) {
       const text = i18next.t('gov-site-explanation', { tld: this.tld });
       if (text) {
-        el.innerHTML = text.replace(`.${this.tld}`, `<strong>.${this.tld}</strong>`);
+        el.innerHTML = text.replace(
+          `.${this.tld}`,
+          `<strong>.${this.tld}</strong>`,
+        );
       }
     }
-  }
+  };
 
   /**
    * This will add <strong> tags around various words in the text that is provided
    * as well as add the SVG lock image.
    */
   private govSiteLockText = () => {
-    const el = this.el.shadowRoot?.querySelector('.gov-site-lock-text') as HTMLElement;
+    const el = this.el.shadowRoot?.querySelector(
+      '.gov-site-lock-text',
+    ) as HTMLElement;
 
     // eslint-disable-next-line i18next/no-literal-string
     const lockSvg = `(&nbsp;<span class="icon-lock"><svg xmlns="http://www.w3.org/2000/svg" width="52" height="64" viewBox="0 0 52 64" role="img" aria-labelledby="banner-lock-description" focusable="false">
     <title id="banner-lock-title">Lock</title>
     <desc id="banner-lock-description">Locked padlock icon</desc>
     <path fill="#000000" fill-rule="evenodd" d="M26 0c10.493 0 19 8.507 19 19v9h3a4 4 0 0 1 4 4v28a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V32a4 4 0 0 1 4-4h3v-9C7 8.507 15.507 0 26 0zm0 8c-5.979 0-10.843 4.77-10.996 10.712L15 19v9h22v-9c0-6.075-4.925-11-11-11z"></path>
-    </svg></span>&nbsp;)`
+    </svg></span>&nbsp;)`;
 
     let html = i18next.t('gov-site-lock', { image: 'SVG', tld: this.tld });
 
@@ -134,15 +145,15 @@ export class VaOfficialGovBanner {
     if (el) {
       el.innerHTML = html;
     }
-  }
+  };
 
   render() {
     const { tld } = this;
     if (tld === 'gov' || tld === 'mil') {
       return (
         <Host>
-            <div class="banner">
-            <div class="accordion"  >
+          <div class="banner">
+            <div class="accordion">
               <div id="header">
                 <div class="inner">
                   <div class="grid-col-auto">
@@ -150,7 +161,8 @@ export class VaOfficialGovBanner {
                       role="presentation"
                       class="header-flag"
                       src="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/tiny-usa-flag.png"
-                      alt="" />
+                      alt=""
+                    />
                   </div>
                   <div class="grid-col-fill" aria-hidden="true">
                     <p class="header-text">{i18next.t('gov-site-label')}</p>
@@ -160,8 +172,11 @@ export class VaOfficialGovBanner {
                     onClick={this.handleClick}
                     type="button"
                     aria-expanded="false"
-                    aria-controls="official-gov-banner">
-                    <span class="button-text">{i18next.t('gov-site-button')}</span>
+                    aria-controls="official-gov-banner"
+                  >
+                    <span class="button-text">
+                      {i18next.t('gov-site-button')}
+                    </span>
                   </button>
                 </div>
               </div>
@@ -169,13 +184,13 @@ export class VaOfficialGovBanner {
               <div class="content" id="official-gov-banner" hidden>
                 <div class="grid-row">
                   <div class="col">
-                    <img
-                      src={iconHttpsSvg}
-                      role="presentation"
-                      alt="" />
+                    <img src={iconHttpsSvg} role="presentation" alt="" />
                     <div class="media-block">
                       <p>
-                        <strong>{i18next.t('gov-site-website', { tld })}</strong><br />
+                        <strong>
+                          {i18next.t('gov-site-website', { tld })}
+                        </strong>
+                        <br />
                         <span class="gov-site-explanation-text">
                           {this.govSiteExplanationText()}
                         </span>
@@ -183,13 +198,11 @@ export class VaOfficialGovBanner {
                     </div>
                   </div>
                   <div class="col">
-                    <img
-                      src={iconDotGovSvg}
-                      role="presentation"
-                      alt="" />
+                    <img src={iconDotGovSvg} role="presentation" alt="" />
                     <div class="media-block">
                       <p>
-                        <strong>{i18next.t('gov-site-https', { tld })}</strong><br />
+                        <strong>{i18next.t('gov-site-https', { tld })}</strong>
+                        <br />
                         <span class="gov-site-lock-text">
                           {this.govSiteLockText()}
                         </span>
@@ -204,5 +217,4 @@ export class VaOfficialGovBanner {
       );
     }
   }
-
 }
