8a45397afbaa6212cca6025495464e8d5e22177e
Belle P <belle@Belle-Poopongpanit.local>
2025-03-17 14:06:16 -0700

    Fixed header chevron styling for service-list-item and added card with critical info story

--- packages/storybook/stories/va-critical-information.stories.jsx
+++ packages/storybook/stories/va-critical-information.stories.jsx
@@ -31,3 +31,49 @@ Default.args = {
 Default.argTypes = {
   ...propStructure(criticalInformationDocs),
 };
+
+export const CardWithCriticalInfo = ({}) => {
+  return (
+    <va-card>
+      <h3 className="claim-list-item-header vads-u-margin-bottom--2">
+        <div>
+          <span
+            className="usa-label vads-u-font-weight--normal vads-u-font-size--md vads-u-margin-bottom--2 vads-u-display--block vads-u-font-family--sans"
+            style={{ width: 'fit-content' }}
+          >
+            IN PROGRESS
+          </span>
+          Claim for compensation
+          <span className="vads-u-font-family--sans vads-u-margin-top--0p5 vads-u-font-weight--normal vads-u-font-size--md vads-u-display--block">
+            Received on June 15, 2023
+          </span>
+        </div>
+      </h3>
+      <va-critical-information
+        link="https://www.va.gov/claim-or-appeal-status/"
+        text="We requested more information from you. Submit by May 21, 2025."
+      />
+      <ul
+        className="details vads-u-padding-left--0"
+        style={{ listStyle: 'none' }}
+      >
+        <li className="vads-u-margin--0">
+          <va-icon
+            icon="mail"
+            size={3}
+            class="vads-u-margin-right--1"
+            aria-hidden="true"
+          />
+          We sent you a development letter
+        </li>
+        <li className="vads-u-margin--0">Step 3 of 8: Evidence gathering</li>
+        <li>Moved to this step on June 18, 2023</li>
+      </ul>
+      <va-link
+        href="https://www.va.gov/claim-or-appeal-status/"
+        text="Details"
+        active="true"
+      />
+    </va-card>
+  );
+};
