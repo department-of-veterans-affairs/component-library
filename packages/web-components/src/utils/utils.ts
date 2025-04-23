d0bca342b321b67c8857197281ea237cd6fbb131
Jami Gibbs <jami.gibbs@adhocteam.us>
2025-04-23 15:20:59 -0500

    va-search-filter: add component (#1568)
    * initial markup and props
    
    * get active filters and display total active filter acount
    
    * display active filters count with facet labels
    
    * remove spec.tsx test file
    
    * add maturity level front matter
    
    * add clear all filters button
    
    * add apply and clear all custom events
    
    * look up facet and category by id
    
    * add filter option validation
    
    * initial tests
    
    * more tests and active filters story
    
    * update tests
    
    * cleanup
    
    * add part selector to legend
    
    * add deepEquals utility function
    
    * DRY improvements
    
    * formatting cleanup

--- packages/web-components/src/utils/utils.ts
+++ packages/web-components/src/utils/utils.ts
@@ -197,3 +197,90 @@ export function isInteractiveLinkOrButton(el: HTMLElement): boolean {
 export function isMessageSet(message: string): boolean {
   return (message && message !== 'false') || false;
 }
+
+/**
+ * Checks if an object is an arguments object
+ */
+function isArguments(object: any): boolean {
+  return Object.prototype.toString.call(object) === "[object Arguments]";
+}
+
+/**
+ * Performs a deep equality check between two values (primitives, arrays, or objects)
+ * @param a First value to compare
+ * @param b Second value to compare
+ * @returns True if the values are deeply equal, false otherwise
+ */
+export function deepEquals(a: any, b: any): boolean {
+  const ca = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+  const cb = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : [];
+
+  // Partially extracted from node-deeper and adapted to exclude comparison
+  // checks for functions.
+  // https://github.com/othiym23/node-deeper
+  if (a === b) {
+    return true;
+  } else if (typeof a === "function" || typeof b === "function") {
+    // Assume all functions are equivalent
+    // see https://github.com/mozilla-services/react-jsonschema-form/issues/255
+    return true;
+  } else if (typeof a !== "object" || typeof b !== "object") {
+    return false;
+  } else if (a === null || b === null) {
+    return false;
+  } else if (a instanceof Date && b instanceof Date) {
+    return a.getTime() === b.getTime();
+  } else if (a instanceof RegExp && b instanceof RegExp) {
+    return a.source === b.source && a.global === b.global && a.multiline === b.multiline && a.lastIndex === b.lastIndex && a.ignoreCase === b.ignoreCase;
+  } else if (isArguments(a) || isArguments(b)) {
+    if (!(isArguments(a) && isArguments(b))) {
+      return false;
+    }
+    const slice = Array.prototype.slice;
+    return (deepEquals as any)(slice.call(a), slice.call(b), ca, cb);
+  } else {
+    if (a.constructor !== b.constructor) {
+      return false;
+    }
+
+    const ka = Object.keys(a);
+    const kb = Object.keys(b);
+    // don't bother with stack acrobatics if there's nothing there
+    if (ka.length === 0 && kb.length === 0) {
+      return true;
+    }
+    if (ka.length !== kb.length) {
+      return false;
+    }
+
+    let cal = ca.length;
+    while (cal--) {
+      if (ca[cal] === a) {
+        return cb[cal] === b;
+      }
+    }
+    ca.push(a);
+    cb.push(b);
+
+    ka.sort();
+    kb.sort();
+    for (let j = ka.length - 1; j >= 0; j--) {
+      if (ka[j] !== kb[j]) {
+        return false;
+      }
+    }
+
+    let key;
+    for (let k = ka.length - 1; k >= 0; k--) {
+      key = ka[k];
+      if (!(deepEquals as any)(a[key], b[key], ca, cb)) {
+        return false;
+      }
+    }
+
+    ca.pop();
+    cb.pop();
+
+    return true;
+  }
+}
