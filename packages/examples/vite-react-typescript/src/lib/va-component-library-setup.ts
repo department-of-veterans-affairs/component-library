824423e2a5cbb3d0f45abc6c1a8b2746e51e2752
Jami Gibbs <jami.gibbs@adhocteam.us>
2025-05-08 13:14:48 -0500

    initial

---
+++ packages/examples/vite-react-typescript/src/lib/va-component-library-setup.ts
@@ -0,0 +1,46 @@
+
+/**
+ * Import the components we need from the VA component library.
+ * Alternatively, you can import all components at once with the following instead:
+ * 
+ *   import { applyPolyfills, defineCustomElements } from '@department-of-veterans-affairs/component-library/dist/components';
+ */
+import { 
+  defineCustomElementVaButton,
+  defineCustomElementVaAlert,
+  defineCustomElementVaHeaderMinimal,
+  defineCustomElementVaMinimalFooter,
+  defineCustomElementVaLink,
+  defineCustomElementVaBreadcrumbs,
+  // Add more components as needed
+} from '@department-of-veterans-affairs/component-library/dist/components';
+
+// Initialize the individual web components
+/**
+ * Initialize the individual web components we need.
+ * 
+ * Alternatively, you can initialize all components at once with the following instead:
+ 
+ *   applyPolyfills();
+ *   defineCustomElements();
+ */
+export const initVAComponents = () => {
+  // Define the custom elements we need
+  defineCustomElementVaButton();
+  defineCustomElementVaAlert();
+  defineCustomElementVaHeaderMinimal();
+  defineCustomElementVaMinimalFooter();
+  defineCustomElementVaLink();
+  defineCustomElementVaBreadcrumbs();
+  // Add more component definitions as needed
+  console.log('VA Component Library components initialized');
+};
+
+// Auto-initialize when this module is loaded
+export const initializeVAComponents = () => {
+  initVAComponents();
+  console.log('VA Component Library components initialized');
+};
+
+// Auto-initialize on import
+initializeVAComponents();
