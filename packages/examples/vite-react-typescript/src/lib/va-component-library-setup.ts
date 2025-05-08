8215cb03c8182876d7ee02a116e137e4cd5c83ba
Jami Gibbs <jami.gibbs@adhocteam.us>
2025-05-08 13:57:04 -0500

    cleanup

--- packages/examples/vite-react-typescript/src/lib/va-component-library-setup.ts
+++ packages/examples/vite-react-typescript/src/lib/va-component-library-setup.ts
@@ -3,7 +3,7 @@
  * Import the components we need from the VA component library.
  * Alternatively, you can import all components at once with the following instead:
  * 
- *   import { applyPolyfills, defineCustomElements } from '@department-of-veterans-affairs/component-library/dist/components';
+ * import { applyPolyfills, defineCustomElements } from '@department-of-veterans-affairs/component-library/dist/components';
  */
 import { 
   defineCustomElementVaButton,
@@ -15,12 +15,12 @@ import {
   // Add more components as needed
 } from '@department-of-veterans-affairs/component-library/dist/components';
 
-// Initialize the individual web components
 /**
  * Initialize the individual web components we need.
  * 
- * Alternatively, you can initialize all components at once with the following instead:
- 
+ * Alternatively, you can initialize all components at once with 
+ * the following instead:
+ * 
  *   applyPolyfills();
  *   defineCustomElements();
  */
