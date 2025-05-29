502c413a6dfe6d786ece55b93fdf97f8fadc15b1
amyleadem <amyleadem@gmail.com>
2025-05-29 16:35:07 -0700

    Reorder controls; Make default prop definition repeatable

--- packages/storybook/stories/va-accordion-uswds.stories.tsx
+++ packages/storybook/stories/va-accordion-uswds.stories.tsx
@@ -3,44 +3,38 @@ import {
   getWebComponentDocs,
   propStructure,
   StoryDocs,
+  propDefaults,
+  getDefaultPropValue,
 } from './wc-helpers';
 
 const accordionDocs = getWebComponentDocs('va-accordion');
 const accordionItemDocs = getWebComponentDocs('va-accordion-item');
-const accordionItemProps = propStructure(accordionItemDocs);
-
-const defaultArgs = {
-  itemsBordered: JSON.parse(accordionItemProps.bordered.defaultValue.value),
-  itemsLevel: JSON.parse(accordionItemProps.level.defaultValue.value),
-  itemsHeadlineSlot: false,
-  itemsIcon: false,
+const subcomponentName = 'va-accordion-item';
+const defaultItemValues = {
+  open: getDefaultPropValue('va-accordion-item', 'open'),
 };
-
-const defaultValues ={
-  open: JSON.parse(accordionItemProps.open.defaultValue.value),
-  subheader: JSON.parse(accordionItemProps.subheader.defaultValue.value),
-}
 
 const items = [
   {
-    ...defaultValues,
+    ...defaultItemValues,
     id: 'first',
     header: 'First Amendment',
+
     body: 'Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.',
   },
   {
-    ...defaultValues,
+    ...defaultItemValues,
     id: 'second',
     header: 'Second Amendment',
     body: 'A well regulated Militia, being necessary to the security of a free State, the right of the people to keep and bear Arms, shall not be infringed.',
   },
   {
-    ...defaultValues,
+    ...defaultItemValues,
     id: 'third',
     header: 'Third Amendment',
     body: 'No Soldier shall, in time of peace be quartered in any house, without the consent of the Owner, nor in time of war, but in a manner to be prescribed by law.',
   },
-]
+];
 
 export default {
   title: 'Components/Accordion USWDS',
@@ -53,44 +47,55 @@ export default {
   },
   argTypes: {
     ...propStructure(accordionDocs),
+    ...propStructure(accordionItemDocs, subcomponentName),
     i18n: {
       name: 'Add internationalization buttons',
       description: 'Adds buttons to change the language of the component',
       control: { type: 'boolean' },
-      table: { category: 'Additional variants' },
+      table: { category: 'Variants' },
     },
     accordionItems: {
-      name: 'va-accordion-item: items',
+      name: 'va-accordion-item: content',
       description: 'Content and props for the accordion items',
-      control: { type: 'object' },
-      table: { category: 'Subcomponent controls' },
-    },
-    itemsBordered: {
-      name: 'va-accordion-item: bordered',
-      description: 'Add the `bordered` prop to the accordion items',
-      control: { type: 'boolean' },
-      table: { category: 'Subcomponent controls' },
-    },
-    itemsLevel: {
-      name: 'va-accordion-item: level',
-      description: 'Set heading level for the accordion items',
-      control: { type: 'number', min:1, max:6 },
-      table: { category: 'Subcomponent controls' },
-    },
-    itemsHeadlineSlot: {
-      table: { disable:true },
+      table: { category: subcomponentName },
     },
     itemsIcon: {
       name: 'va-accordion-item: add header icons',
       description: 'Add header and subheader icons via a slot',
       control: { type: 'boolean' },
-      table: { category: 'Subcomponent controls' },
+      table: { category: subcomponentName },
+    },
+    itemsHeadlineSlot: {
+      table: { disable: true },
+    },
+    level: {
+      name: 'va-accordion-item: level',
+      description: 'Set heading level for all accordion items',
+      control: { type: 'number', min: 1, max: 6 },
+      table: {
+        category: subcomponentName,
+        defaultValue: {
+          summary: getDefaultPropValue('va-accordion-item', 'level'),
+        },
+      },
+    },
+    // Hiding these controls because these values are managed in the items array
+    open: {
+      table: { disable: true },
+    },
+    header: {
+      table: { disable: true },
+    },
+    subheader: {
+      table: { disable: true },
     },
   },
   args: {
-    ...defaultArgs,
-    accordionItems: items,
+    ...propDefaults(getWebComponentDocs('va-accordion')),
     i18n: false,
+    itemsHeadlineSlot: false,
+    itemsIcon: false,
+    accordionItems: items,
   },
 };
 
@@ -109,34 +114,42 @@ const Template = args => {
           <va-button onClick={e => setLang('tl')} text="Tagalog" />
         </div>
       )}
-      <va-accordion {...args.props}>
+      <va-accordion
+        open-single={args['open-single'] ? args['open-single'] : undefined}
+        disableAnalytics={
+          args['disable-analytics'] ? args['disable-analytics'] : undefined
+        }
+        section-heading={
+          args['section-heading'] ? args['section-heading'] : undefined
+        }
+      >
         {args.accordionItems.map(accordion => (
           <va-accordion-item
             id={accordion.id}
             header={args.itemsHeadlineSlot ? undefined : accordion.header}
-            bordered={args.itemsBordered}
-            subheader={accordion.subheader ? accordion.subheader : args.itemsIcon ? 'Subheader' : undefined}
-            level={args.itemsLevel ? args.itemsLevel : undefined}
+            bordered={args.bordered}
+            subheader={
+              accordion.subheader
+                ? accordion.subheader
+                : args.itemsIcon
+                ? 'Subheader'
+                : undefined
+            }
+            level={args.level ? args.level : undefined}
             open={accordion.open}
           >
             {args.itemsIcon && (
-                <span slot="icon" className="vads-u-color--green">
-                  <va-icon icon="info" />
-                </span>
+              <span slot="icon" className="vads-u-color--green">
+                <va-icon icon="info" />
+              </span>
             )}
-            {args.itemsIcon && (
-                <va-icon
-                  icon="mail"
-                  slot="subheader-icon"
-                />
-            )}
+            {args.itemsIcon && <va-icon icon="mail" slot="subheader-icon" />}
             {args.itemsHeadlineSlot && (
               <h3 slot="headline">Slotted {accordion.header}</h3>
             )}
             <p>{accordion.body}</p>
           </va-accordion-item>
-        )
-      )}
+        ))}
       </va-accordion>
     </>
   );
@@ -146,14 +159,12 @@ export const Default = Template.bind(null);
 
 export const SingleSelect = Template.bind(null);
 SingleSelect.args = {
-  props: {
-    'open-single': true,
-  },
+  'open-single': true,
 };
 
 export const Bordered = Template.bind(null);
 Bordered.args = {
-  itemsBordered: true,
+  bordered: true,
 };
 
 export const Subheader = Template.bind(null);
@@ -166,7 +177,7 @@ Subheader.args = {
 
 export const IconHeaders = Template.bind(null);
 IconHeaders.args = {
-  itemsIcon: true
+  itemsIcon: true,
 };
 
 export const HeadlineSlot = Template.bind(null);
@@ -175,16 +186,16 @@ HeadlineSlot.argTypes = {
     name: 'va-accordion-item: use headline slot',
     description: 'Add the header via a slot',
     control: { type: 'boolean' },
-    table: { disable:false, category: 'Subcomponent controls' },
+    table: { disable: false, category: subcomponentName },
   },
-}
+};
 HeadlineSlot.args = {
   itemsHeadlineSlot: true,
 };
 
 export const CustomHeaderLevel = Template.bind(null);
 CustomHeaderLevel.args = {
-  itemsLevel: 4,
+  level: 4,
 };
 
 export const Internationalization = Template.bind(null);
@@ -197,43 +208,43 @@ ManyAccordions.args = {
   accordionItems: [
     ...items,
     {
-      ...defaultValues,
+      ...defaultItemValues,
       id: 'fourth',
       header: 'Fourth Amendment',
       body: 'The right of the people to be secure in their persons, houses, papers, and effects, against unreasonable searches and seizures, shall not be violated, and no Warrants shall issue, but upon probable cause, supported by Oath or affirmation, and particularly describing the place to be searched, and the persons or things to be seized.',
     },
     {
-      ...defaultValues,
+      ...defaultItemValues,
       id: 'fifth',
       header: 'Fifth Amendment',
       body: 'No person shall be held to answer for a capital, or otherwise infamous crime, unless on a presentment or indictment of a Grand Jury, except in cases arising in the land or naval forces, or in the Militia, when in actual service in time of War or public danger; nor shall any person be subject for the same offence to be twice put in jeopardy of life or limb; nor shall be compelled in any criminal case to be a witness against himself, nor be deprived of life, liberty, or property, without due process of law; nor shall private property be taken for public use, without just compensation.',
     },
     {
-      ...defaultValues,
+      ...defaultItemValues,
       id: 'sixth',
       header: 'Sixth Amendment',
       body: 'In all criminal prosecutions, the accused shall enjoy the right to a speedy and public trial, by an impartial jury of the State and district wherein the crime shall have been committed, which district shall have been previously ascertained by law, and to be informed of the nature and cause of the accusation; to be confronted with the witnesses against him; to have compulsory process for obtaining witnesses in his favor, and to have the Assistance of Counsel for his defence.',
     },
-      {
-      ...defaultValues,
+    {
+      ...defaultItemValues,
       id: 'seventh',
       header: 'Seventh Amendment',
       body: 'In Suits at common law, where the value in controversy shall exceed twenty dollars, the right of trial by jury shall be preserved, and no fact tried by a jury, shall be otherwise re-examined in any Court of the United States, than according to the rules of the common law.',
     },
     {
-      ...defaultValues,
+      ...defaultItemValues,
       id: 'Eighth',
       header: 'Eighth Amendment',
       body: 'Excessive bail shall not be required, nor excessive fines imposed, nor cruel and unusual punishments inflicted.',
     },
     {
-      ...defaultValues,
+      ...defaultItemValues,
       id: 'Ninth',
       header: 'Ninth Amendment',
       body: 'The enumeration in the Constitution, of certain rights, shall not be construed to deny or disparage others retained by the people.',
     },
     {
-      ...defaultValues,
+      ...defaultItemValues,
       id: 'Tenth',
       header: 'Tenth Amendment',
       body: 'The powers not delegated to the United States by the Constitution, nor prohibited by it to the States, are reserved to the States respectively, or to the people.',
@@ -246,13 +257,13 @@ export const PrintAccordion = Template.bind(null);
 PrintAccordion.args = {
   accordionItems: [
     {
-      ...defaultValues,
+      ...defaultItemValues,
       id: 'first',
       header: 'First Item',
-      body: 'This story is for the Design System Team to be able to test the accordion items being opened when the page they\'re on is being printed.',
+      body: "This story is for the Design System Team to be able to test the accordion items being opened when the page they're on is being printed.",
     },
     {
-      ...defaultValues,
+      ...defaultItemValues,
       id: 'second',
       header: 'Second Item',
       body: 'Accordion items will always open when being printed.',
