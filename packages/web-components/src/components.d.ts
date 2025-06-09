42d9b7685da6c20964fdfd6710e660252e08e4a5
Ryan Munsch <ryan.munsch@wilcore.io>
2025-06-09 11:14:32 -0400

    Add disable-analytics prop

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -1844,6 +1844,10 @@ export namespace Components {
          */
         "countryCode"?: string;
         /**
+          * If `true`, doesn't fire the CustomEvent which can be used for analytics tracking.
+         */
+        "disableAnalytics"?: boolean;
+        /**
           * Optional numeric string phone number extension
          */
         "extension"?: string;
@@ -5600,6 +5604,10 @@ declare namespace LocalJSX {
           * Prepends the country code to the given contact number. Do NOT include the '+'
          */
         "countryCode"?: string;
+        /**
+          * If `true`, doesn't fire the CustomEvent which can be used for analytics tracking.
+         */
+        "disableAnalytics"?: boolean;
         /**
           * Optional numeric string phone number extension
          */
