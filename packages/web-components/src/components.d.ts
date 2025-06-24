ccd3b1b9f4c1a45046e229a3a3e3fbeb5f3bd263
Matthew Dingee <52176351+humancompanion-usds@users.noreply.github.com>
2025-06-24 10:35:42 -0400

    va-promo-banner: update maturity level to deprecated (#1653)
    * Banner - Promo: Deprecation. [Amends #4142]
    
    * Puts promo banner into the deprecated section.

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -1374,8 +1374,8 @@ export namespace Components {
     }
     /**
      * @componentName Banner - Promo
-     * @maturityCategory use
-     * @maturityLevel deployed
+     * @maturityCategory dont_use
+     * @maturityLevel deprecated
      * @guidanceHref banner/promo
      */
     interface VaPromoBanner {
@@ -3161,8 +3161,8 @@ declare global {
     }
     /**
      * @componentName Banner - Promo
-     * @maturityCategory use
-     * @maturityLevel deployed
+     * @maturityCategory dont_use
+     * @maturityLevel deprecated
      * @guidanceHref banner/promo
      */
     interface HTMLVaPromoBannerElement extends Components.VaPromoBanner, HTMLStencilElement {
@@ -5136,8 +5136,8 @@ declare namespace LocalJSX {
     }
     /**
      * @componentName Banner - Promo
-     * @maturityCategory use
-     * @maturityLevel deployed
+     * @maturityCategory dont_use
+     * @maturityLevel deprecated
      * @guidanceHref banner/promo
      */
     interface VaPromoBanner {
@@ -6337,8 +6337,8 @@ declare module "@stencil/core" {
             "va-progress-bar": LocalJSX.VaProgressBar & JSXBase.HTMLAttributes<HTMLVaProgressBarElement>;
             /**
              * @componentName Banner - Promo
-             * @maturityCategory use
-             * @maturityLevel deployed
+             * @maturityCategory dont_use
+             * @maturityLevel deprecated
              * @guidanceHref banner/promo
              */
             "va-promo-banner": LocalJSX.VaPromoBanner & JSXBase.HTMLAttributes<HTMLVaPromoBannerElement>;
