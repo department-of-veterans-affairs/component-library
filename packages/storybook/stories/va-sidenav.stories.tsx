de34545b8f16921b50f419775f99166b7de32e0c
Jami Gibbs <jami.gibbs@adhocteam.us>
2025-06-25 16:00:08 -0500

    add mobile web va-accordion

--- packages/storybook/stories/va-sidenav.stories.tsx
+++ packages/storybook/stories/va-sidenav.stories.tsx
@@ -14,53 +14,23 @@ export default {
   },
 };
 
-const Template = ({}) => (
-  <va-sidenav header="Profile" icon-name="account_circle" icon-background-color="vads-color-primary">
-    <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
-    <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
-    <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
-    <va-sidenav-item href="#" label="Parent item with a very long label for a sidenav item to see how it reflows when it is very long or if we zoom in"></va-sidenav-item>
-    <va-sidenav-submenu label="Parent item for submenu (non-accordion)">
-        <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-        <va-sidenav-item href="#" label="Child item as current page" current-page={true}></va-sidenav-item>
-    </va-sidenav-submenu>
-    {/* <va-sidenav-accordion label="Parent item for submenu (accordion)">
-      <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-      <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-      <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-      <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-    </va-sidenav-accordion> */}
-  </va-sidenav>
-);
-
-const TemplateMobileWeb= ({}) => (
-  <va-accordion>
-    <va-accordion-item bordered header="Profile menu">
-      <span slot="icon">
-        <va-icon icon="menu" />
-      </span>
-      <va-sidenav>
-        <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
-        <va-sidenav-item href="#" label="Parent item with current page"></va-sidenav-item>
-        <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
-        <va-sidenav-item href="#" label="Parent item with a very long label for a sidenav item to see how it reflows when it is very long or if we zoom in"></va-sidenav-item>
-        <va-sidenav-submenu label="Parent item for submenu (non-accordion)">
+const Template = () => { 
+  return (
+    <va-sidenav header="Profile" icon-name="account_circle" icon-background-color="vads-color-primary">
+      <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
+      <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
+      <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
+      <va-sidenav-item href="#" label="Parent item with a very long label for a sidenav item to see how it reflows when it is very long or if we zoom in"></va-sidenav-item>
+      <va-sidenav-submenu label="Parent item for submenu (non-accordion)">
           <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-          <va-sidenav-item href="#" label="Child item" current-page={true}></va-sidenav-item>
-        </va-sidenav-submenu>
-        <va-sidenav-accordion label="Parent item for submenu (accordion)">
+          <va-sidenav-item href="#" label="Child item as current page" current-page={true}></va-sidenav-item>
           <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-          <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-          <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-          <va-sidenav-item href="#" label="Child item"></va-sidenav-item>
-        </va-sidenav-accordion>
-        </va-sidenav>
-    </va-accordion-item>
-  </va-accordion>
-);
+      </va-sidenav-submenu>
+      <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
+      <va-sidenav-item href="#" label="Parent item"></va-sidenav-item>
+    </va-sidenav>
+  )
+};
 
 export const Default = Template.bind(null);
 Default.argTypes = propStructure(sidenavDocs);
-
-export const MobileWeb = TemplateMobileWeb.bind(null);
-MobileWeb.argTypes = propStructure(sidenavDocs);
--- packages/web-components/src/components/va-sidenav/va-sidenav.tsx
+++ packages/web-components/src/components/va-sidenav/va-sidenav.tsx
@@ -1,4 +1,11 @@
-import { Component, Host, h, Prop } from '@stencil/core';
+import { Component, Host, h, Prop, Listen, Element } from '@stencil/core';
+import { forceUpdate } from '@stencil/core';
+
+/**
+ * Value corresponds with the --tablet breakpoint size.
+ * https://design.va.gov/foundation/breakpoints
+ */
+const TABLET_BREAKPOINT = 640;
 
 /**
  * @componentName Side Navigation
@@ -11,6 +18,8 @@ import { Component, Host, h, Prop } from '@stencil/core';
   shadow: true,
 })
 export class VaSidenav {
+
+  @Element() el!: HTMLElement;
 
   /**
    * Header text to display at the top of the side navigation.
@@ -32,25 +41,52 @@ export class VaSidenav {
    */
   @Prop() iconBackgroundColor?: string = 'vads-color-primary';
 
+  @Listen('resize', { target: 'window' })
+  handleResize() {
+    this.isDesktop = window.innerWidth > TABLET_BREAKPOINT;
+    forceUpdate(this);
+  }
+
+  private isDesktop: boolean = window.innerWidth > TABLET_BREAKPOINT;
+
   render() {
+    if (this.isDesktop) {
+      return (
+        <Host>
+          <nav aria-label={this.header || this.sectionName}>
+            {this.header && (
+              <h2 class="va-sidenav__header">
+                {this.iconName && this.iconBackgroundColor && (
+                  <span class="va-sidenav__icon-background" style={{ backgroundColor: `var(--${this.iconBackgroundColor})` }}>
+                    <va-icon icon={this.iconName}></va-icon>
+                  </span>
+                )}
+                {this.header}
+              </h2>
+            )}
+            <div class="va-sidenav__list">
+              <slot></slot>
+            </div>
+          </nav>
+        </Host>
+      );
+    }
+  
     return (
       <Host>
-        <nav aria-label={`Pages related to ${this.header || this.sectionName}`}>
-          {this.header && (
-            <h2 class="va-sidenav__header">
-              {this.iconName && this.iconBackgroundColor && (
-                <span class="va-sidenav__icon-background" style={{ backgroundColor: `var(--${this.iconBackgroundColor})` }}>
-                  <va-icon icon={this.iconName}></va-icon>
-                </span>
-              )}
-              {this.header}
-            </h2>
-          )}
-          <div class="va-sidenav__list">
-            <slot></slot>
-          </div>
-        </nav>
-      </Host>
+        <va-accordion open-single>
+          <va-accordion-item bordered header={`${this.header || this.sectionName} menu`}>
+            <span slot="icon">
+              <va-icon icon="menu" />
+            </span>
+            <nav aria-label={`Pages related to ${this.header || this.sectionName}`}>
+              <div class="va-sidenav__list">
+                <slot></slot>
+              </div>
+            </nav>
+          </va-accordion-item>
+        </va-accordion>
+      </Host> 
     );
   }
 }
