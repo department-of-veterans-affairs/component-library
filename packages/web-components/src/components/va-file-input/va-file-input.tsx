ce5949d2beb48b77b7781e2d95118f5dae0e365e
Matthew Dingee <52176351+humancompanion-usds@users.noreply.github.com>
2025-06-30 15:14:37 -0400

    Fix va-file-input delete modal text to use 'delete' and 'file' instead of 'remove' and 'document'
    - Changed 'We'll remove the uploaded document' to 'We'll delete the uploaded file'
    - Addresses GitHub issue #3087 for consistency with content guidance
    - Affects va-file-input component which is used by va-file-input-multiple

--- packages/web-components/src/components/va-file-input/va-file-input.tsx
+++ packages/web-components/src/components/va-file-input/va-file-input.tsx
@@ -162,8 +162,8 @@ export class VaFileInput {
       //This won't be read if its not in a timeout due to other messages being read.
       setTimeout(() => {this.updateStatusMessage(value)});
   }
-  
-  @Watch('percentUploaded') 
+
+  @Watch('percentUploaded')
     percentHandler(value: number) {
       if (value >= 100) {
         this.resetState();
@@ -171,7 +171,7 @@ export class VaFileInput {
     }
 
   /**
-   * called when file has been uploaded 
+   * called when file has been uploaded
    * or file upload has been cancelled
    * only relevant when percentUploaded specified
    */
@@ -502,7 +502,7 @@ export class VaFileInput {
       : 'selected-files-wrapper';
     const hintClass = 'usa-hint' + (headless ? ' usa-sr-only' : '');
 
-    
+
     const showProgBar = percentUploaded !== null && percentUploaded < 100;
 
     let statusClassNames = 'file-status-label'
