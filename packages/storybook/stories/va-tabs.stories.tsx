ee577fc1ab63931a0fb35dba28213e72b08d89ed
Ryan Munsch <ryan.munsch@wilcore.io>
2025-07-25 09:11:08 -0400

    va-tab stories - Add stories with panel heading variations

--- packages/storybook/stories/va-tabs.stories.tsx
+++ packages/storybook/stories/va-tabs.stories.tsx
@@ -14,16 +14,19 @@ const tabPanelDocs= getWebComponentDocs('va-tab-panel');
 const tabItems = [
   {
     label: 'Status',
+    alternateHeading: 'Status Report',
     targetId: 'panel-1',
     panelContent: 'Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.',
   },
   {
     label: 'Issues',
+    alternateHeading: 'Pressing Issues',
     targetId: 'panel-2',
     panelContent: 'A well regulated Militia, being necessary to the security of a free State, the right of the people to keep and bear Arms, shall not be infringed.',
   },
   {
     label: 'Overview',
+    alternateHeading: 'General Overview',
     targetId: 'panel-3',
     panelContent: 'No Soldier shall, in time of peace be quartered in any house, without the consent of the Owner, nor in time of war, but in a manner to be prescribed by law.'
   }
@@ -57,6 +60,8 @@ export default {
     tabItems: tabItems,
     selected: 0,
     templateKey: 0, // Used to differentiate between multiple instances in the DOM to prevent duplicate IDs
+    omitPanelHeading: false, // Used to omit the panel heading in the template.
+    useAlternateHeading: false, // Used to show a different heading than the tab label.
   },
 };
 
@@ -65,10 +70,25 @@ const Template = (args) => {
     <va-tabs label={args.label} selected={args.selected}>
       {
         args.tabItems.map((item, index) => {
+          // Format the label for the tab item, using a long label if specified
+          // in args or the alternate heading if specified.
           let formattedLabel = item.label;
+          let panelContentHeading = item.label;
           if (args.longTabLabelExample && index === 1) {
-            formattedLabel = args.longTabLabelExample
+            formattedLabel = args.longTabLabelExample;
+            panelContentHeading = args.longTabLabelExample;
           }
+          else if (args.useAlternateHeading) {
+            panelContentHeading = item.alternateHeading;
+          }
+
+          // Construct child content to be passed to the panel.
+          let panelChildren = !args.omitPanelHeading ?(
+            <Fragment>
+              <h2>{panelContentHeading}</h2>
+              <p>{item.panelContent}</p>
+            </Fragment>
+          ) : <p>{item.panelContent}</p>;
 
           return (
             <Fragment key={`fragment-${item.targetId}-${args.templateKey}`}>
@@ -82,8 +102,7 @@ const Template = (args) => {
                 panel-id={item.targetId}
                 selected={index === args.selected}
               >
-                <h2>{formattedLabel}</h2>
-                <p>{item.panelContent}</p>
+                {panelChildren}
               </va-tab-panel>
             </Fragment>
           );
@@ -111,3 +130,19 @@ WithALongLabel.args = {
   templateKey: 2,
 };
 WithALongLabel.argTypes = propStructure(tabsDocs);
+
+export const WithoutPanelHeading = Template.bind(null);
+WithoutPanelHeading.args = {
+  ...Default.args,
+  omitPanelHeading: true,
+  templateKey: 3,
+};
+WithoutPanelHeading.argTypes = propStructure(tabsDocs);
+
+export const WithHeadingNotMatchingTab = Template.bind(null);
+WithHeadingNotMatchingTab.args = {
+  ...Default.args,
+  useAlternateHeading: true,
+  templateKey: 4,
+};
+WithHeadingNotMatchingTab.argTypes = propStructure(tabsDocs);
