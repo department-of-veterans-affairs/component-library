4ecb18f72bb80b24d250e60ecfa87bb8ab585ff4
macalemid <mahmed@ironarchtech.com>
2025-11-24 12:56:34 -0500

    Updated search-filter documentation.

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -5706,6 +5706,8 @@ declare namespace LocalJSX {
     interface VaSearchFilter {
         /**
           * Represents a list of filter facets and their categories. Use a JSON array of objects with label and id properties.
+          * Set 'isRadio' to true on a filter to use radio buttons instead of checkboxes. If 'isRadio' is set to true, then one
+          * category must be set to active.
          */
         "filterOptions"?: Filter[];
         /**
