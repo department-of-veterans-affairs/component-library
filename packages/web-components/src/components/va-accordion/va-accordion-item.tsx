23ab2d2f0fb4510ccf3a75805710ef1ef501f776
Kerry Powell <1776069+powellkerry@users.noreply.github.com>
2025-12-10 14:47:19 -0700

    simple change

--- packages/web-components/src/components/va-accordion/va-accordion-item.tsx
+++ packages/web-components/src/components/va-accordion/va-accordion-item.tsx
@@ -142,7 +142,7 @@ export class VaAccordionItem {
 
     return (
       <Host>
-        <div>Testing</div>
+        <div>Testings</div>
         <div class={accordionItemClass}>
           <Header />
           <div
