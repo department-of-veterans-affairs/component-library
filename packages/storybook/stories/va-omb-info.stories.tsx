af4153cae4636c0ba441ee6127e7f1541612ead5
Ryan Munsch <ryan.munsch@wilcore.io>
2026-01-12 12:18:57 -0500

    Rough draft - first test story for omb-info

--- packages/storybook/stories/va-omb-info.stories.tsx
+++ packages/storybook/stories/va-omb-info.stories.tsx
@@ -1,16 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import { expect, userEvent, within, findByTestId } from 'storybook/test';
 import { getWebComponentDocs, propStructure, StoryDocs } from './wc-helpers';
+import { VaOmbInfo } from '@department-of-veterans-affairs/web-components/react-bindings';
 
 const ombInfoDocs = getWebComponentDocs('va-omb-info');
 
-const defaultArgs = {
-  'benefit-type': undefined,
-  'exp-date': '12/31/2077',
-  'omb-number': '12-3456',
-  'res-burden': 120,
-  'form-id': undefined,
-};
-
-export default {
+const meta: Meta<typeof VaOmbInfo> = {
+  component: VaOmbInfo,
   title: 'Components/OMB info',
   id: 'components/va-omb-info',
   parameters: {
@@ -19,10 +15,59 @@ export default {
       page: () => <StoryDocs storyDefault={Default} data={ombInfoDocs} />,
     },
   },
-  args: defaultArgs,
   argTypes: propStructure(ombInfoDocs),
+} satisfies Meta<typeof VaOmbInfo>;
+
+export default meta;
+
+type Story = StoryObj<typeof VaOmbInfo>;
+
+const defaultArgs = {
+  'benefitType': undefined,
+  'expDate': '12/31/2077',
+  'ombNumber': '12-3456',
+  'resBurden': 120,
+  'formId': undefined,
 };
 
+// export default {
+//   title: 'Components/OMB info',
+//   id: 'components/va-omb-info',
+//   parameters: {
+//     componentSubtitle: 'va-omb-info web component',
+//     docs: {
+//       page: () => <StoryDocs storyDefault={Default} data={ombInfoDocs} />,
+//     },
+//   },
+//   args: defaultArgs,
+//   argTypes: propStructure(ombInfoDocs),
+// };
+
+export const Test: Story = {
+  args: {
+    ...defaultArgs,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const ombInfo = await canvas.findByTestId('va-omb-info');
+
+    // Verify that the privacy act statement is visible
+    expect(ombInfo).toBeInTheDocument();
+
+    const vaButton = ombInfo.shadowRoot.querySelector('va-button');
+    expect(vaButton).toBeInTheDocument();
+
+    const button = vaButton.shadowRoot.querySelector('button');
+    expect(button).toBeInTheDocument();
+
+    // Open the modal
+    await userEvent.click(button);
+
+    const modal = ombInfo.shadowRoot.querySelector('va-modal');
+    expect(modal).toBeInTheDocument();
+    await expect(modal).toBeVisible();
+  },
+}
 
 const Template = ({
   'benefit-type': benefitType,
@@ -44,6 +89,7 @@ const Template = ({
 };
 
 export const Default = Template.bind(null);
+
 
 export const WithFormId = Template.bind(null);
 WithFormId.args = {
