929e345bb6797e9b906c38c95c6034b1e35237a8
amyleadem <amyleadem@gmail.com>
2026-01-15 12:37:29 -0700

    Downgrade react-output-target to 0.5.3

--- packages/web-components/react-bindings/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Re-export all components from ./components to maintain backward compatibility.
- * Consumers can import from '@department-of-veterans-affairs/web-components/react-bindings'
- * without needing to specify '/components' in the path.
- */
-export * from './components';
