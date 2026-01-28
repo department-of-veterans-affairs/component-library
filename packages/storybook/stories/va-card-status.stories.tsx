48b28158ba90f62dc7e6e57724f2e37d93817d7b
Edwin Otero <30818791+ediiotero@users.noreply.github.com>
2026-01-28 12:24:34 -0500

    va-card-status: add new component (#1862)
    * setting up card status comp
    
    * remove card-status comp and move changes into va-card comp
    
    * reset va-card and added new va-card-status
    
    * adding tests
    
    * update heading style and added va-link-action, updated tests
    
    * Removed slots, updated stories and tests
    
    * Update for 54.4.0 release
    
    * removing a story example
    
    * update component header and added subheader story
    
    * update card status stories
    
    * change error-message to error and update tests
    
    * update story names and links within cards
    
    * merge with main
    
    * Change heading to header, and updated tests
    
    * merge with main updates to index file
    
    * fix header moving when level is changed
    
    * update string for messageAriaDescribedby
    
    * add sr-only text to card header and update error message
    
    * update maturityCategory and maturityLevel
    
    ---------
    
    Signed-off-by: ediiotero <e.otero81@gmail.com>
    Co-authored-by: Jami Gibbs <jami.gibbs@adhocteam.us>

---
+++ packages/storybook/stories/va-card-status.stories.tsx
@@ -0,0 +1,112 @@
+import { getWebComponentDocs, propStructure, StoryDocs } from './wc-helpers';
+
+const cardStatusDocs = getWebComponentDocs('va-card-status');
+
+export default {
+  title: 'Components/Card Status',
+  id: 'components/va-card-status',
+  parameters: {
+    componentSubtitle: 'va-card-status web component',
+    docs: {
+      page: () => <StoryDocs storyDefault={Default} data={cardStatusDocs} />,
+    },
+  },
+};
+
+const defaultArgs = {
+  'header-level': 3,
+  'header-text': 'Mobile phone number',
+  'subheader-text': '',
+  'tag-status': 'info',
+  'tag-text': '',
+  'error': '',
+  'link-href': 'https://www.va.gov',
+  'link-text': 'Edit mobile phone number',
+  'required': false,
+};
+
+const Template = ({
+  'header-level': headerLevel,
+  'header-text': headerText,
+  'subheader-text': subheaderText,
+  'tag-status': tagStatus,
+  'tag-text': tagText,
+  'error': error,
+  'link-href': linkHref,
+  'link-text': linkText,
+  'required': required,
+}) => (
+  <va-card-status
+    header-level={headerLevel}
+    header-text={headerText}
+    subheader-text={subheaderText}
+    tag-status={tagStatus}
+    tag-text={tagText}
+    error={error}
+    link-href={linkHref}
+    link-text={linkText}
+    required={required}
+  >
+    <p>123-867-5309</p>
+  </va-card-status>
+);
+
+export const Default = Template.bind(null);
+Default.args = {
+  ...defaultArgs,
+};
+Default.argTypes = propStructure(cardStatusDocs);
+
+const TemplateErrorState = ({
+  'header-level': headerLevel,
+  'header-text': headerText,
+  'subheader-text': subheaderText,
+  'tag-status': tagStatus,
+  'tag-text': tagText,
+  'error': error,
+  'link-href': linkHref,
+  'link-text': linkText,
+  'required': required,
+}) => (
+  <va-card-status
+    header-level={headerLevel}
+    header-text={headerText}
+    subheader-text={subheaderText}
+    tag-status={tagStatus}
+    tag-text={tagText}
+    error={error}
+    link-href={linkHref}
+    link-text={linkText}
+    required={required}
+  >
+    <p>Not provided</p>
+  </va-card-status>
+);
+
+export const missingContentState = TemplateErrorState.bind(null);
+missingContentState.args = {
+  ...defaultArgs,
+  'tag-text': 'Missing',
+  'tag-status': 'error',
+  'link-text': 'Add mobile phone number',
+  'required': true,
+};
+
+export const errorState = TemplateErrorState.bind(null);
+errorState.args = {
+  ...defaultArgs,
+  'error':
+    'You must add your mobile phone number',
+  'tag-text': 'Missing',
+  'tag-status': 'error',
+  'link-text': 'Add mobile phone number',
+  'required': true,
+};
+
+export const withInfoTagAndSubheader= Template.bind(null);
+withInfoTagAndSubheader.args = {
+  ...defaultArgs,
+  'tag-text': 'Status',
+  'required': true,
+  'subheader-text': 'Optional subheader',
+};
