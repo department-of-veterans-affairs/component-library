d25c3a9263e45793971020f011bb9ccce6fbc854
Dan Brady <danbrady@users.noreply.github.com>
2026-02-10 09:44:47 -0500

    Update storybook preview viewports

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -23,7 +23,6 @@ import '@department-of-veterans-affairs/css-library/dist/stylesheets/modules/m-n
 import '@department-of-veterans-affairs/css-library/dist/stylesheets/shame.css';
 
 import '@department-of-veterans-affairs/component-library/dist/main.css';
-import { allModes } from "./modes";
 import {
   applyPolyfills,
   defineCustomElements,
@@ -58,45 +57,180 @@ applyPolyfills().then(() => {
 })();
 
 const viewports = {
-  xsmall: {
-    name: 'XSmall Screen',
+  mobile: {
+    name: '320px - VADS Mobile',
     styles: {
       height: '568px',
       width: '320px',
     },
     type: 'mobile',
   },
-  small: {
-    name: 'Small Screen',
+  mobileLg: {
+    name: '480px - VADS Large Mobile',
     styles: {
       height: '896px',
-      width: '481px',
+      width: '480px',
     },
     type: 'mobile',
   },
-  medium: {
-    name: 'Medium',
+  tablet: {
+    name: '640px - VADS Tablet',
     styles: {
-      height: '1112px',
+      height: '1136px',
+      width: '640px',
+    },
+    type: 'tablet',
+  },
+  mediumScreen: {
+    name: '768px - VADS Medium Screen',
+    styles: {
+      height: '1024px',
       width: '768px',
     },
     type: 'tablet',
   },
-  smallDesktop: {
-    name: 'Small Desktop',
+  desktop: {
+    name: '1024px - VADS Desktop',
     styles: {
-      height: '1400px',
+      height: '',
       width: '1024px',
     },
     type: 'desktop',
   },
-  large: {
-    name: 'Large',
+  desktopLg: {
+    name: '1200px - VADS Large Desktop',
     styles: {
-      height: '1600px',
-      width: '1201px',
+      height: '',
+      width: '1200px',
     },
     type: 'desktop',
+  },
+  widescreen: {
+    name: '1400px - VADS Widescreen',
+    styles: {
+      height: '',
+      width: '1400px',
+    },
+    type: 'desktop',
+  },
+};
+
+// Taken from https://analytics.usa.gov/veterans-affairs to reflect the most common devices used by our users. (February 2026)
+const commonDevices = {
+  common: {
+    name: '👇 Common VA.gov User Devices',
+    styles: {
+      height: '',
+      width: '',
+    },
+    type: 'other',
+  },
+  galaxy: {
+    name: '360px - Samsung Galaxy S24/25',
+    styles: {
+      height: '780px',
+      width: '360px',
+    },
+    type: 'other',
+  },
+
+  iphoneX: {
+    name: '375px - iPhone X, XS, 11 Pro',
+    styles: {
+      height: '812px',
+      width: '375px',
+    },
+    type: 'other',
+  },
+
+  iphone12: {
+    name: '390px - iPhone 12/13/14',
+    styles: {
+      height: '844px',
+      width: '390px',
+    },
+    type: 'other',
+  },
+
+  iphone15: {
+    name: '393px - iPhone 15/16',
+    styles: {
+      height: '852px',
+      width: '393px',
+    },
+    type: 'other',
+  },
+
+  iphone17: {
+    name: '402px - iPhone 17/17 Pro',
+    styles: {
+      height: '874px',
+      width: '402px',
+    },
+    type: 'other',
+  },
+
+  pixel: {
+    name: '412px - Google Pixel 6/7/8',
+    styles: {
+      height: '915px',
+      width: '412px',
+    },
+    type: 'other',
+  },
+
+  iphoneAir: {
+    name: '420px - iPhone Air',
+    styles: {
+      height: '912px',
+      width: '420px',
+    },
+    type: 'other',
+  },
+
+  iphone14ProMax: {
+    name: '430px - iPhone 14-16 Pro Max, 15/16 Plus',
+    styles: {
+      height: '932px',
+      width: '430px',
+    },
+    type: 'other',
+  },
+
+  iphone16ProMax: {
+    name: '440px - iPhone 16/17 Pro Max',
+    styles: {
+      height: '956px',
+      width: '440px',
+    },
+    type: 'other',
+  },
+
+  galaxyUltra: {
+    name: '440px - Samsung Galaxy S24-25 Ultra',
+    styles: {
+      height: '956px',
+      width: '440px',
+    },
+    type: 'other',
+  },
+
+  smallLaptops: {
+    name: '1440px - Common Laptops',
+    styles: {
+      height: '900px',
+      width: '1440px',
+    },
+    type: 'other',
+  },
+
+  largeLaptops: {
+    name: '1536px - Windows Laptop (125% scaling on 1920x1080)',
+    styles: {
+      height: '864px',
+      width: '1536px',
+    },
+    type: 'other',
   },
 };
 
@@ -118,32 +252,27 @@ export const parameters = {
   },
 
   viewport: {
-    options: viewports,
+    options: { ...viewports, ...commonDevices },
   },
 
   viewMode: 'docs',
 
   docs: {
-    codePanel: true
+    codePanel: true,
   },
 };
 
-export const initialGlobals = {
-  viewport: { value: 'small' },
-};
-
 export const decorators = [
-  (Story, { parameters }) => 
-  {
+  (Story, { parameters }) => {
     const pageLayout = parameters.storyType;
     switch (pageLayout) {
       case 'form':
         return (
-          <div className='sb-type--form'>
+          <div className="sb-type--form">
             <Story />
           </div>
         );
-      default: 
+      default:
         return (
           <div>
             <Story />
@@ -159,7 +288,7 @@ const observeDocsRoot = () => {
   const targetNode = document.getElementById('docs-root');
 
   // Configuration options for the observer
-  const config = {attributes: true, childList: false, subtree: false}
+  const config = { attributes: true, childList: false, subtree: false };
 
   // Callback function that 'resets' the aria-hidden attribute to false
   const callback = (
@@ -177,7 +306,7 @@ const observeDocsRoot = () => {
 
   // Start observing
   if (targetNode) observer.observe(targetNode, config);
-}
+};
 
 document.body.onload = function () {
   observeDocsRoot();
