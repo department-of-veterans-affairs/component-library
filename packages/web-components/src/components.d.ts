bd44b6f23cc385a1fcf3d0cce469efbd0d2d2e39
Ryan Munsch <ryan.munsch@wilcore.io>
2026-02-12 13:13:04 -0500

    Make headerText, linkHref, and linkText required

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -446,7 +446,7 @@ export namespace Components {
          */
         "required"?: boolean;
         /**
-          * Text to be displayed in the card subheader.  Heading level will be +1 of headerLevel Prop.
+          * Text to be displayed in the card subheader. Heading level will be +1 of headerLevel Prop.
          */
         "subheaderText": string;
         /**
@@ -4543,21 +4543,21 @@ declare namespace LocalJSX {
         /**
           * Text to be displayed in the card header.
          */
-        "headerText"?: string;
+        "headerText": string;
         /**
           * Set the href for the card link.
          */
-        "linkHref"?: string;
+        "linkHref": string;
         /**
           * Set the text for the card link.
          */
-        "linkText"?: string;
+        "linkText": string;
         /**
           * Sets the card to required and renders the (*Required) text.
          */
         "required"?: boolean;
         /**
-          * Text to be displayed in the card subheader.  Heading level will be +1 of headerLevel Prop.
+          * Text to be displayed in the card subheader. Heading level will be +1 of headerLevel Prop.
          */
         "subheaderText"?: string;
         /**
