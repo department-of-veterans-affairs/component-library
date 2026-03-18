dfb4f55b19101f27f006fc31cc28039b48c1b0f4
Jami Gibbs <jami.gibbs@adhocteam.us>
2026-03-18 13:31:54 -0500

    test

--- packages/web-components/src/components/va-back-to-top/va-back-to-top.tsx
+++ packages/web-components/src/components/va-back-to-top/va-back-to-top.tsx
@@ -18,7 +18,6 @@ import 'intersection-observer';
  * @componentName Back to top
  * @maturityCategory use
  * @maturityLevel deployed
- * @guidanceHref back-to-top
  */
 @Component({
   tag: 'va-back-to-top',
