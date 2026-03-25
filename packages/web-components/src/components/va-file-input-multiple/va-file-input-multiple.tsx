d21310434b90d1d6887bbab0f1f50f30478507c5
Ryan Munsch <ryan.munsch@wilcore.io>
2026-03-25 12:50:06 -0400

    Bold labels for error states on various components (#2041)
    * Fix date input label
    
    * Fix memorable-date and telephone-input labels
    
    * Fix va-text-input label
    
    * Fix va-file-input label
    
    * Update va-file-input-multiple.tsx
    
    * Add generic change and error handlers
    
    * Add conditional for file changed to generic handler
    
    * Fix label top margin for file input error state
    
    * Fix label top margin for text-input error state

--- packages/web-components/src/components/va-file-input-multiple/va-file-input-multiple.tsx
+++ packages/web-components/src/components/va-file-input-multiple/va-file-input-multiple.tsx
@@ -416,6 +416,7 @@ export class VaFileInputMultiple {
     label: string,
     required: boolean,
     headerSize?: number,
+    hasErrors?: boolean
   ) => {
     const requiredSpan = required ? (
       <span class="required"> {i18next.t('required')}</span>
@@ -437,7 +438,10 @@ export class VaFileInputMultiple {
     } else {
       return (
         <div class="label-header">
-          <label part="label" class="usa-label">
+          <label
+            part="label"
+            class={"usa-label" + (hasErrors ? ' usa-label--error' : '')}
+          >
             {label}
             {requiredSpan}
           </label>
@@ -538,13 +542,14 @@ export class VaFileInputMultiple {
       disablePasswordSubmitButtonPattern,
     } = this;
     const outerWrapClass = this.isEmpty() ? '' : 'outer-wrap';
-    const hasError = this.hasErrors() ? 'has-error' : '';
+    const hasErrors = this.hasErrors();
+    const hasError = hasErrors ? 'has-error' : '';
 
     return (
-      <Host class={hasError} error={hasError ? 'error' : ''}>
+      <Host class={hasError} error={hasErrors ? 'error' : ''}>
         {label &&
           !readOnly &&
-          this.renderLabelOrHeader(label, required, headerSize)}
+          this.renderLabelOrHeader(label, required, headerSize, hasErrors)}
         {hint && !readOnly && (
           <div class="usa-hint" id="input-hint-message">
             {hint}
