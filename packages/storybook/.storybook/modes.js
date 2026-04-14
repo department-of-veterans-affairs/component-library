a54b6f22e7208f983d9fc63efe8249dd7fbd57a6
Jami Gibbs <jami.gibbs@adhocteam.us>
2026-04-14 14:36:55 -0500

    Remove mobile snapshots from chromatic (#2069)
    * remove mobile snapshots from chromatic
    
    * remove additional mobile snapshot config

--- packages/storybook/.storybook/modes.js
+++ packages/storybook/.storybook/modes.js
@@ -1,7 +1,4 @@
 export const allModes = {
-  mobile: {
-    viewport: 'mobile', // references the 'mobile' viewport from preview.ts
-  },
   desktop: {
     viewport: 'desktopLg', // references the 'desktopLg' viewport from preview.ts
   },
